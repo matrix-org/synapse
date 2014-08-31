df752a15cea3c96e884a27c2fb92e3d4dc7e843f
Matthew Hodgson <matthew@matrix.org>
2014-09-01 00:42:03 +0100

    licenses, please...

--- webclient/login/login-controller.js
+++ webclient/login/login-controller.js
@@ -1,3 +1,19 @@
+/*
+ Copyright 2014 matrix.org
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+ 
 angular.module('LoginController', ['matrixService'])
 .controller('LoginController', ['$scope', '$location', 'matrixService', 'eventStreamService',
                                     function($scope, $location, matrixService, eventStreamService) {
@@ -13,11 +29,9 @@ angular.module('LoginController', ['matrixService'])
     {
         hs_url += ":" + $location.port();
     }
-    var example_domain = $location.host();
     
     $scope.account = {
         homeserver: hs_url,
-        example_domain: example_domain,
         desired_user_name: "",
         user_id: "",
         password: "",
