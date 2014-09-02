0a1260b03a4522a41105478c9b123eed36f40a25
Mark Haines <mark.haines@matrix.org>
2014-09-02 18:00:15 +0100

    Add ratelimiting config

---
+++ synapse/config/ratelimiting.py
@@ -0,0 +1,21 @@
+from ._base import Config
+
+class RatelimitConfig(Config):
+
+    def __init__(self, args):
+        super(RatelimitConfig, self).__init__(args)
+        self.rc_messages_per_second = args.rc_messages_per_second
+        self.rc_message_burst_count = args.rc_message_burst_count
+
+    @classmethod
+    def add_arguments(cls, parser):
+        super(RatelimitConfig, cls).add_arguments(parser)
+        rc_group = parser.add_argument_group("ratelimiting")
+        rc_group.add_argument(
+            "--rc-messages-per-second", type=float, default=0.2
+            help="number of messages a client can send per second"
+        )
+        rc_group.add_argument(
+            "--rc_messsage_burst_count", type=float, default=10
+            help="number of message a client can send before being throttled"
+        )
