90f5eb12701bb7e623c176aa123aad41652f3417
Kegan Dougal <kegan@matrix.org>
2014-09-22 15:00:23 +0100

    Set required environment variables for e2e testing.
    Added an 'id' to the login button so it can be automatically triggered.
    Also, added an onPrepare section to protractor.conf to do the login.

--- webclient/test/protractor.conf.js
+++ webclient/test/protractor.conf.js
@@ -1,6 +1,11 @@
 var env = require("./environment-protractor.js");
-
 exports.config = {
-  seleniumAddress: env.seleniumAddress,
-  specs: ['e2e/*.spec.js']
+    seleniumAddress: env.seleniumAddress,
+    specs: ['e2e/*.spec.js'],
+    onPrepare: function() {
+        browser.driver.get(env.loginUrl);
+        browser.driver.findElement(by.id("user_id")).sendKeys(env.username);
+        browser.driver.findElement(by.id("password")).sendKeys(env.password);
+        browser.driver.findElement(by.id("login")).click();
+    }
 }
