95acf63ea38eae6762fb8c7d3ac672661f711dfd
Kegan Dougal <kegan@matrix.org>
2014-09-22 16:50:12 +0100

    Add working protractor e2e test.
    This uses the ignoreSynchronization flag because of the longpoll on the event
    stream. It would be better to use $interval, but couldn't get that to
    *reliably* work when testing. I suspect that $interval won't help us here,
    since there is genuinely an open $http connection, as we're doing a long
    poll. https://github.com/angular/protractor/issues/49 for more info.

--- webclient/test/e2e/user.spec.js
+++
@@ -1,6 +0,0 @@
-describe("user page", function() {
-    it("should have a title", function() {
-        browser.get("http://matrix.org/alpha/#/login");
-        expect(browser.getTitle()).toEqual("[matrix]");
-    });
-});
