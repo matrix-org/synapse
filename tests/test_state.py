83c53113af50b8c838e5a6795f049f63178641c9
Kegan Dougal <kegan@matrix.org>
2014-10-09 15:51:05 +0100

    Break a test.

--- tests/test_state.py
+++ tests/test_state.py
@@ -71,7 +71,7 @@ class StateTestCase(unittest.TestCase):
 
         is_new = yield self.state.handle_new_state(new_pdu)
 
-        self.assertTrue(is_new)
+        self.assertFalse(is_new)
 
         self.persistence.get_unresolved_state_tree.assert_called_once_with(
             new_pdu
