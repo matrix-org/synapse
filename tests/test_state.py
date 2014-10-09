3db09c4d1576c96c41c825f04aab6deeaadb18de
Kegan Dougal <kegan@matrix.org>
2014-10-09 15:53:40 +0100

    Still broken.

--- tests/test_state.py
+++ tests/test_state.py
@@ -71,7 +71,8 @@ class StateTestCase(unittest.TestCase):
 
         is_new = yield self.state.handle_new_state(new_pdu)
 
-        self.assertFalse(is_new)
+        self.assertTrue(is_new)
+        self.assertTrue(False)
 
         self.persistence.get_unresolved_state_tree.assert_called_once_with(
             new_pdu
