06a5a40e903589c734d9a5c9972a2fdc331959e9
Mark Haines <mark.haines@matrix.org>
2014-10-20 15:11:01 +0100

    use a tagged version of syutil rather than master

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "py-bcrypt",
     ],
     dependency_links=[
-        "https://github.com/matrix-org/syutil/tarball/master#egg=syutil-0.0.2",
+        "https://github.com/matrix-org/syutil/tarball/v0.0.2#egg=syutil-0.0.2",
     ],
     setup_requires=[
         "setuptools_trial",
