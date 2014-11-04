dca3ba2f77a3530d78a951556ea8c6cfdc0463df
Mark Haines <mark.haines@matrix.org>
2014-11-04 16:19:03 +0000

    Determine webclient path by the python module it is contained in

--- demo/start.sh
+++ demo/start.sh
@@ -41,6 +41,6 @@ for port in 8080 8081 8082; do
 done
 
 echo "Starting webclient on port 8000..."
-python "demo/webserver.py" -p 8000 -P "$DIR/webserver.pid" "webclient"
+python "demo/webserver.py" -p 8000 -P "$DIR/webserver.pid" "syweb/webclient"
 
 cd "$CWD"
