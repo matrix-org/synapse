0d1221155e8363a6e0c8cdc9c113e43172085e48
Mark Haines <mark.haines@matrix.org>
2014-11-19 16:40:01 +0000

    remove unused import

--- synapse/notifier.py
+++ synapse/notifier.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from twisted.internet import defer, reactor
+from twisted.internet import defer
 
 from synapse.util.logutils import log_function
 from synapse.util.logcontext import PreserveLoggingContext
