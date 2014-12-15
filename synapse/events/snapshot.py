670dcdfc14d855c45b076f852673906dc450f515
Erik Johnston <erik@matrix.org>
2014-12-15 16:16:58 +0000

    Remove unused functions

--- synapse/events/snapshot.py
+++ synapse/events/snapshot.py
@@ -13,48 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from twisted.internet import defer
-
-
-class EventSnapshot(object):
-    def __init__(self, prev_events, depth, current_state,
-                 current_state_group):
-        self._prev_events = prev_events
-        self._depth = depth
-        self._current_state = current_state
-        self._current_state_group = current_state_group
-
-
-class EventCache(object):
-    def __init__(self, store):
-        self._store = store
-
-        self._cache = {}
-
-    @defer.inlineCallbacks
-    def load_event(self, event_id):
-        event = self._cache.get(event_id, None)
-
-        if not event:
-            event = yield self._store.get_event(
-                event_id,
-                allow_none=True
-            )
-
-            if event:
-                self._cache[event_id] = event
-
-        defer.returnValue(event)
-
-    def load_event_from_cache(self, event_id):
-        return self._cache.get(event_id, None)
-
-    def add_to_cache(self, *events):
-        self._cache.update({
-            event.event_id: event
-            for event in events
-        })
-
 
 class EventContext(object):
 
