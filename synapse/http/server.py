adb04b1e572d13b75541f4684aac3683e94d70b8
Mark Haines <mark.haines@matrix.org>
2015-01-06 13:21:39 +0000

    Update copyright notices

--- synapse/http/server.py
+++ synapse/http/server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2014 OpenMarket Ltd
+# Copyright 2014, 2015 OpenMarket Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -233,7 +233,7 @@ def respond_with_json_bytes(request, code, json_bytes, send_cors=False,
     request.setResponseCode(code, message=response_code_message)
     request.setHeader(b"Content-Type", b"application/json")
     request.setHeader(b"Server", AGENT_NAME)
-    request.setHeader(b"Content-Length", b"%d" % len(json_bytes))
+    request.setHeader(b"Content-Length", b"%d" % (len(json_bytes),))
 
     if send_cors:
         request.setHeader("Access-Control-Allow-Origin", "*")
