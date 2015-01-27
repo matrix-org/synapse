f9958f34043bf4fdf5923f471f193b40188e67bb
Paul "LeoNerd" Evans <paul@matrix.org>
2015-01-27 13:17:25 +0000

    Use new V2AlphaRestTestCase

--- tests/rest/client/v2_alpha/test_filter.py
+++ tests/rest/client/v2_alpha/test_filter.py
@@ -13,52 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tests import unittest
 from twisted.internet import defer
 
-from mock import Mock
+from . import V2AlphaRestTestCase
 
-from ....utils import MockHttpResource, MockKey
-
-from synapse.server import HomeServer
 from synapse.rest.client.v2_alpha import filter
-from synapse.types import UserID
 
 
-PATH_PREFIX = "/_matrix/client/v2_alpha"
-
-
-class FilterTestCase(unittest.TestCase):
+class FilterTestCase(V2AlphaRestTestCase):
     USER_ID = "@apple:test"
     TO_REGISTER = [filter]
-
-    def setUp(self):
-        self.mock_resource = MockHttpResource(prefix=PATH_PREFIX)
-
-        mock_config = Mock()
-        mock_config.signing_key = [MockKey()]
-
-        hs = HomeServer("test",
-            db_pool=None,
-            datastore=Mock(spec=[
-                "insert_client_ip",
-            ]),
-            http_client=None,
-            resource_for_client=self.mock_resource,
-            resource_for_federation=self.mock_resource,
-            config=mock_config,
-        )
-
-        def _get_user_by_token(token=None):
-            return {
-                "user": UserID.from_string(self.USER_ID),
-                "admin": False,
-                "device_id": None,
-            }
-        hs.get_auth().get_user_by_token = _get_user_by_token
-
-        for r in self.TO_REGISTER:
-            r.register_servlets(hs, self.mock_resource)
 
     @defer.inlineCallbacks
     def test_filter(self):
