0b725f5c4f8f0550205c09bbdc775f6ad613a1fd
Matthew Hodgson <matthew@matrix.org>
2015-02-09 16:48:31 +0000

    oops

--- synapse/python_dependencies.py
+++ synapse/python_dependencies.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 REQUIREMENTS = {
     "syutil==0.0.2": ["syutil"],
     "matrix_angular_sdk==0.6.0": ["syweb>=0.6.0"],
-    "Twisted==14.0.0": ["twisted==14.0.2"],
+    "Twisted==14.0.2": ["twisted==14.0.2"],
     "service_identity>=1.0.0": ["service_identity>=1.0.0"],
     "pyopenssl>=0.14": ["OpenSSL>=0.14"],
     "pyyaml": ["yaml"],
