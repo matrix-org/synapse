e49d6b1568eab259dc5eea434da2d0e65876e492
Erik Johnston <erik@matrix.org>
2015-03-06 11:37:24 +0000

    Unused import

--- synapse/config/tls.py
+++ synapse/config/tls.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ._base import Config, ConfigError
+from ._base import Config
 
 from OpenSSL import crypto
 import subprocess
