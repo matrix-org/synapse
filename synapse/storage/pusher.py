6e91f14d09cf64100dc8fecc83c333f59fe8e77a
Erik Johnston <erik@matrix.org>
2015-04-15 16:25:07 +0100

    Add missing yield

--- synapse/storage/pusher.py
+++ synapse/storage/pusher.py
@@ -99,7 +99,7 @@ class PusherStore(SQLBaseStore):
                    app_display_name, device_display_name,
                    pushkey, pushkey_ts, lang, data):
         try:
-            next_id = self._pushers_id_gen.get_next()
+            next_id = yield self._pushers_id_gen.get_next()
             yield self._simple_upsert(
                 PushersTable.table_name,
                 dict(
