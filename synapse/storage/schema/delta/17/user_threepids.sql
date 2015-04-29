f8c30faf256bb47c6725b7752e36e49c519d1adc
David Baker <dave@matrix.org>
2015-04-29 16:58:42 +0100

    Oops, update the contraint too

--- synapse/storage/schema/delta/17/user_threepids.sql
+++ synapse/storage/schema/delta/17/user_threepids.sql
@@ -4,6 +4,6 @@ CREATE TABLE user_threepids (
     address TEXT NOT NULL,
     validated_at BIGINT NOT NULL,
     added_at BIGINT NOT NULL,
-    CONSTRAINT user_medium_address UNIQUE (user, medium, address)
+    CONSTRAINT user_medium_address UNIQUE (user_id, medium, address)
 );
 CREATE INDEX user_threepids_user_id ON user_threepids(user_id);
