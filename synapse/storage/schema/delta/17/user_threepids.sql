fb95035a6534d5a16776242b5f31ceef567ca2cc
David Baker <dave@matrix.org>
2015-04-29 16:53:41 +0100

    Be postgressive

--- synapse/storage/schema/delta/17/user_threepids.sql
+++ synapse/storage/schema/delta/17/user_threepids.sql
@@ -1,10 +1,9 @@
 CREATE TABLE user_threepids (
-    id INTEGER PRIMARY KEY NOT NULL,
     user TEXT NOT NULL,
     medium TEXT NOT NULL,
     address TEXT NOT NULL,
-    validated_at INTEGER NOT NULL,
-    added_at INTEGER NOT NULL,
-    CONSTRAINT user_medium_address UNIQUE (user, medium, address) ON CONFLICT REPLACE
+    validated_at BIGINT NOT NULL,
+    added_at BIGINT NOT NULL,
+    CONSTRAINT user_medium_address UNIQUE (user, medium, address)
 );
 CREATE INDEX user_threepids_user ON user_threepids(user);
