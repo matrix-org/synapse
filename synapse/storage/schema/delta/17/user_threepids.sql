61cd5d9045b767e2c1ef4e48e67ed0c70e1092fe
David Baker <dave@matrix.org>
2015-04-29 16:57:14 +0100

    Be more postgressive

--- synapse/storage/schema/delta/17/user_threepids.sql
+++ synapse/storage/schema/delta/17/user_threepids.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user_threepids (
-    user TEXT NOT NULL,
+    user_id TEXT NOT NULL,
     medium TEXT NOT NULL,
     address TEXT NOT NULL,
     validated_at BIGINT NOT NULL,
     added_at BIGINT NOT NULL,
     CONSTRAINT user_medium_address UNIQUE (user, medium, address)
 );
-CREATE INDEX user_threepids_user ON user_threepids(user);
+CREATE INDEX user_threepids_user_id ON user_threepids(user_id);
