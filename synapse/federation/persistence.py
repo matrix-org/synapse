977338a7afa5e95dba1ce230ba253daf2b239fb5
Erik Johnston <erik@matrix.org>
2015-05-05 18:12:44 +0100

    Use buffer(...) when inserting into bytea column

--- synapse/federation/persistence.py
+++ synapse/federation/persistence.py
@@ -23,8 +23,6 @@ from twisted.internet import defer
 
 from synapse.util.logutils import log_function
 
-from syutil.jsonutil import encode_canonical_json
-
 import logging
 
 
@@ -71,7 +69,7 @@ class TransactionActions(object):
             transaction.transaction_id,
             transaction.origin,
             code,
-            encode_canonical_json(response)
+            response,
         )
 
     @defer.inlineCallbacks
