81682d0f820a6209535267a45ee28b8f66ff7794
Muthu Subramanian <muthu.subramanian.karunanidhi@ericsson.com>
2015-07-07 17:40:30 +0530

    Integrate SAML2 basic authentication - uses pysaml2

--- synapse/python_dependencies.py
+++ synapse/python_dependencies.py
@@ -31,6 +31,7 @@ REQUIREMENTS = {
     "pillow": ["PIL"],
     "pydenticon": ["pydenticon"],
     "ujson": ["ujson"],
+    "pysaml2": ["saml2"],
 }
 CONDITIONAL_REQUIREMENTS = {
     "web_client": {
