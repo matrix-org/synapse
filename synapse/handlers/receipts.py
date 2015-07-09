f0979afdb0b0b8f96730a97cf0b51de6024cabda
Erik Johnston <erik@matrix.org>
2015-07-09 16:02:07 +0100

    Remove spurious comment

--- synapse/handlers/receipts.py
+++ synapse/handlers/receipts.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Contains handlers for federation events."""
-
 from ._base import BaseHandler
 
 from twisted.internet import defer
