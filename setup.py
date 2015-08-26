06094591c5397af64a495dce2271da2df5b491b9
Mark Haines <mark.haines@matrix.org>
2015-08-26 13:13:01 +0100

    Pass an empty list of arguments to tox if no arguments are given

--- setup.py
+++ setup.py
@@ -65,6 +65,8 @@ class Tox(Command):
         args = self.tox_args
         if args:
             args = shlex.split(self.tox_args)
+        else:
+            args = []
         errno = tox.cmdline(args=args)
         sys.exit(errno)
 
