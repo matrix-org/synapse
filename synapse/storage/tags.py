767c20a869933204b6e545c6f1495cd7cd298a87
Matthew Hodgson <matthew@matrix.org>
2015-11-06 20:49:57 +0100

    add a key existence check to tags_by_room to avoid /events 500'ing when testing against vector

--- synapse/storage/tags.py
+++ synapse/storage/tags.py
@@ -95,7 +95,8 @@ class TagsStore(SQLBaseStore):
         if room_ids:
             tags_by_room = yield self.get_tags_for_user(user_id)
             for room_id in room_ids:
-                results[room_id] = tags_by_room[room_id]
+                if room_id in tags_by_room:
+                    results[room_id] = tags_by_room[room_id]
 
         defer.returnValue(results)
 
