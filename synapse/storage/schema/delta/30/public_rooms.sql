b2757655455fb2bf485c66affeb5a294eb9459c2
Erik Johnston <erik@matrix.org>
2016-03-23 14:15:32 +0000

    Comment about weird SQL

--- synapse/storage/schema/delta/30/public_rooms.sql
+++ synapse/storage/schema/delta/30/public_rooms.sql
@@ -15,7 +15,9 @@
 
 
 /* This release removes the restriction that published rooms must have an alias,
- * so we go back and ensure the only 'public' rooms are ones with an alias.*/
+ * so we go back and ensure the only 'public' rooms are ones with an alias.
+ * We use (1 = 0) and (1 = 1) so that it works in both postgres and sqlite
+ */
 UPDATE rooms SET is_public = (1 = 0) WHERE is_public = (1 = 1) AND room_id not in (
     SELECT room_id FROM room_aliases
 );
