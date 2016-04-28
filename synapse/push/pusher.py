937c407eef78648f1c4c1afe56fdfd8598315b19
David Baker <dave@matrix.org>
2016-04-28 15:12:14 +0100

    Only import email pusher if email notifs are on

--- synapse/push/pusher.py
+++ synapse/push/pusher.py
@@ -1,12 +1,14 @@
 from httppusher import HttpPusher
-from emailpusher import EmailPusher
-
-PUSHER_TYPES = {
-    'http': HttpPusher,
-    'email': EmailPusher,
-}
 
 
 def create_pusher(hs, pusherdict):
+    PUSHER_TYPES = {
+        "http": HttpPusher,
+    }
+
+    if hs.config.email_enable_notifs:
+        from emailpusher import EmailPusher
+        PUSHER_TYPES["email"] = EmailPusher
+
     if pusherdict['kind'] in PUSHER_TYPES:
         return PUSHER_TYPES[pusherdict['kind']](hs, pusherdict)
