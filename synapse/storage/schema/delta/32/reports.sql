fcd1eb642dc8b73e372b78bb788e3a17a5e40ace
Erik Johnston <erik@matrix.org>
2016-05-04 16:51:51 +0100

    Add primary key

--- synapse/storage/schema/delta/32/reports.sql
+++ synapse/storage/schema/delta/32/reports.sql
@@ -15,7 +15,7 @@
 
 
 CREATE TABLE event_reports(
-    id BIGINT NOT NULL,
+    id BIGINT NOT NULL PRIMARY KEY,
     received_ts BIGINT NOT NULL,
     room_id TEXT NOT NULL,
     event_id TEXT NOT NULL,
