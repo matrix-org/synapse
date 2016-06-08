defa28efa186013fab18b3da76f60273cb6c3bb1
Mark Haines <mjark@negativecurvature.net>
2016-06-08 15:11:31 +0100

    Disable the synchrotron on jenkins until the sytest support lands (#855)
    * Disable the synchrotron on jenkins until the sytest support lands
    
    * Poke jenkins
    
    * Poke jenkins
    
    * Poke jenkins
    
    * Poke jenkins
    
    * Poke jenkins
    
    * Poke jenkins
    
    * Poke jenkins
    
    * Poke jenkins

--- jenkins-dendron-postgres.sh
+++ jenkins-dendron-postgres.sh
@@ -73,11 +73,12 @@ git checkout "${GIT_BRANCH}" || (echo >&2 "No ref ${GIT_BRANCH} found, falling b
 
 ./jenkins/prep_sytest_for_postgres.sh
 
+mkdir -p var
+
 echo >&2 "Running sytest with PostgreSQL";
 ./jenkins/install_and_run.sh --python $TOX_BIN/python \
                              --synapse-directory $WORKSPACE \
                              --dendron $WORKSPACE/dendron/bin/dendron \
-                             --synchrotron \
                              --pusher \
                              --port-base $PORT_BASE
 
