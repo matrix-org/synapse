aa3a4944d51c60886984211a7f8ae6b7fbac765d
David Baker <dave@matrix.org>
2016-06-24 11:45:23 +0100

    more pep8

--- synapse/storage/room.py
+++ synapse/storage/room.py
@@ -18,7 +18,6 @@ from twisted.internet import defer
 from synapse.api.errors import StoreError
 
 from ._base import SQLBaseStore
-from synapse.util.caches.descriptors import cachedInlineCallbacks
 from .engines import PostgresEngine, Sqlite3Engine
 
 import collections
