732cf72b861c99492b240a21e4539a3c40474596
Erik Johnston <erik@matrix.org>
2016-08-17 18:12:21 +0100

    Fix push_display_name_rename schema update

--- synapse/storage/schema/delta/34/push_display_name_rename.sql
+++ synapse/storage/schema/delta/34/push_display_name_rename.sql
@@ -13,5 +13,8 @@
  * limitations under the License.
  */
 
+DELETE FROM push_rules WHERE rule_id = 'global/override/.m.rule.contains_display_name';
 UPDATE push_rules SET rule_id = 'global/override/.m.rule.contains_display_name' WHERE rule_id = 'global/underride/.m.rule.contains_display_name';
+
+DELETE FROM push_rules_enable WHERE rule_id = 'global/override/.m.rule.contains_display_name';
 UPDATE push_rules_enable SET rule_id = 'global/override/.m.rule.contains_display_name' WHERE rule_id = 'global/underride/.m.rule.contains_display_name';
