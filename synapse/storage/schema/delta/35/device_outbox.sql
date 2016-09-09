0877157353c5610e8ede2f205a84ae80bef7983b
Mark Haines <mark.haines@matrix.org>
2016-09-09 11:04:47 +0100

    Just move the schema and add some DROPs

--- synapse/storage/schema/delta/35/device_outbox.sql
+++ synapse/storage/schema/delta/35/device_outbox.sql
@@ -13,6 +13,7 @@
  * limitations under the License.
  */
 
+DROP TABLE IF EXISTS device_federation_outbox;
 CREATE TABLE device_federation_outbox (
     destination TEXT NOT NULL,
     stream_id BIGINT NOT NULL,
@@ -21,16 +22,18 @@ CREATE TABLE device_federation_outbox (
 );
 
 
+DROP INDEX IF EXISTS device_federation_outbox_destination_id;
 CREATE INDEX device_federation_outbox_destination_id
     ON device_federation_outbox(destination, stream_id);
 
 
+DROP TABLE IF EXISTS device_federation_inbox;
 CREATE TABLE device_federation_inbox (
     origin TEXT NOT NULL,
     message_id TEXT NOT NULL,
     received_ts BIGINT NOT NULL
 );
 
-
+DROP INDEX IF EXISTS device_federation_inbox_sender_id;
 CREATE INDEX device_federation_inbox_sender_id
     ON device_federation_inbox(origin, message_id);
