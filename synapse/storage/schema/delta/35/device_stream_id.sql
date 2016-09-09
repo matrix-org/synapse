647c7245733a72b9b71decb2b321869a942dbb88
Mark Haines <mark.haines@matrix.org>
2016-09-09 11:52:44 +0100

    Use the previous MAX value if any to set the stream_id

--- synapse/storage/schema/delta/35/device_stream_id.sql
+++ synapse/storage/schema/delta/35/device_stream_id.sql
@@ -17,4 +17,5 @@ CREATE TABLE device_max_stream_id (
     stream_id BIGINT NOT NULL
 );
 
-INSERT INTO device_max_stream_id (stream_id) VALUES (0);
+INSERT INTO device_max_stream_id (stream_id)
+    SELECT COALESCE(MAX(stream_id), 0) FROM device_inbox;
