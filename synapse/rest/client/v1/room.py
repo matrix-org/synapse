1d98cf26be4c429a19f29aa371e7781a0dcf2a28
Erik Johnston <erik@matrix.org>
2016-09-15 13:18:35 +0100

    By default limit /publicRooms to 100 entries

--- synapse/rest/client/v1/room.py
+++ synapse/rest/client/v1/room.py
@@ -319,7 +319,7 @@ class PublicRoomListRestServlet(ClientV1RestServlet):
             else:
                 pass
 
-        limit = parse_integer(request, "limit", 0)
+        limit = parse_integer(request, "limit", 100)
         since_token = parse_string(request, "since", None)
 
         handler = self.hs.get_room_list_handler()
