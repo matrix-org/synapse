8009d843647a8c693340c7b9ec341066fb6db3b6
Erik Johnston <erik@matrix.org>
2016-09-21 16:46:59 +0100

    Match against event_id, rather than room_id

--- synapse/storage/schema/delta/36/readd_public_rooms.sql
+++ synapse/storage/schema/delta/36/readd_public_rooms.sql
@@ -16,7 +16,7 @@
 -- Re-add some entries to stream_ordering_to_exterm that were incorrectly deleted
 INSERT INTO stream_ordering_to_exterm (stream_ordering, room_id, event_id)
     SELECT
-        (SELECT max(stream_ordering) FROM events where room_id = e.room_id) AS stream_ordering,
+        (SELECT stream_ordering FROM events where event_id = e.event_id) AS stream_ordering,
         room_id,
         event_id
     FROM event_forward_extremities AS e
