59db6d451b572ea18fe4d99b1720c7122506e4a5
Gereon Frey <me@gereonfrey.de>
2016-11-05 19:01:38 +0100

    Fix issue with wrong DN in simple bind
    During search mode bind the DN for the user to log in is retrieved. This
    information was deferred afterwards, when calling the simple bind
    method. I restructured the code so that _ldap_simple_bind wraps and
    _ldap_internal_bind method, building the DN. The latter method is then
    used during search mode to bind the DN returned by the first search.

--- synapse/util/ldap_auth_provider.py
+++ synapse/util/ldap_auth_provider.py
@@ -219,9 +219,8 @@ class LdapAuthProvider(object):
 
         return ldap_config
 
-    def _ldap_simple_bind(self, server, localpart, password):
-        """ Attempt a simple bind with the credentials
-            given by the user against the LDAP server.
+    def _ldap_internal_bind(self, server, bind_dn, password):
+        """ Attempt a simple bind with the given dn against the LDAP server.
 
             Returns True, LDAP3Connection
                 if the bind was successful
@@ -230,18 +229,11 @@ class LdapAuthProvider(object):
         """
 
         try:
-            # bind with the the local users ldap credentials
-            bind_dn = "{prop}={value},{base}".format(
-                prop=self.ldap_attributes['uid'],
-                value=localpart,
-                base=self.ldap_base
-            )
             conn = ldap3.Connection(server, bind_dn, password)
             logger.debug(
                 "Established LDAP connection in simple bind mode: %s",
                 conn
             )
-
             if self.ldap_start_tls:
                 conn.start_tls()
                 logger.debug(
@@ -265,6 +257,25 @@ class LdapAuthProvider(object):
         except ldap3.core.exceptions.LDAPException as e:
             logger.warn("Error during LDAP authentication: %s", e)
             return False, None
+
+    def _ldap_simple_bind(self, server, localpart, password):
+        """ Attempt a simple bind with the credentials
+            given by the user against the LDAP server.
+
+            Returns True, LDAP3Connection
+                if the bind was successful
+            Returns False, None
+                if an error occured
+        """
+
+        # bind with the the local users ldap credentials
+        bind_dn = "{prop}={value},{base}".format(
+            prop=self.ldap_attributes['uid'],
+            value=localpart,
+            base=self.ldap_base
+        )
+
+        return self._ldap_internal_bind(server, bind_dn, password)
 
     def _ldap_authenticated_search(self, server, localpart, password):
         """ Attempt to login with the preconfigured bind_dn
@@ -337,7 +348,7 @@ class LdapAuthProvider(object):
                 # Note: do not use rebind(), for some reason it did not verify
                 #       the password for me!
                 conn.unbind()
-                return self._ldap_simple_bind(server, localpart, password)
+                return self._ldap_internal_bind(server, user_dn, password)
             else:
                 # BAD: found 0 or > 1 results, abort!
                 if len(conn.response) == 0:
