c0380402bc0c736a88db35a48ad554cbc2770fa6
David Baker <dave@matrix.org>
2017-05-03 10:56:22 +0100

    List caught expection types

--- synapse/http/client.py
+++ synapse/http/client.py
@@ -260,7 +260,7 @@ class SimpleHttpClient(object):
             errcode = jsonBody['errcode']
             error = jsonBody['error']
             return MatrixCodeMessageException(response.code, error, errcode)
-        except:
+        except (ValueError, KeyError) as e:
             return CodeMessageException(response.code, body)
 
     # XXX: FIXME: This is horribly copy-pasted from matrixfederationclient.
