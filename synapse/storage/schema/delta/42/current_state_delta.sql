dd48f7204c511d5ba4438dfe01679bcb7367216d
Erik Johnston <erik@matrix.org>
2017-05-30 15:01:22 +0100

    Add comment

--- synapse/storage/schema/delta/42/current_state_delta.sql
+++ synapse/storage/schema/delta/42/current_state_delta.sql
@@ -20,7 +20,7 @@ CREATE TABLE current_state_delta_stream (
     type TEXT NOT NULL,
     state_key TEXT NOT NULL,
     event_id TEXT,  -- Is null if the key was removed
-    prev_event_id TEXT
+    prev_event_id TEXT  -- Is null if the key was added
 );
 
 CREATE INDEX current_state_delta_stream_idx ON current_state_delta_stream(stream_id);
