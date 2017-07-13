5bbb53580a3fc732f0e4aab49f7893bd4f7e2a43
Erik Johnston <erik@matrix.org>
2017-07-13 10:25:29 +0100

    raise NotImplementedError

--- synapse/groups/groups_server.py
+++ synapse/groups/groups_server.py
@@ -463,9 +463,7 @@ class GroupsServerHandler(object):
         }
 
         if self.hs.is_mine_id(user_id):
-            groups_local = self.hs.get_groups_local_handler()
-            res = yield groups_local.on_invite(group_id, user_id, content)
-            local_attestation = None
+            raise NotImplementedError()
         else:
             local_attestation = self.attestations.create_attestation(group_id, user_id)
             content.update({
@@ -593,8 +591,7 @@ class GroupsServerHandler(object):
 
         if is_kick:
             if self.hs.is_mine_id(user_id):
-                groups_local = self.hs.get_groups_local_handler()
-                yield groups_local.user_removed_from_group(group_id, user_id, {})
+                raise NotImplementedError()
             else:
                 yield self.transport_client.remove_user_from_group_notification(
                     get_domain_from_id(user_id), group_id, user_id, {}
