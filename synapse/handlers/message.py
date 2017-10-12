fae708c0e8c35930f1172322b7c0e9f0b1b3f9a4
hera <matrix@template.upcloud.com>
2017-10-12 10:41:46 +0000

    Disable auth on room_members for now
    because the moznet bridge is broken (https://github.com/matrix-org/matrix-appservice-irc/issues/506)

--- synapse/handlers/message.py
+++ synapse/handlers/message.py
@@ -390,7 +390,7 @@ class MessageHandler(BaseHandler):
         # If this is an AS, double check that they are allowed to see the members.
         # This can either be because the AS user is in the room or becuase there
         # is a user in the room that the AS is "interested in"
-        if requester.app_service and user_id not in users_with_profile:
+        if False and requester.app_service and user_id not in users_with_profile:
             for uid in users_with_profile:
                 if requester.app_service.is_interested_in_user(uid):
                     break
