4b1fceb913dbcd7472346e2d31c6cd4d99424f9a
Matthew Hodgson <matthew@matrix.org>
2017-11-30 01:34:03 +0000

    fix alternation operator for FTS4 - how did this ever work!?

--- synapse/storage/user_directory.py
+++ synapse/storage/user_directory.py
@@ -748,7 +748,7 @@ def _parse_query_sqlite(search_term):
 
     # Pull out the individual words, discarding any non-word characters.
     results = re.findall(r"([\w\-]+)", search_term, re.UNICODE)
-    return " & ".join("(%s* | %s)" % (result, result,) for result in results)
+    return " & ".join("(%s* OR %s)" % (result, result,) for result in results)
 
 
 def _parse_query_postgres(search_term):
