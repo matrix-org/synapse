142fb0a7d40f17d834e1e4d13963839195c1040b
Richard van der Hoff <richard@matrix.org>
2018-01-09 12:03:12 +0000

    Disable user_directory updates for UTs
    Fix flakiness in the UTs caused by the user_directory being updated in the
    background

--- tests/utils.py
+++ tests/utils.py
@@ -58,6 +58,10 @@ def setup_test_homeserver(name="test", datastore=None, config=None, **kargs):
         config.email_enable_notifs = False
         config.block_non_admin_invites = False
 
+        # disable user directory updates, because they get done in the
+        # background, which upsets the test runner.
+        config.update_user_directory = False
+
     config.use_frozen_dicts = True
     config.database_config = {"name": "sqlite3"}
     config.ldap_enabled = False
