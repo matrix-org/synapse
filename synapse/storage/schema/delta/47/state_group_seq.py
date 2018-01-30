22be5200059b6c9bf31c751151c0b9858044dcfe
Richard van der Hoff <richard@matrix.org>
2018-01-30 12:44:33 +0000

    Set start val for state_group_id_seq
    ... otherwise we try to recreate old state groups

--- synapse/storage/schema/delta/47/state_group_seq.py
+++ synapse/storage/schema/delta/47/state_group_seq.py
@@ -17,7 +17,20 @@ from synapse.storage.engines import PostgresEngine
 
 def run_create(cur, database_engine, *args, **kwargs):
     if isinstance(database_engine, PostgresEngine):
-        cur.execute("CREATE SEQUENCE state_group_id_seq")
+        # if we already have some state groups, we want to start making new
+        # ones with a higher id.
+        cur.execute("SELECT max(id) FROM state_groups")
+        row = cur.fetchone()
+
+        if row[0] is None:
+            start_val = 1
+        else:
+            start_val = row[0] + 1
+
+        cur.execute(
+            "CREATE SEQUENCE state_group_id_seq START WITH %s",
+            (start_val, ),
+        )
 
 
 def run_upgrade(*args, **kwargs):
