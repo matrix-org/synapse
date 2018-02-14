daec1d77be9fb3cd5f71db4cc49b4aa4acb33c26
Erik Johnston <erik@matrix.org>
2018-02-14 13:53:55 +0000

    Make _get_joined_hosts_cache cache non-iterable

--- synapse/storage/roommember.py
+++ synapse/storage/roommember.py
@@ -675,7 +675,7 @@ class RoomMemberStore(SQLBaseStore):
 
         defer.returnValue(result)
 
-    @cached(max_entries=10000, iterable=True)
+    @cached(max_entries=10000)
     def _get_joined_hosts_cache(self, room_id):
         return _JoinedHostsCache(self, room_id)
 
