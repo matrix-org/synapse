2165de3a574700402c058429760891cebf4f3de1
Erik Johnston <erik@matrix.org>
2018-02-16 12:08:42 +0000

    Split AccountDataStore and TagStore

--- synapse/storage/tags.py
+++ synapse/storage/tags.py
@@ -23,15 +23,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class TagsStore(SQLBaseStore):
-    def get_max_account_data_stream_id(self):
-        """Get the current max stream id for the private user data stream
-
-        Returns:
-            A deferred int.
-        """
-        return self._account_data_id_gen.get_current_token()
-
+class TagsWorkerStore(SQLBaseStore):
     @cached()
     def get_tags_for_user(self, user_id):
         """Get all the tags for a user.
@@ -169,6 +161,16 @@ class TagsStore(SQLBaseStore):
         ).addCallback(lambda rows: {
             row["tag"]: json.loads(row["content"]) for row in rows
         })
+
+
+class TagsStore(TagsWorkerStore):
+    def get_max_account_data_stream_id(self):
+        """Get the current max stream id for the private user data stream
+
+        Returns:
+            A deferred int.
+        """
+        return self._account_data_id_gen.get_current_token()
 
     @defer.inlineCallbacks
     def add_tag_to_room(self, user_id, room_id, tag, content):
