6f727653717d9fc32f5676f4d991fbbe85c1c812
Erik Johnston <erik@matrix.org>
2018-02-21 10:54:18 +0000

    Split PusherStore

--- synapse/replication/slave/storage/pushers.py
+++ synapse/replication/slave/storage/pushers.py
@@ -16,10 +16,10 @@
 from ._base import BaseSlavedStore
 from ._slaved_id_tracker import SlavedIdTracker
 
-from synapse.storage import DataStore
+from synapse.storage.pusher import PusherWorkerStore
 
 
-class SlavedPusherStore(BaseSlavedStore):
+class SlavedPusherStore(PusherWorkerStore, BaseSlavedStore):
 
     def __init__(self, db_conn, hs):
         super(SlavedPusherStore, self).__init__(db_conn, hs)
@@ -27,13 +27,6 @@ class SlavedPusherStore(BaseSlavedStore):
             db_conn, "pushers", "id",
             extra_tables=[("deleted_pushers", "stream_id")],
         )
-
-    get_all_pushers = DataStore.get_all_pushers.__func__
-    get_pushers_by = DataStore.get_pushers_by.__func__
-    get_pushers_by_app_id_and_pushkey = (
-        DataStore.get_pushers_by_app_id_and_pushkey.__func__
-    )
-    _decode_pushers_rows = DataStore._decode_pushers_rows.__func__
 
     def stream_positions(self):
         result = super(SlavedPusherStore, self).stream_positions()
