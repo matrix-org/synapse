fecb45e0c38912f72655d53faa953e7713511e7f
Erik Johnston <erik@matrix.org>
2018-03-26 13:31:49 +0100

    Remove last usage of ujson

--- synapse/storage/group_server.py
+++ synapse/storage/group_server.py
@@ -19,7 +19,7 @@ from synapse.api.errors import SynapseError
 
 from ._base import SQLBaseStore
 
-import ujson as json
+import simplejson as json
 
 
 # The category ID for the "default" category. We don't store as null in the
