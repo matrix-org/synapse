d1e56cfcd11bcd509d8fa3954c00e06a84bddd87
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2018-04-10 00:21:51 +0100

    Fix pep8 error on psycopg2cffi hack

--- synapse/storage/engines/__init__.py
+++ synapse/storage/engines/__init__.py
@@ -32,12 +32,11 @@ def create_engine(database_config):
     engine_class = SUPPORTED_MODULE.get(name, None)
 
     if engine_class:
-        needs_pypy_hack = (name == "psycopg2" and
-                platform.python_implementation() == "PyPy")
-        if needs_pypy_hack:
-            module = importlib.import_module("psycopg2cffi")
-        else:
-            module = importlib.import_module(name)
+        # pypy requires psycopg2cffi rather than psycopg2
+        if (name == "psycopg2" and
+                platform.python_implementation() == "PyPy"):
+            name = "psycopg2cffi"
+        module = importlib.import_module(name)
         return engine_class(module, database_config)
 
     raise RuntimeError(
