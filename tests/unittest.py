16bbd63016c8d853be9dfdc829fa5655e5c84740
Amber Brown <hawkowl@atleastfornow.net>
2018-06-01 21:23:54 +1000

    ensure exceptions appear in Trial's logger.

--- tests/unittest.py
+++ tests/unittest.py
@@ -14,6 +14,8 @@
 # limitations under the License.
 import twisted
 from twisted.trial import unittest
+from synapse.util.logcontext import LoggingContextFilter
+from twisted.logger import Logger, LogLevel
 
 import logging
 
@@ -21,14 +23,26 @@ import logging
 # but since the highest value is 50, 1000000 should do ;)
 NEVER = 1000000
 
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(
-    "%(levelname)s:%(name)s:%(message)s  [%(pathname)s:%(lineno)d]"
-))
-logging.getLogger().addHandler(handler)
-logging.getLogger().setLevel(NEVER)
-logging.getLogger("synapse.storage.SQL").setLevel(NEVER)
-logging.getLogger("synapse.storage.txn").setLevel(NEVER)
+# Set up putting Synapse's logs into Trial's.
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.DEBUG)
+
+log_format = (
+    "%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(request)s"
+    " - %(message)s"
+)
+
+class ToTwistedHandler(logging.Handler):
+    tx_log = Logger()
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tx_log.emit(LogLevel.levelWithName(record.levelname.lower()), log_entry)
+
+handler = ToTwistedHandler()
+formatter = logging.Formatter(log_format)
+handler.setFormatter(formatter)
+handler.addFilter(LoggingContextFilter(request=""))
+rootLogger.addHandler(handler)
 
 
 def around(target):
@@ -61,7 +75,7 @@ class TestCase(unittest.TestCase):
 
         method = getattr(self, methodName)
 
-        level = getattr(method, "loglevel", getattr(self, "loglevel", NEVER))
+        level = getattr(method, "loglevel", getattr(self, "loglevel", logging.ERROR))
 
         @around(self)
         def setUp(orig):
