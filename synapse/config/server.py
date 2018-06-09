b800834351f3f15c9de4996b18149a7e8cae0c34
Vincent Breitmoser <look@my.amazin.horse>
2018-06-09 22:50:29 +0200

    add note that the affinity package is required for the cpu_affinity setting

--- synapse/config/server.py
+++ synapse/config/server.py
@@ -201,6 +201,8 @@ class ServerConfig(Config):
         # different cores. See
         # https://www.mirantis.com/blog/improve-performance-python-programs-restricting-single-cpu/.
         #
+        # This setting requires the affinity package to be installed!
+        #
         # cpu_affinity: 0xFFFFFFFF
 
         # Whether to serve a web client from the HTTP/HTTPS root resource.
