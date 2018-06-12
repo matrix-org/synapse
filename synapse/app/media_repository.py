12285a1a76991f813c71234a14a428192f2c6f93
Travis Ralston <travpc@gmail.com>
2018-06-12 12:51:31 -0600

    The flag is named enable_metrics, not collect_metrics
    Signed-off-by: Travis Ralston <travpc@gmail.com>

--- synapse/app/media_repository.py
+++ synapse/app/media_repository.py
@@ -118,7 +118,7 @@ class MediaRepositoryServer(HomeServer):
             elif listener["type"] == "metrics":
                 if not self.get_config().enable_metrics:
                     logger.warn(("Metrics listener configured, but "
-                                 "collect_metrics is not enabled!"))
+                                 "enable_metrics is not True!"))
                 else:
                     _base.listen_metrics(listener["bind_addresses"],
                                          listener["port"])
