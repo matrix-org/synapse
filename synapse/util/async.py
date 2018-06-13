4411d142e0861a28e87a997a0699a33405641e7d
Amber Brown <hawkowl@atleastfornow.net>
2018-06-14 01:46:37 +1000

    import global reactor here

--- synapse/util/async.py
+++ synapse/util/async.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from twisted.internet import defer
+from twisted.internet import defer, reactor
 from twisted.internet.defer import CancelledError
 from twisted.python import failure
 
