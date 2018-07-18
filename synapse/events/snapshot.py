a20c8a906aca78013118044165ee689beeb68794
Erik Johnston <erik@matrix.org>
2018-07-18 09:48:51 +0100

    isort

--- synapse/events/snapshot.py
+++ synapse/events/snapshot.py
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from frozendict import frozendict
-
 import abc
+
+from frozendict import frozendict
 
 from twisted.internet import defer
 
