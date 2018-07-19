f1a15ea20692e9f4b347cdddf6d714912356ba64
Richard van der Hoff <richard@matrix.org>
2018-07-19 11:14:20 +0100

    revert 00bc979
    ... we've fixed the things that caused the warnings, so we should reinstate the
    warning.

--- synapse/storage/_base.py
+++ synapse/storage/_base.py
@@ -343,10 +343,9 @@ class SQLBaseStore(object):
         """
         parent_context = LoggingContext.current_context()
         if parent_context == LoggingContext.sentinel:
-            # warning disabled for 0.33.0 release; proper fixes will land imminently.
-            # logger.warn(
-            #    "Running db txn from sentinel context: metrics will be lost",
-            # )
+            logger.warn(
+                "Starting db connection from sentinel context: metrics will be lost",
+            )
             parent_context = None
 
         start_time = time.time()
