949d6bfb49b093b30609688deb21a31de7e6414f
Amber Brown <hawkowl@atleastfornow.net>
2018-08-21 06:23:26 +1000

    some comments

--- synapse/storage/_base.py
+++ synapse/storage/_base.py
@@ -1221,23 +1221,34 @@ class _RollbackButIsFineException(Exception):
 
 def db_to_json(db_content):
     """
-    Take some data from a database row and return JSON.
+    Take some data from a database row and return a JSON-decoded object.
+
+    Args:
+        db_content (memoryview|buffer|bytes|bytearray|unicode)
     """
     # psycopg2 on Python 3 returns memoryview objects, which we need to
     # cast to bytes to decode
     if isinstance(db_content, memoryview):
         db_content = db_content.tobytes()
 
+    # psycopg2 on Python 2 returns buffer objects, which we need to cast to
+    # bytes to decode
     if PY2 and isinstance(db_content, buffer):
         db_content = bytes(db_content)
 
+    # Decode it to a Unicode string before feeding it to json.loads, so we
+    # consistenty get a Unicode-containing object out.
     if isinstance(db_content, (bytes, bytearray)):
         db_content = db_content.decode('utf8')
 
     try:
         return json.loads(db_content)
     except Exception:
-
+        # I don't know how, but somehow some binary objects get mangled upon
+        # coming out of PostgreSQL on Python 3. The particular mangling is that
+        # the first byte ("{") is byte-escaped, but the rest is hex-encoded.
+        # This attempts to unmangle it, and warns about it. If it can't
+        # unmangle it, it'll complain and then log the problem.
         try:
             if db_content.startswith("\\x7b"):
                 logging.warning("Detecting mangled JSON, trying to unmangle...")
