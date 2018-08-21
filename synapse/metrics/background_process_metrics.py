3f6762f0bbeb611714ba6c9c4329ea7d5e46cbda
Erik Johnston <erik@matrix.org>
2018-08-21 09:38:38 +0100

    isort

--- synapse/metrics/background_process_metrics.py
+++ synapse/metrics/background_process_metrics.py
@@ -13,8 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import six
 import threading
+
+import six
 
 from prometheus_client.core import REGISTRY, Counter, GaugeMetricFamily
 
