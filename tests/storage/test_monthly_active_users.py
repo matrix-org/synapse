4addb15865aab0d17dd7ef1c3d1f42910be026a3
Amber Brown <hawkowl@atleastfornow.net>
2018-08-30 02:06:54 +1000

    fix

--- tests/storage/test_monthly_active_users.py
+++ tests/storage/test_monthly_active_users.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tests.unittest
-
 from tests.unittest import HomeserverTestCase
 
 FORTY_DAYS = 40 * 24 * 60 * 60
