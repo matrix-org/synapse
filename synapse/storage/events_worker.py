b3b28c429bb03d16974e2be6c472130247a9e882
Amber Brown <hawkowl@atleastfornow.net>
2018-08-29 21:06:23 +1000

    fixes

--- synapse/storage/events_worker.py
+++ synapse/storage/events_worker.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import division
-
 import itertools
 import logging
 from collections import namedtuple
