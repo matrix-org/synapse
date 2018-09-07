3bccef4885e69cdd03a5d2fad4d9909ef7fd281d
Matthew Hodgson <matthew@matrix.org>
2018-09-07 17:23:53 +0100

    fix uninitialised var

--- synapse/storage/state.py
+++ synapse/storage/state.py
@@ -175,6 +175,7 @@ class StateGroupWorkerStore(EventsWorkerStore, SQLBaseStore):
             # Turns out that postgres doesn't like doing a list of OR's and
             # is about 1000x slower, so we just issue a query for each specific
             # type seperately.
+            additional_args = ()
             if types:
                 clause_to_args = [
                     (
@@ -195,6 +196,7 @@ class StateGroupWorkerStore(EventsWorkerStore, SQLBaseStore):
                 # If types is None we fetch all the state, and so just use an
                 # empty where clause with no extra args.
                 clause_to_args = [("", [])]
+
             for where_clause, where_args in clause_to_args:
                 args = [room_id]
                 args.extend(where_args)
