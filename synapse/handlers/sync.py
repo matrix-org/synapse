ed223bdd03fe1ec7270a7d2ee8971e2778ff6f1d
Matthew Hodgson <matthew@matrix.org>
2018-09-12 15:42:38 +0100

    fix NPE

--- synapse/handlers/sync.py
+++ synapse/handlers/sync.py
@@ -568,14 +568,14 @@ class SyncHandler(object):
         # haven't been "deleted" by blatting {} over the top.
         if name_id:
             name = yield self.store.get_event(name_id, allow_none=False)
-            if name.content:
+            if name and name.content:
                 defer.returnValue(summary)
 
         if canonical_alias_id:
             canonical_alias = yield self.store.get_event(
                 canonical_alias_id, allow_none=False,
             )
-            if canonical_alias.content:
+            if canonical_alias and canonical_alias.content:
                 defer.returnValue(summary)
 
         joined_user_ids = [
