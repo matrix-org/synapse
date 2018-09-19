593e197a2da841bd9fab3812334131f805ad47d0
Amber Brown <hawkowl@atleastfornow.net>
2018-09-19 19:01:36 +1000

    fixes

--- test_postgresql.sh
+++ test_postgresql.sh
@@ -1,3 +1,12 @@
-#! /usr/bin/env bash
-docker build . -f Dockerfile-pgtests -t synapsepgtests
+#!/bin/bash
+
+# This script builds the Docker image to run the PostgreSQL tests, and then runs
+# the tests.
+
+set -e
+
+# Build, and tag
+docker build docker/ -f docker/Dockerfile-pgtests -t synapsepgtests
+
+# Run, mounting the current directory into /src
 docker run --rm -it -v $(pwd)\:/src synapsepgtests
