94ae1dea3cf746ba5f26b1ab57f945cb2e734ba1
Erik Johnston <erik@matrix.org>
2018-09-20 17:05:34 +0100

    Add missing logger

--- synapse/metrics/background_process_metrics.py
+++ synapse/metrics/background_process_metrics.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import threading
 
 import six
@@ -22,6 +23,9 @@ from prometheus_client.core import REGISTRY, Counter, GaugeMetricFamily
 from twisted.internet import defer
 
 from synapse.util.logcontext import LoggingContext, PreserveLoggingContext
+
+logger = logging.getLogger(__name__)
+
 
 _background_process_start_count = Counter(
     "synapse_background_process_start_count",
