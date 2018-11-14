d32ccea04ed9550d8c848aa8610e7e878645ec1b
Amber Brown <hawkowl@atleastfornow.net>
2018-11-14 00:26:41 -0600

    fix

--- synapse/rest/media/v1/preview_url_resource.py
+++ synapse/rest/media/v1/preview_url_resource.py
@@ -54,7 +54,7 @@ from ._base import FileInfo
 logger = logging.getLogger(__name__)
 
 _charset_match = re.compile(br"<\s*meta[^>]*charset\s*=\s*([a-z0-9-]+)", flags=re.I)
-_content_type_match = re.compile(r'.*; *charset="?(.*?)"?(;|$)', flags=re.I | re.ASCII)
+_content_type_match = re.compile(r'.*; *charset="?(.*?)"?(;|$)', flags=re.I)
 
 
 class PreviewUrlResource(Resource):
