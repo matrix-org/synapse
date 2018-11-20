5aafeff9e8967fa1af8989ffe6176f0d32a62ba2
Richard van der Hoff <richard@matrix.org>
2018-11-20 11:13:27 +0000

    update comments in config

--- synapse/config/registration.py
+++ synapse/config/registration.py
@@ -93,6 +93,9 @@ class RegistrationConfig(Config):
 
         # The list of identity servers trusted to verify third party
         # identifiers by this server.
+        #
+        # Also defines the ID server which will be called when an account is
+        # deactivated (one will be picked arbitrarily).
         trusted_third_party_id_servers:
             - matrix.org
             - vector.im
