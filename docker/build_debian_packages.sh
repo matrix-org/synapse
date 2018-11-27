6dde62326f28c121506dee42f064db5a5f9d8ee8
Amber Brown <hawkowl@atleastfornow.net>
2018-11-27 19:48:31 +1100

    some cleanups

--- docker/build_debian_packages.sh
+++ docker/build_debian_packages.sh
@@ -5,5 +5,5 @@ mkdir -p ../debs
 docker build --tag dh-venv-builder:bionic --build-arg distro=ubuntu:bionic -f Dockerfile-dhvirtualenv .
 #docker build --tag dh-venv-builder:stretch --build-arg distro=debian:stretch -f Dockerfile-dhvirtualenv .
 
-docker run -it --rm --volume=$(pwd)/../\:/build dh-venv-builder:bionic
+docker run -it --rm --volume=$(pwd)/../\:/build -u $(id -u):$(id -g) dh-venv-builder:bionic
 #ocker run -it --rm --volume=$(pwd)/../\:/build dh-venv-builder:stretch
