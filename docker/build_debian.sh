f76a9191ad0efc236af0c789d04ce3201d6fbc6a
Richard van der Hoff <richard@matrix.org>
2018-12-13 16:01:08 +0000

    chown within the docker container
    so that we don't have to invoke sudo

--- docker/build_debian.sh
+++ docker/build_debian.sh
@@ -32,5 +32,9 @@ dpkg-buildpackage -us -uc
 
 ls -l ..
 
-# copy the build results out
-cp ../*.deb ../*.dsc ../*.tar.xz /debs
+# copy the build results out, setting perms if necessary
+for i in ../*.deb ../*.dsc ../*.tar.xz; do
+    [ -z "$TARGET_USERID" ] || chown "$TARGET_USERID" "$i"
+    [ -z "$TARGET_GROUPID" ] || chgrp "$TARGET_GROUPID" "$i"
+    mv "$i" /debs
+done
