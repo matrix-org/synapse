01b2fce76159afd67bfad35d20b43981f7c11ea3
Richard van der Hoff <richard@matrix.org>
2018-12-14 00:41:41 +0000

    Accept a list of dists on the cmdline

--- docker/build_debian_packages.sh
+++ docker/build_debian_packages.sh
@@ -5,17 +5,25 @@
 # This script builds the Docker images and then executes them sequentially, each
 # one building a Debian package for the targeted operating system. It is
 # designed to be a "single command" to produce all the images.
+#
+# By default, builds for all known distributions, but a list of distributions
+# can be passed on the commandline for debugging.
 
 set -ex
 
 cd `dirname $0`
 
+if [ $# -lt 1 ]; then
+    DISTS=(debian:stretch debian:sid ubuntu:xenial ubuntu:bionic ubuntu:cosmic)
+else
+    DISTS=("$@")
+fi
+
 # Make the dir where the debs will live
 mkdir -p ../debs
 
 # Build each OS image;
-for i in debian:stretch debian:sid ubuntu:xenial ubuntu:bionic ubuntu:cosmic;
-do
+for i in "${DISTS[@]}"; do
     TAG=$(echo ${i} | cut -d ":" -f 2)
     docker build --tag dh-venv-builder:${TAG} --build-arg distro=${i} -f Dockerfile-dhvirtualenv .
     docker run -it --rm --volume=$(pwd)/../\:/synapse/source:ro --volume=$(pwd)/../debs:/debs \
