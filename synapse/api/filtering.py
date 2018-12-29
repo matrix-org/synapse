41d9ed2f53d8ac9a6244fc8702c4ef53b0ea48e6
Michael Telatynski <7t3chguy@gmail.com>
2018-12-29 23:27:08 +0000

    comply with isort

--- synapse/api/filtering.py
+++ synapse/api/filtering.py
@@ -12,8 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import jsonschema
 import six
+
+import jsonschema
 from canonicaljson import json
 from jsonschema import FormatChecker
 
