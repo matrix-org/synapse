e9472d3ec6ebcdbbddc94a8847faa93056dc05dd
Amber Brown <hawkowl@atleastfornow.net>
2019-01-05 01:34:26 +1100

    test

--- synapse/_scripts/delete_user_ip_duplicates.py
+++ synapse/_scripts/delete_user_ip_duplicates.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
