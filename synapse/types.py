2d97bdb12a7aa7d13f20ade2369272903913cb22
Mathias Koehler <mail@mathias.im>
2019-01-05 05:17:23 +0100

    Use generic exception because of fallback
    Signed-off-by: Mathias Koehler <mail@mathias.im>

--- synapse/types.py
+++ synapse/types.py
@@ -165,7 +165,7 @@ class DomainSpecificString(
                 domain = decode_idna(domain)
             else:
                 encode_idna(domain)
-        except (idna.IDNAError, IndexError):
+        except (Exception):
             raise SynapseError(
                 400, "%s got invalid domain name" % (cls.__name__, )
             )
