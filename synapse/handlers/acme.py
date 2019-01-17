fa9b705af70199bc7404ff7bce5609dded82317e
Amber Brown <hawkowl@atleastfornow.net>
2019-01-17 21:53:52 +1100

    try provisioning...?

--- synapse/handlers/acme.py
+++ synapse/handlers/acme.py
@@ -136,7 +136,7 @@ class AcmeHandler(BaseHandler):
             logger.exception("Fail!")
             raise
         logger.warning("Reprovisioned %s, saving.", hostname)
-        cert_chain = self._store.certs[hostname]
+        cert_chain = self._store.certs[hostname.encode('ascii')]
 
         try:
             tls_private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, cert_chain)
