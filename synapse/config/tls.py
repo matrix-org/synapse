4db63ab362f88753b978d0741a602ce673c07426
Amber Brown <hawkowl@atleastfornow.net>
2019-01-22 23:25:31 +1100

    fix

--- synapse/config/tls.py
+++ synapse/config/tls.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import logging
+import os
 from datetime import datetime
 from hashlib import sha256
 
