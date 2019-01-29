b82a76c3845be27f91c53b6770d8ccbad675ca4f
Erik Johnston <erik@matrix.org>
2019-01-29 13:50:59 +0000

    Finish comment...

--- synapse/api/constants.py
+++ synapse/api/constants.py
@@ -104,7 +104,7 @@ class ThirdPartyEntityKind(object):
 class RoomVersions(object):
     V1 = "1"
     V2 = "2"
-    V3 = "3"  # Not currently fully supported, so
+    V3 = "3"  # Not currently fully supported, so we don't add to known versions below
     VDH_TEST = "vdh-test-version"
     STATE_V2_TEST = "state-v2-test"
 
