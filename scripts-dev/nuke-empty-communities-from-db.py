35ad139668727ffcc1436027d60af285664977c5
Werner Sembach <werner.sembach@fau.de>
2019-02-20 12:33:43 +0100

    Reduce maximum line length to conform to pep8

--- scripts-dev/nuke-empty-communities-from-db.py
+++ scripts-dev/nuke-empty-communities-from-db.py
@@ -9,7 +9,8 @@ if len(argv) < 3 or (argv[1] != "sqlite" and argv[1] != "postgresql"):
     print "Usage:"
     print "    " + argv[0] + " sqlite <path_to_db_file>"
     print "or"
-    print "    " + argv[0] + " postgresql <dbname> [-u <user>] [-p <password>] [-h <host>]"
+    print "    " + argv[0] + \
+          " postgresql <dbname> [-u <user>] [-p <password>] [-h <host>]"
     quit()
 
 db = None
@@ -38,23 +39,40 @@ if not db:
 
 with db:
     cur = db.cursor()
-    cur.execute('SELECT g.group_id FROM groups g WHERE (SELECT count(*) from group_users u WHERE g.group_id = u.group_id) = 0;')
+    cur.execute('SELECT g.group_id FROM groups g WHERE (SELECT count(*) '
+                'from group_users u WHERE g.group_id = u.group_id) = 0;')
     groups = cur.fetchall()
     for group in groups:
         group_id = group[0]
         print "Deleting " + group_id
-        cur.execute('DELETE FROM group_users WHERE group_id = \'' + group_id + '\';')  # this is here to avoid a race condition when an empty public community is joined while this script is running
-        cur.execute('DELETE FROM group_invites WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_rooms WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_summary_rooms WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_summary_room_categories WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_room_categories WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_summary_users WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_summary_roles WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_roles WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_attestations_renewals WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM group_attestations_remote WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM local_group_membership WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM local_group_updates WHERE group_id = \'' + group_id + '\';')
-        cur.execute('DELETE FROM groups WHERE group_id = \'' + group_id + '\';')
+        # group_users should be empty. The first statement is here to avoid a race
+        # condition when an empty public community is joined while this script is running.
+        cur.execute('DELETE FROM group_users WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_invites WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_rooms WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_summary_rooms WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_summary_room_categories WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_room_categories WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_summary_users WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_summary_roles WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_roles WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_attestations_renewals WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM group_attestations_remote WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM local_group_membership WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM local_group_updates WHERE group_id = \''
+                    + group_id + '\';')
+        cur.execute('DELETE FROM groups WHERE group_id = \''
+                    + group_id + '\';')
     db.commit()
