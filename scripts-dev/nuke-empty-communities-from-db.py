9648d8c996806c25f4f8cf1ab421c033384a9b5e
Werner Sembach <werner.sembach@fau.de>
2019-02-20 13:38:44 +0100

    Port script to Python 3

--- scripts-dev/nuke-empty-communities-from-db.py
+++ scripts-dev/nuke-empty-communities-from-db.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-from sys import argv
-from getopt import getopt
 import sqlite3
 import psycopg2
+from getopt import getopt
+from sys import argv
 
 if len(argv) < 3 or (argv[1] != "sqlite" and argv[1] != "postgresql"):
-    print "Usage:"
-    print "    " + argv[0] + " sqlite <path_to_db_file>"
-    print "or"
-    print "    " + argv[0] + \
-          " postgresql <dbname> [-u <user>] [-p <password>] [-h <host>]"
+    print("Usage:")
+    print("    " + argv[0] + " sqlite <path_to_db_file>")
+    print("or")
+    print("    " + argv[0] +
+          " postgresql <dbname> [-u <user>] [-p <password>] [-h <host>]")
     quit()
 
 db = None
 if argv[1] == "sqlite":
-    print "Connecting to SQLite: " + argv[2]
+    print("Connecting to SQLite: " + argv[2])
     db = sqlite3.connect(argv[2])
 elif argv[1] == "postgresql":
     db_args = "dbname=" + argv[2]
@@ -27,14 +27,14 @@ elif argv[1] == "postgresql":
             db_args += " password=" + arg
         elif opt == "-h":
             db_args += " host=" + arg
-    print "Connecting to PostgreSQL: " + db_args
+    print("Connecting to PostgreSQL: " + db_args)
     db = psycopg2.connect(db_args)
 else:
-    print "This should be unreachable, report a bug."
+    print("This should be unreachable, report a bug.")
     quit()
 
 if not db:
-    print "Connecting failed"
+    print("Connecting failed")
     quit()
 
 with db:
@@ -44,7 +44,7 @@ with db:
     groups = cur.fetchall()
     for group in groups:
         group_id = group[0]
-        print "Deleting " + group_id
+        print("Deleting " + group_id)
         # group_users should be empty. The first statement is here to avoid a race
         # condition when an empty public community is joined while this script is running.
         cur.execute('DELETE FROM group_users WHERE group_id = \''
