c628aaa8a90bb529aa6b4c7c01bb91a1c385debc
Amber Brown <hawkowl@atleastfornow.net>
2019-02-28 14:11:07 -0800

    cleanup

--- synapse/storage/schema/delta/53/user_share.sql
+++ synapse/storage/schema/delta/53/user_share.sql
@@ -14,35 +14,35 @@
  */
 
 -- Old disused version of the tables below.
-DROP TABLE users_who_share_rooms;
+DROP TABLE IF EXISTS users_who_share_rooms;
 
 -- This is no longer used because it's duplicated by the users_who_share_public_rooms
-DROP TABLE users_in_public_rooms;
+DROP TABLE IF EXISTS users_in_public_rooms;
 
 -- Tables keeping track of what users share rooms. This is a map of local users
 -- to local or remote users, per room. If it is a local user, there will also be
 -- an entry making the user in the same room as themselves. Remote users cannot
 -- be in the user_id column, only the other_user_id column.
 -- There are two sets of tables, those for public rooms and those for private rooms.
-CREATE TABLE users_who_share_public_rooms (
+CREATE TABLE IF NOT EXISTS users_who_share_public_rooms (
     user_id TEXT NOT NULL,
     other_user_id TEXT NOT NULL,
     room_id TEXT NOT NULL
 );
 
-CREATE TABLE users_who_share_private_rooms (
+CREATE TABLE IF NOT EXISTS users_who_share_private_rooms (
     user_id TEXT NOT NULL,
     other_user_id TEXT NOT NULL,
     room_id TEXT NOT NULL
 );
 
-CREATE UNIQUE INDEX users_who_share_public_rooms_u_idx ON users_who_share_public_rooms(user_id, other_user_id, room_id);
-CREATE INDEX users_who_share_public_rooms_r_idx ON users_who_share_public_rooms(room_id);
-CREATE INDEX users_who_share_public_rooms_o_idx ON users_who_share_public_rooms(other_user_id);
+CREATE UNIQUE INDEX IF NOT EXISTS users_who_share_public_rooms_u_idx ON users_who_share_public_rooms(user_id, other_user_id, room_id);
+CREATE INDEX IF NOT EXISTS users_who_share_public_rooms_r_idx ON users_who_share_public_rooms(room_id);
+CREATE INDEX IF NOT EXISTS users_who_share_public_rooms_o_idx ON users_who_share_public_rooms(other_user_id);
 
-CREATE UNIQUE INDEX users_who_share_private_rooms_u_idx ON users_who_share_private_rooms(user_id, other_user_id, room_id);
-CREATE INDEX users_who_share_private_rooms_r_idx ON users_who_share_private_rooms(room_id);
-CREATE INDEX users_who_share_private_rooms_o_idx ON users_who_share_private_rooms(other_user_id);
+CREATE UNIQUE INDEX IF NOT EXISTS users_who_share_private_rooms_u_idx ON users_who_share_private_rooms(user_id, other_user_id, room_id);
+CREATE INDEX IF NOT EXISTS users_who_share_private_rooms_r_idx ON users_who_share_private_rooms(room_id);
+CREATE INDEX IF NOT EXISTS users_who_share_private_rooms_o_idx ON users_who_share_private_rooms(other_user_id);
 
 -- Make sure that we populate the tables initially by resetting the stream ID
 UPDATE user_directory_stream_pos SET stream_id = NULL;
