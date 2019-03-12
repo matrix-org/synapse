91522209852c44d45bf01ee53aff1813f74fa715
Brendan Abolivier <babolivier@matrix.org>
2019-03-12 16:03:49 +0000

    Use helper to register user

--- tests/rest/client/v1/test_login.py
+++ tests/rest/client/v1/test_login.py
@@ -2,7 +2,7 @@ import json
 
 from synapse.api.constants import LoginType
 from synapse.api.errors import HttpResponseException
-from synapse.rest.client.v1 import login
+from synapse.rest.client.v1 import admin, login
 from synapse.rest.client.v2_alpha import register
 
 from tests import unittest
@@ -11,6 +11,7 @@ from tests import unittest
 class LoginRestServletTestCase(unittest.HomeserverTestCase):
 
     servlets = [
+        admin.register_servlets,
         register.register_servlets,
         login.register_servlets,
     ]
@@ -34,7 +35,7 @@ class LoginRestServletTestCase(unittest.HomeserverTestCase):
         # Create different users so we're sure not to be bothered by the per-user
         # ratelimiter.
         for i in range(0, 6):
-            self.create_user("kermit" + str(i))
+            self.register_user("kermit" + str(i), "monkey")
 
         for i in range(0, 6):
             params = {
@@ -75,7 +76,7 @@ class LoginRestServletTestCase(unittest.HomeserverTestCase):
         self.hs.config.rc_login_request_per_user_burst_count = 5
         self.hs.config.rc_login_requests_per_user_per_second = 0.17
 
-        self.create_user("kermit")
+        self.register_user("kermit", "monkey")
 
         for i in range(0, 6):
             params = {
@@ -111,21 +112,3 @@ class LoginRestServletTestCase(unittest.HomeserverTestCase):
         self.render(request)
 
         self.assertEquals(channel.result["code"], b"200", channel.result)
-
-    def create_user(self, localpart):
-        request_data = json.dumps(
-            {
-                "username": localpart,
-                "password": "monkey",
-                "device_id": "frogfone",
-                "auth": {"type": LoginType.DUMMY},
-            }
-        )
-        request_url = b"/_matrix/client/r0/register"
-        request, channel = self.make_request(b"POST", request_url, request_data)
-        self.render(request)
-
-        if channel.code != 200:
-            raise HttpResponseException(
-                channel.code, channel.result["reason"], channel.result["body"]
-            ).to_synapse_error()
