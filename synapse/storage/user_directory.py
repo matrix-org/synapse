8b618041efc230c87c74b912640b8e5727fcc539
Amber Brown <hawkowl@atleastfornow.net>
2019-03-12 18:06:28 +1100

    fixup

--- synapse/storage/user_directory.py
+++ synapse/storage/user_directory.py
@@ -648,7 +648,7 @@ class UserDirectoryStore(BackgroundUpdateStore):
                 ) AS s USING (user_id)
             """
             join_args = (user_id,)
-            where_clause = "p.user_id IS NOT NULL"
+            where_clause = "(s.user_id IS NOT NULL OR p.user_id IS NOT NULL)"
 
         if isinstance(self.database_engine, PostgresEngine):
             full_query, exact_query, prefix_query = _parse_query_postgres(search_term)
