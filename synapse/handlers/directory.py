cc197a61a1b494e5f8a7fbbc299161845f2ab8af
Erik Johnston <erik@matrix.org>
2019-03-20 14:30:36 +0000

    Disable publishing to room list when its disabled

--- synapse/handlers/directory.py
+++ synapse/handlers/directory.py
@@ -44,6 +44,7 @@ class DirectoryHandler(BaseHandler):
         self.appservice_handler = hs.get_application_service_handler()
         self.event_creation_handler = hs.get_event_creation_handler()
         self.config = hs.config
+        self.enable_room_list_search = hs.config.enable_room_list_search
 
         self.federation = hs.get_federation_client()
         hs.get_federation_registry().register_query_handler(
@@ -410,6 +411,13 @@ class DirectoryHandler(BaseHandler):
 
         if visibility not in ["public", "private"]:
             raise SynapseError(400, "Invalid visibility setting")
+
+        if visibility == "public" and not self.enable_room_list_search:
+            # The room list has been disabled.
+            raise AuthError(
+                403,
+                "This user is not permitted to publish rooms to the room list"
+            )
 
         room = yield self.store.get_room(room_id)
         if room is None:
