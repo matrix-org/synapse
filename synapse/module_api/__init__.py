7b28dabff0cb8c1c743ab1d616c16a5a69245db6
Andrew Morgan <andrew@amorgan.xyz>
2019-03-25 19:55:58 +0000

    isort lint

--- synapse/module_api/__init__.py
+++ synapse/module_api/__init__.py
@@ -12,11 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import time
+
 from twisted.internet import defer
 
 from synapse.types import UserID
-
-import time
 
 
 class ModuleApi(object):
