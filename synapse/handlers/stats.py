a79acf5d4a06b630fe6f89788f17144dc8e8675f
Amber Brown <hawkowl@atleastfornow.net>
2019-03-25 22:43:20 +1100

    fix

--- synapse/handlers/stats.py
+++ synapse/handlers/stats.py
@@ -488,3 +488,25 @@ class StatsHandler(StateDeltasHandler):
         logger.debug("Adding new local user to stats, %r", user_id)
 
         yield defer.succeed(1)
+
+    @defer.inlineCallbacks
+    def get_all_rooms(self):
+        """Get all room_ids we've ever known about, in ascending order of "size"
+        """
+        sql = """
+            SELECT room_id FROM current_state_events
+            GROUP BY room_id
+            ORDER BY count(*) ASC
+        """
+        rows = yield self._execute("get_all_rooms", None, sql)
+        defer.returnValue([room_id for room_id, in rows])
+
+    @defer.inlineCallbacks
+    def get_all_local_users(self):
+        """Get all local users
+        """
+        sql = """
+            SELECT name FROM users
+        """
+        rows = yield self._execute("get_all_local_users", None, sql)
+        defer.returnValue([name for name, in rows])
