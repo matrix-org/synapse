f9c5e36aaaa26fc30f1bd8bae3594517acaf49a1
Neil Johnson <neil@fragile.org.uk>
2019-04-02 17:41:18 +0100

    remove presence_list from schema

--- synapse/storage/schema/full_schemas/16/presence.sql
+++ synapse/storage/schema/full_schemas/16/presence.sql
@@ -27,14 +27,3 @@ CREATE TABLE IF NOT EXISTS presence_allow_inbound(
   observer_user_id TEXT NOT NULL, -- a UserID,
   UNIQUE (observed_user_id, observer_user_id)
 );
-
--- For each of /my/ users (watcher), which possibly-remote users are they
--- watching?
-CREATE TABLE IF NOT EXISTS presence_list(
-  user_id TEXT NOT NULL,
-  observed_user_id TEXT NOT NULL, -- a UserID,
-  accepted BOOLEAN NOT NULL,
-  UNIQUE (user_id, observed_user_id)
-);
-
-CREATE INDEX presence_list_user_id ON presence_list (user_id);
