0546e866eb449304083d4b1524d0ea4e1b5d8441
Neil Johnson <neil@fragile.org.uk>
2019-04-02 14:18:33 +0100

    pep8

--- synapse/rest/client/v1/presence.py
+++ synapse/rest/client/v1/presence.py
@@ -92,5 +92,6 @@ class PresenceStatusRestServlet(ClientV1RestServlet):
     def on_OPTIONS(self, request):
         return (200, {})
 
+
 def register_servlets(hs, http_server):
     PresenceStatusRestServlet(hs).register(http_server)
