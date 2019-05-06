1418d4f4acb76a1ab8a8fac7c6e9f3619b58bf9d
Richard van der Hoff <richard@matrix.org>
2019-05-06 21:50:12 +0100

    Remove invalid test

--- tests/rest/admin/test_admin.py
+++ tests/rest/admin/test_admin.py
@@ -37,28 +37,13 @@ class VersionTestCase(unittest.HomeserverTestCase):
     url = '/_matrix/client/r0/admin/server_version'
 
     def test_version_string(self):
-        self.register_user("admin", "pass", admin=True)
-        self.admin_token = self.login("admin", "pass")
-
-        request, channel = self.make_request("GET", self.url,
-                                             access_token=self.admin_token)
+        request, channel = self.make_request("GET", self.url)
         self.render(request)
 
         self.assertEqual(200, int(channel.result["code"]),
                          msg=channel.result["body"])
         self.assertEqual({'server_version', 'python_version'},
                          set(channel.json_body.keys()))
-
-    def test_inaccessible_to_non_admins(self):
-        self.register_user("unprivileged-user", "pass", admin=False)
-        user_token = self.login("unprivileged-user", "pass")
-
-        request, channel = self.make_request("GET", self.url,
-                                             access_token=user_token)
-        self.render(request)
-
-        self.assertEqual(403, int(channel.result['code']),
-                         msg=channel.result['body'])
 
 
 class UserRegisterTestCase(unittest.HomeserverTestCase):
