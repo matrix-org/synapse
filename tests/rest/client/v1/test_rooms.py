8f9ce1a8a22a6299b62eb0d6ab73cb95a4e01d20
Brendan Abolivier <babolivier@matrix.org>
2019-05-16 15:25:54 +0100

    Lint

--- tests/rest/client/v1/test_rooms.py
+++ tests/rest/client/v1/test_rooms.py
@@ -1004,4 +1004,3 @@ class PerRoomProfilesForbiddenTestCase(unittest.HomeserverTestCase):
 
         res_displayname = channel.json_body["content"]["displayname"]
         self.assertEqual(res_displayname, self.displayname, channel.result)
-
