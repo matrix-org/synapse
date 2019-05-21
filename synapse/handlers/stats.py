0f2b24a51649e71fdc5fea34f245c2f92455f310
Amber Brown <hawkowl@atleastfornow.net>
2019-05-20 21:51:06 -0500

    fixes

--- synapse/handlers/stats.py
+++ synapse/handlers/stats.py
@@ -22,7 +22,6 @@ from synapse.handlers.state_deltas import StateDeltasHandler
 from synapse.metrics import event_processing_positions
 from synapse.metrics.background_process_metrics import run_as_background_process
 from synapse.types import UserID
-from synapse.util import logcontext
 from synapse.util.metrics import Measure
 
 logger = logging.getLogger(__name__)
@@ -103,7 +102,6 @@ class StatsHandler(StateDeltasHandler):
                 yield self.store.update_stats_stream_pos(self.pos)
 
                 event_processing_positions.labels("stats").set(self.pos)
-
 
     @defer.inlineCallbacks
     def _handle_deltas(self, deltas):
