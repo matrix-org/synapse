4d31165086b9f51ba46523bfb2320bf08ba14c30
Andrew Morgan <andrew@amorgan.xyz>
2019-05-22 09:30:36 +0100

    Punctuation

--- synapse/api/room_versions.py
+++ synapse/api/room_versions.py
@@ -96,7 +96,7 @@ KNOWN_ROOM_VERSIONS = {
 }   # type: dict[str, RoomVersion]
 
 
-# the version we will give rooms which are created on this server
+# the version we will give rooms which are created on this server.
 # can be overridden by the config
 def get_default_room_version(config):
     return config.default_room_version
