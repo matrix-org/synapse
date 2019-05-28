dac219b3648054f815d3b5c3f25c5f1150d68802
Amber Brown <hawkowl@atleastfornow.net>
2019-05-28 17:26:15 +1000

    update this copyright since this is original

--- synapse/rest/client/v1/_base.py
+++ synapse/rest/client/v1/_base.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2014-2016 OpenMarket Ltd
+# Copyright 2019 Matrix.org Foundation C.I.C
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-This module contains base REST classes for constructing client v1 servlets.
-"""
-
 
 from synapse.rest.client.v2_alpha._base import client_patterns as _client_patterns
 
