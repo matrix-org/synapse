e2c3660a0ffb13d4198893e91a90ae1abcad8915
Erik Johnston <erik@matrix.org>
2019-05-30 14:54:56 +0100

    Add index to temp table

--- synapse/storage/schema/delta/54/delete_forward_extremities.sql
+++ synapse/storage/schema/delta/54/delete_forward_extremities.sql
@@ -20,3 +20,4 @@ INSERT INTO background_updates (update_name, progress_json) VALUES
 
 DROP TABLE IF EXISTS _extremities_to_check;  -- To make this delta schema file idempotent.
 CREATE TABLE _extremities_to_check AS SELECT event_id FROM event_forward_extremities;
+CREATE INDEX _extremities_to_check_id ON _extremities_to_check(event_id);
