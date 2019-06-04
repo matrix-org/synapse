1cc5fc1f6c316e8ea1c50669cd80f4a7d441570a
Brendan Abolivier <babolivier@matrix.org>
2019-06-04 13:51:23 +0100

    Lint

--- synapse/config/emailconfig.py
+++ synapse/config/emailconfig.py
@@ -138,7 +138,6 @@ class EmailConfig(Config):
                 if not os.path.isfile(p):
                     raise ConfigError("Unable to find email template file %s" % (p, ))
 
-
     def default_config(self, config_dir_path, server_name, **kwargs):
         return """
         # Enable sending emails for notification events or expiry notices
