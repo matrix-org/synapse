f194bf7af6f34b1d0dc97758e26cf436d713aeae
Andrew Morgan <andrew@amorgan.xyz>
2019-06-05 12:51:31 +0100

    Add comma

--- synapse/storage/registration.py
+++ synapse/storage/registration.py
@@ -1025,7 +1025,7 @@ class RegistrationStore(
         def get_threepid_validation_session_txn(txn):
             sql = "SELECT %s FROM threepid_validation_session WHERE %s" % (
                 ", ".join(cols_to_return),
-                " AND ".join("%s = ?" % k for k in iterkeys(keyvalues))
+                " AND ".join("%s = ?" % k for k in iterkeys(keyvalues)),
             )
 
             if validated is not None:
