c9573ca0691b15a93cec065804b0f6cc887e9301
Andrew Morgan <andrew@amorgan.xyz>
2019-06-05 15:13:03 +0100

    Change template_dir to originate from synapse root dir

--- synapse/config/emailconfig.py
+++ synapse/config/emailconfig.py
@@ -64,7 +64,7 @@ class EmailConfig(Config):
         # work for the same reason.)
         if not template_dir:
             template_dir = pkg_resources.resource_filename(
-                'synapse', 'res/templates'
+                'synapse', '../res/templates'
             )
 
         self.email_template_dir = os.path.abspath(template_dir)
