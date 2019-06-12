f6af7c2dbc9bae5b67b9734f59eab58402722d74
Brendan Abolivier <babolivier@matrix.org>
2019-06-12 16:12:32 +0100

    Lint

--- tests/rest/client/v2_alpha/test_account.py
+++ tests/rest/client/v2_alpha/test_account.py
@@ -15,9 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import os
 import re
-import json
 from email.parser import Parser
 
 import pkg_resources
