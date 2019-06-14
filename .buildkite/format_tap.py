3b96adcde33a64766c169652c3970cb1a88d456f
Amber H. Brown <hawkowl@atleastfornow.net>
2019-06-15 04:36:12 +1000

    make tap rendering nicer

--- .buildkite/format_tap.py
+++ .buildkite/format_tap.py
@@ -10,17 +10,23 @@ in_error = False
 
 for line in p.parse_file(sys.argv[1]):
     if isinstance(line, Result):
+        if in_error:
+            out.append("</pre></code></details>")
+            out.append("")
+            out.append("----")
+            out.append("")
         in_error = False
 
         if not line.ok and not line.todo:
             in_error = True
 
-            out.append("- FAILURE Test #%d: %s" % (line.number, line.description))
+            out.append("FAILURE Test #%d: ``%s``" % (line.number, line.description))
             out.append("")
+            out.append("<details><summary>Show log</summary><code><pre>")
 
     elif isinstance(line, Diagnostic) and in_error:
-        out.append((" " * 7) + line.text)
+        out.append(line.text)
 
-
-for line in out:
-    print(line)
+if out:
+    for line in out[:-3]:
+        print(line)
