0009fdd3adbcd52ef5628800e3f8a04ca4c9b9d2
Jorik Schellekens <joriks@matrix.org>
2019-06-25 14:19:39 +0100

    Copyright

--- synapse/config/tracer.py
+++ synapse/config/tracer.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 New Vector Ltd
+# Copyright 2019 The Matrix.org Foundation C.I.C.d
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
