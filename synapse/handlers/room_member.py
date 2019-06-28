7c644198c8562c3d872d178be5f3520e7d8d01bc
Brendan Abolivier <babolivier@matrix.org>
2019-06-28 15:17:15 +0100

    Don't update the ratelimiter before sending a 3PID invite
    This would cause emails being sent, but Synapse responding with a 429 when creating the event. The client would then retry, and with bad timing the same scenario would happen again. Some testing I did ended up sending me 10 emails for one single invite because of this.

--- synapse/handlers/room_member.py
+++ synapse/handlers/room_member.py
@@ -773,7 +773,7 @@ class RoomMemberHandler(object):
 
         # We need to rate limit *before* we send out any 3PID invites, so we
         # can't just rely on the standard ratelimiting of events.
-        yield self.base_handler.ratelimit(requester)
+        yield self.base_handler.ratelimit(requester, update=False)
 
         can_invite = yield self.third_party_event_rules.check_threepid_can_be_invited(
             medium, address, room_id,
