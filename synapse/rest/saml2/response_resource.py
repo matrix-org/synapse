f40a7dc41fdd738a74546ff22b110a4c8ab850fe
Amber Brown <hawkowl@atleastfornow.net>
2019-06-29 17:06:55 +1000

    Make the http server handle coroutine-making REST servlets (#5475)

--- synapse/rest/saml2/response_resource.py
+++ synapse/rest/saml2/response_resource.py
@@ -18,34 +18,27 @@ import logging
 import saml2
 from saml2.client import Saml2Client
 
-from twisted.web.resource import Resource
-from twisted.web.server import NOT_DONE_YET
-
 from synapse.api.errors import CodeMessageException
-from synapse.http.server import wrap_html_request_handler
+from synapse.http.server import DirectServeResource, wrap_html_request_handler
 from synapse.http.servlet import parse_string
 from synapse.rest.client.v1.login import SSOAuthHandler
 
 logger = logging.getLogger(__name__)
 
 
-class SAML2ResponseResource(Resource):
+class SAML2ResponseResource(DirectServeResource):
     """A Twisted web resource which handles the SAML response"""
 
     isLeaf = 1
 
     def __init__(self, hs):
-        Resource.__init__(self)
+        super().__init__()
 
         self._saml_client = Saml2Client(hs.config.saml2_sp_config)
         self._sso_auth_handler = SSOAuthHandler(hs)
 
-    def render_POST(self, request):
-        self._async_render_POST(request)
-        return NOT_DONE_YET
-
     @wrap_html_request_handler
-    def _async_render_POST(self, request):
+    async def _async_render_POST(self, request):
         resp_bytes = parse_string(request, "SAMLResponse", required=True)
         relay_state = parse_string(request, "RelayState", required=True)
 
