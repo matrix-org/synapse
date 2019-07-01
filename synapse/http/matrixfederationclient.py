30ed28417eee255e4f7b18166a8407cbced9cfca
Amber H. Brown <hawkowl@atleastfornow.net>
2019-07-01 21:47:17 +1000

    make logging a little better

--- synapse/http/matrixfederationclient.py
+++ synapse/http/matrixfederationclient.py
@@ -523,7 +523,7 @@ class MatrixFederationHttpClient(object):
                     break
                 except RequestSendFailed as e:
                     logger.warn(
-                        "{%s} [%s] Request failed, %s - %s: %s %s: %s",
+                        "{%s} [%s] Request failed, %s: %s %s: %s",
                         request.txn_id,
                         request.destination,
                         "retrying" if e.can_retry else "not retrying",
