965427760bcc68804c098f2edd6663e3d3e24c4e
Jorik Schellekens <joriks@matrix.org>
2019-07-02 09:05:05 +0100

    Typoed

--- synapse/util/scopecontextmanager.py
+++ synapse/util/scopecontextmanager.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Matrix.org Foundation C.I.C.d
+# Copyright 2019 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
