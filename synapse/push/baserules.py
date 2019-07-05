dd4617d059a1e94086a61d41bd5b77c53d71af48
J. Ryan Stinnett <jryans@gmail.com>
2019-07-05 15:06:48 +0100

    Update copyright header

--- synapse/push/baserules.py
+++ synapse/push/baserules.py
@@ -1,5 +1,6 @@
 # Copyright 2015, 2016 OpenMarket Ltd
 # Copyright 2017 New Vector Ltd
+# Copyright 2019 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
