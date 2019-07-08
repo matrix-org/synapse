122af1a3cb865139c182d9396b3b4fde82bb0896
Amber H. Brown <hawkowl@atleastfornow.net>
2019-07-09 01:14:24 +1000

    fix old prometheus client

--- synapse/metrics/_exposition.py
+++ synapse/metrics/_exposition.py
@@ -14,17 +14,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 from urllib.parse import parse_qs, urlparse
 
-from prometheus_client.registry import REGISTRY
-from prometheus_client.utils import floatToGoString
+from prometheus_client import REGISTRY
 
 from twisted.web.resource import Resource
 
 CONTENT_TYPE_LATEST = str("text/plain; version=0.0.4; charset=utf-8")
+
+
+INF = float("inf")
+MINUS_INF = float("-inf")
+
+
+def floatToGoString(d):
+    d = float(d)
+    if d == INF:
+        return "+Inf"
+    elif d == MINUS_INF:
+        return "-Inf"
+    elif math.isnan(d):
+        return "NaN"
+    else:
+        s = repr(d)
+        dot = s.find(".")
+        # Go switches to exponents sooner than Python.
+        # We only need to care about positive values for le/quantile.
+        if d > 0 and dot > 6:
+            mantissa = "{0}.{1}{2}".format(s[0], s[1:dot], s[dot + 1 :]).rstrip("0.")
+            return "{0}e+0{1}".format(mantissa, dot - 1)
+        return s
 
 
 def sample_line(line, name):
