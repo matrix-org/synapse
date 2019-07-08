e16b9b164f808ef7fb8f393a68efbc6015c138f3
Andrew Morgan <andrew@amorgan.xyz>
2019-07-08 10:14:12 +0100

    Explain script, restrict black run dirs

--- scripts-dev/lint.sh
+++ scripts-dev/lint.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+# Runs linting scripts over the local Synapse checkout
+# isort - sorts import statements
+# flake8 - lints and finds mistakes
+# black - opinionated code formatter
+
 isort -y -rc synapse tests scripts-dev scripts
 flake8 synapse tests
-python3 -m black .
+python3 -m black synapse tests scripts-dev scripts
