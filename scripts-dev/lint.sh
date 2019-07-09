f4dc17d765eabce7de47b523b4fedea5311d5f23
Andrew Morgan <andrew@amorgan.xyz>
2019-07-09 18:17:38 +0100

    move set -e to correct location

--- scripts-dev/lint.sh
+++ scripts-dev/lint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
-set -e
-
+#
 # Runs linting scripts over the local Synapse checkout
 # isort - sorts import statements
 # flake8 - lints and finds mistakes
 # black - opinionated code formatter
+
+set -e
 
 isort -y -rc synapse tests scripts-dev scripts
 flake8 synapse tests
