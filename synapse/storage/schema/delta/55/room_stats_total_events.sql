df3fafa827ed439c15ee7a42d1d155e5891fd7fc
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2019-07-18 14:42:07 +0100

    Introduce `total_events` column in `room_stats`.
    Signed-off-by: Olivier Wilkinson (reivilibre) <olivier@librepush.net>

---
+++ synapse/storage/schema/delta/55/room_stats_total_events.sql
@@ -0,0 +1,17 @@
+/* Copyright 2019 The Matrix.org Foundation C.I.C.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- the total number of events stored for this room at time `ts`
+ALTER TABLE room_stats ADD COLUMN total_events INT;
