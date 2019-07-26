1afd06a02abab0744839b5daa48be3844ea5fff2
Andrew Morgan <andrew@amorgan.xyz>
2019-07-26 13:27:32 +0100

    Add changelog and lint

--- synapse/rest/client/v2_alpha/sync.py
+++ synapse/rest/client/v2_alpha/sync.py
@@ -138,7 +138,9 @@ class SyncRestServlet(RestServlet):
                 except StoreError as err:
                     if err.code == 404 and err.errcode == Codes.UNKNOWN:
                         # fix up the description and errcode to be more useful
-                        raise SynapseError(404, "No such filter", errcode=Codes.NOT_FOUND)
+                        raise SynapseError(
+                            404, "No such filter", errcode=Codes.NOT_FOUND
+                        )
                     else:
                         raise
         else:
