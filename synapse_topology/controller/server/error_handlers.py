d2d36126f73ab97cd3d98e2cda03cd478b93f533
Jorik Schellekens <joriks@matrix.org>
2019-07-30 19:06:54 +0100

    Set up initial endpoints for backend server.

---
+++ synapse_topology/controller/server/error_handlers.py
@@ -0,0 +1,47 @@
+from jsonschema import ValidationError
+from simplejson.errors import JSONDecodeError
+from synapse_topology.model.errors import (
+    BasConfigInUseError,
+    BaseConfigNotFoundError,
+    ConfigNotFoundError,
+)
+
+from . import server
+
+app = server.app
+
+
+@app.handle_errors(ValidationError)
+def validation_error(request, failure):
+    request.setResponseCode(400)
+    return "Invalid post schema {}".format(failure.getErrorMessage())
+
+
+@app.handle_errors(JSONDecodeError)
+def json_decode_error(request, failure):
+    request.setResponseCode(400)
+    return "Invalid post json"
+
+
+@app.handle_errors(BaseConfigNotFoundError)
+def base_config_not_found(request, failure):
+    request.setResponseCode(500)
+    return "Config file not setup, please initialise it using the /servername endpoint"
+
+
+@app.handle_errors(ConfigNotFoundError)
+def config_not_found(request, failure):
+    request.setResponseCode(404)
+    return "The config does not exist"
+
+
+@app.handle_errors(BasConfigInUseError)
+def base_config_in_use(request, failure):
+    request.setResponseCode(409)
+    return "Sever name and keys already configured"
+
+
+@app.handle_errors(Exception)
+def handle_generic_error(request, failure):
+    request.setResponseCode(500)
+    return "Internal server error\n{}".format(failure)
