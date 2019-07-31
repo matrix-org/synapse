781ade836b05b7e327baa7f927c553941edcc368
Hubert Chathi <hubert@uhoreg.ca>
2019-07-30 23:09:50 -0400

    apply changes from PR review

--- synapse/storage/schema/delta/56/hidden_devices.sql
+++ synapse/storage/schema/delta/56/hidden_devices.sql
@@ -15,4 +15,4 @@
 
 -- device list needs to know which ones are "real" devices, and which ones are
 -- just used to avoid collisions
-ALTER TABLE devices ADD COLUMN hidden BOOLEAN NULLABLE;
+ALTER TABLE devices ADD COLUMN hidden BOOLEAN DEFAULT FALSE;
