7d0549093d60acf4e2c0b13cb2fe5441d65c11fd
Jorik Schellekens <joriks@matrix.org>
2019-08-05 18:42:17 +0100

    Selecting ports for delegation.

--- synapse_topology/view/webui/js/components/DelegationPortSelection.jsx
+++ synapse_topology/view/webui/js/components/DelegationPortSelection.jsx
@@ -2,48 +2,75 @@ import React, { useState } from 'react';
 
 import ContentWrapper from './ContentWrapper';
 
-export default ({ onClick }) => {
-  const [delegationPort, setDelegationPort] = useState("");
-  const [validInput, setValidInput] = useState(true);
+import style from '../../less/main.less';
 
-  const onChange = event => {
+export default ({ onClick }) => {
+  const [fedPort, setFedPort] = useState("");
+  const [clientPort, setClientPort] = useState("");
+  const [clientPortValid, setClientPortValid] = useState(true)
+  const [fedPortValid, setFedPortValid] = useState(true)
+
+  const updateValidity = (port, setValid) => setValid(
+    !port ||
+    (!isNaN(port) && 0 < port && port <= 65535)
+  )
+
+  const onFederationChange = event => {
     const val = event.target.value;
-    setValidInput(!isNaN(event.target.value) && 0 < val && val < 65535);
-    setDelegationPort(val);
+    setFedPort(val);
+    updateValidity(val, setFedPortValid);
+  }
+
+  const onClientChange = event => {
+    const val = event.target.value;
+    setClientPort(val);
+    updateValidity(val, setClientPortValid);
   }
 
   return <ContentWrapper>
     <h1>Outward facing port selection</h1>
     <p>
-      Normally other servers will try to contact the Synapse install's server on
+      Normally other matrix servers will try to contact the Synapse install's server on
       port 8448 and clients, such as riot, riotX, neo etc., will try to contact
       the install server on port 443.
     </p>
     <p>
       Delegation let's us tell those servers and clients to try a different port!
       (Flexible!)
-      However, we can only specify one port. That one port will be used for both
-      the servers and the clients.
     </p>
     <p>
       It's perfectly fine to leave the defaults. Only change them if you have a
       real need to.
     </p>
     <p>
-      I would recommend an unprivileged port but I would recommend the default ports
-      more strongly.
+      I would recommend using unprivileged ports but I would recommend the
+      default ports more strongly.
+    </p>
+    <p>
+      Please choose the port for other matrix servers to contact:
     </p>
     <input
       type="text"
-      onChange={onChange}
+      onChange={onFederationChange}
+      className={fedPortValid ? undefined : style.invalidInput}
       autoFocus
-      placeholder="Use Defaults"
+      placeholder="Use Default 8448"
+    ></input>
+    <p>
+      Please choose the port for clients to contact:
+    </p>
+    <input
+      type="text"
+      onChange={onClientChange}
+      className={clientPortValid ? undefined : style.invalidInput}
+      autoFocus
+      placeholder="Use Default 443"
     ></input>
     <div>
       <button
-        disabled={!delegationPort || validInput ? undefined : true}
-        onClick={() => onClick(delegationPort)}
-      >Use {delegationPort ? delegationPort : "default ports"}</button>
+        disabled={clientPortValid && fedPortValid ? undefined : true}
+        onClick={() => onClick(fedPort, clientPort)}
+      >Use These Ports</button>
     </div>
   </ContentWrapper>
 }
