d599ab6249d9541a28eb599e54bcad7d770c3caf
Jorik Schellekens <joriks@matrix.org>
2019-08-06 12:04:28 +0100

    Matrix branding

--- synapse_topology/view/webui/js/index.jsx
+++ synapse_topology/view/webui/js/index.jsx
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import UI from './containers/UI';
+import style from '../less/main.less';
+import logo from '../fonts/matrix-logo.svg';
 
 import { startup } from './actions';
 
@@ -18,6 +20,7 @@ store.dispatch(startup());
 
 render(
   <Provider store={store}>
+    <img className={style.logo} src={logo} />
     <UI />
   </Provider>,
   document.getElementById("content")
