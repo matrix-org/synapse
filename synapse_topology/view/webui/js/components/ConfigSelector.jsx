47ca66431114ee742b6b66f775137ab7f0e50d8e
Jorik Schellekens <joriks@matrix.org>
2019-08-12 11:57:23 +0100

    Missing import

--- synapse_topology/view/webui/js/components/ConfigSelector.jsx
+++ synapse_topology/view/webui/js/components/ConfigSelector.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
-
-import style from '../../less/main.less';
+import ContentWrapper from '../containers/ContentWrapper';
 
 export default () => {
   return <ContentWrapper>
     <h1>Config selection</h1>
     <p>The base config has already been setup. Please select a config to edit:</p>
     <p>TODO: .. well .. this.</p>
-    <div>
-      <button />
-    </div>
   </ContentWrapper>;
 }
