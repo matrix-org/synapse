d8a19849e86d053864e1f2cc4c917c6e4bae93d4
Jorik Schellekens <joriks@matrix.org>
2019-08-16 17:04:25 +0100

    Clickable

--- synapse_topology/webui/src/js/components/AccordionToggle.jsx
+++ synapse_topology/webui/src/js/components/AccordionToggle.jsx
@@ -4,11 +4,11 @@ import { reset } from 'ansi-colors';
 import Chevron from './Chevron';
 
 export default ({ active, open, children, eventKey, as, reset }) => {
-
+    const clickable = active & !open;
     const toggle = useAccordionToggle(eventKey);
     const decoratedOnClick = () => {
 
-        if (active) {
+        if (clickable) {
 
             toggle();
             reset();
@@ -17,7 +17,7 @@ export default ({ active, open, children, eventKey, as, reset }) => {
 
     }
     const As = as;
-    return <div className={active ? "active-card-header" : "inactive-card-header"}>
+    return <div className={clickable ? "active-card-header" : "inactive-card-header"}>
         <As onClick={decoratedOnClick}> {children} <Chevron open={open} /></As>
     </div>
 
