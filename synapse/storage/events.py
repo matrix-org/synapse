cb50616b6b028d612f9a4c6a838ecbf6aec1f597
Richard van der Hoff <richard@matrix.org>
2019-08-20 16:49:51 +0100

    purge some more tables

--- synapse/storage/events.py
+++ synapse/storage/events.py
@@ -2244,8 +2244,12 @@ class EventsStore(
         for table in (
             "event_auth",
             "event_edges",
+            "event_push_actions_staging",
             "event_reference_hashes",
+            "event_relations",
             "event_to_state_groups",
+            "group_rooms",
+            "redactions",
             "rejections",
             "state_events",
         ):
@@ -2261,7 +2265,7 @@ class EventsStore(
                 (room_id,),
             )
 
-        # and finally, the tables with an index on room_id
+        # and finally, the tables with an index on room_id (or no useful index)
         for table in (
             "current_state_events",
             "event_backward_extremities",
@@ -2270,14 +2274,58 @@ class EventsStore(
             "event_push_actions",
             "event_search",
             "events",
+            "group_rooms",
+            "public_room_list_stream",
             "receipts_graph",
             "receipts_linearized",
+            "room_aliases",
             "room_depth",
             "room_memberships",
+            "room_state",
+            "room_stats",
+            "room_stats_earliest_token",
             "rooms",
+            "stream_ordering_to_exterm",
+            "topics",
+            "users_in_public_rooms",
+            "users_who_share_private_rooms",
+            # no useful index, but let's clear them anyway
+            "appservice_room_list",
+            "e2e_room_keys",
+            "event_push_summary",
+            "pusher_throttle",
+            "group_summary_rooms",
+            "local_invites",
+            "room_account_data",
+            "room_tags",
         ):
             logger.info("[purge] removing %s from %s", room_id, table)
             txn.execute("DELETE FROM %s WHERE room_id=?" % (table,), (room_id,))
+
+        # Other tables we do NOT need to clear out:
+        #
+        #  - blocked_rooms
+        #    This is important, to make sure that we don't accidentally rejoin a blocked
+        #    room after it was purged
+        #
+        #  - user_directory
+        #    This has a room_id column, but it is unused
+        #
+
+        # Other tables that we might want to consider clearing out include:
+        #
+        #  - event_reports
+        #       Given that these are intended for abuse management my initial
+        #       inclination is to leave them in place.
+        #
+        #  - current_state_delta_stream
+        #  - ex_outlier_stream
+        #  - room_tags_revisions
+        #       The problem with these is that they are largeish and there is no room_id
+        #       index on them. In any case we should be clearing out 'stream' tables
+        #       periodically anyway (#5888)
+
+        # TODO: we could probably usefully do a bunch of cache invalidation here
 
         logger.info("[purge] done")
 
