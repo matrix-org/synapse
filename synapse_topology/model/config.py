aed01c8b939787bdd1200897cdc5b0cc4962ad6f
Jorik Schellekens <joriks@matrix.org>
2019-08-21 16:49:18 +0100

    Tls config

--- synapse_topology/model/config.py
+++ synapse_topology/model/config.py
@@ -1,15 +1,21 @@
 from synapse.config.database import DatabaseConfig
 from synapse.config.server import ServerConfig
+from synapse.config.tls import TlsConfig
+from synapse.config.logger import LoggingConfig
 from model import get_config_dir, get_data_dir, set_config_dir
 
 
 def create_config(conf):
+    server_name = conf["server_name"]
+    del conf["server_name"]
     server = ServerConfig().generate_config_section(
-        conf["server_name"], get_data_dir(), False, conf["listeners"]
+        server_name, get_data_dir(), False, **conf
     )
     database = DatabaseConfig().generate_config_section(get_data_dir(), **conf)
-
-    return "\n\n".join([server, database])
+    tls = TlsConfig().generate_config_section(
+        get_config_dir(), server_name, get_data_dir(), **conf
+    )
+    return "\n\n".join([server, database, tls])
 
 
 set_config_dir("/exampledir/")
@@ -32,6 +38,9 @@ print(
                     "type": "http",
                 },
             ],
+            "tls_certificate_path": "asdfasfdasdfadf",
+            "tls_private_key_path": "asdfasfdha;kdfjhafd",
+            "acme_domain": "asdfaklhsadfkj",
         }
     )
 )
