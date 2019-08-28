7dc398586c2156a456d9526ac0e42c1fec9f8143
Amber Brown <hawkowl@atleastfornow.net>
2019-08-28 21:18:53 +1000

    Implement a structured logging output system. (#5680)

--- synapse/app/appservice.py
+++ synapse/app/appservice.py
@@ -141,8 +141,6 @@ def start(config_options):
 
     assert config.worker_app == "synapse.app.appservice"
 
-    setup_logging(config, use_worker_options=True)
-
     events.USE_FROZEN_DICTS = config.use_frozen_dicts
 
     database_engine = create_engine(config.database_config)
@@ -166,6 +164,8 @@ def start(config_options):
         version_string="Synapse/" + get_version_string(synapse),
         database_engine=database_engine,
     )
+
+    setup_logging(ps, config, use_worker_options=True)
 
     ps.setup()
     reactor.addSystemEventTrigger(
