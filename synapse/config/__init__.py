d1ec76f10d51fd40645305a6643953ae27382233
Jorik Schellekens <joriksch@gmail.com>
2019-08-28 15:18:40 +0100

    Misleading comment

--- synapse/config/__init__.py
+++ synapse/config/__init__.py
@@ -15,7 +15,7 @@
 
 from ._base import ConfigError, find_config_files
 
-# export ConfigError, find_config_files, read_config_files if somebody does
+# export ConfigError and find_config_files if somebody does
 # import *
 # this is largely a fudge to stop PEP8 moaning about the import
 __all__ = ["ConfigError", "find_config_files"]
