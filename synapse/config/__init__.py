765d1f13d2b183dbd36c7d441741c0ab4719df14
Jorik Schellekens <joriks@matrix.org>
2019-08-28 10:08:51 +0100

    Explicitely read yaml for clarity.

--- synapse/config/__init__.py
+++ synapse/config/__init__.py
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ._base import ConfigError, find_config_files, read_config_files
+from ._base import ConfigError, find_config_files
 
 # export ConfigError, find_config_files, read_config_files if somebody does
 # import *
 # this is largely a fudge to stop PEP8 moaning about the import
-__all__ = ["ConfigError", "find_config_files", "read_config_files"]
+__all__ = ["ConfigError", "find_config_files"]
