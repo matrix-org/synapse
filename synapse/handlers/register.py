a98b8583c647d841b49b94a3de030b9e8d5271c0
Andrew Morgan <andrew@amorgan.xyz>
2019-09-03 19:58:51 +0100

    Remove unnecessary variable declaration

--- synapse/handlers/register.py
+++ synapse/handlers/register.py
@@ -397,7 +397,6 @@ class RegistrationHandler(BaseHandler):
 
     @defer.inlineCallbacks
     def _join_user_to_room(self, requester, room_identifier):
-        room_id = None
         room_member_handler = self.hs.get_room_member_handler()
         if RoomID.is_valid(room_identifier):
             room_id = room_identifier
