67910600edf7d2b3de01efbfbf48604e29388235
Amber H. Brown <hawkowl@atleastfornow.net>
2019-09-06 23:22:32 +1000

    don't idna decode if we're simply looking for an ip

--- synapse/crypto/context_factory.py
+++ synapse/crypto/context_factory.py
@@ -198,7 +198,7 @@ class ConnectionVerifier(object):
     def __init__(self, hostname: bytes, verify_certs):
         self._verify_certs = verify_certs
 
-        _decoded = idna.decode(hostname)
+        _decoded = hostname.decode('ascii')
         if isIPAddress(_decoded) or isIPv6Address(_decoded):
             self._is_ip_address = True
         else:
