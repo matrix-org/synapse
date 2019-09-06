27eac52813e0dc56aadb063b2993c1c2552d0bab
Travis Ralston <travpc@gmail.com>
2019-09-05 20:41:54 -0600

    import the important things in life

--- synapse/rest/client/v2_alpha/receipts.py
+++ synapse/rest/client/v2_alpha/receipts.py
@@ -18,7 +18,7 @@ import logging
 from twisted.internet import defer
 
 from synapse.api.errors import SynapseError
-from synapse.http.servlet import RestServlet
+from synapse.http.servlet import RestServlet, parse_json_object_from_request
 
 from ._base import client_patterns
 
