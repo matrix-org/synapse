976c1ce5f7386225ecbb0fcd7f30138f55750c49
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2019-09-17 12:23:59 +0100

    Update synapse/handlers/stats.py

--- synapse/handlers/stats.py
+++ synapse/handlers/stats.py
@@ -84,6 +84,9 @@ class StatsHandler(StateDeltasHandler):
         # Loop round handling deltas until we're up to date
 
         while True:
+            # Be sure to read the max stream_ordering *before* checking if there are any outstanding
+            # deltas, since there is otherwise a chance that we could miss updates which arrive
+            # after we check the deltas.
             room_max_stream_ordering = yield self.store.get_room_max_stream_ordering()
             if self.pos == room_max_stream_ordering:
                 break
