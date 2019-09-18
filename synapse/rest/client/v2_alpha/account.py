ea93a211cf8659422bdae2333a73ef28bf0646bb
Andrew Morgan <andrew@amorgan.xyz>
2019-09-18 17:56:10 +0100

    Remove POST method instead

--- synapse/rest/client/v2_alpha/account.py
+++ synapse/rest/client/v2_alpha/account.py
@@ -272,28 +272,6 @@ class PasswordResetSubmitTokenServlet(RestServlet):
         request.write(html.encode("utf-8"))
         finish_request(request)
 
-    @defer.inlineCallbacks
-    def on_POST(self, request, medium):
-        if medium != "email":
-            raise SynapseError(
-                400, "This medium is currently not supported for password resets"
-            )
-
-        body = parse_json_object_from_request(request)
-        assert_params_in_dict(body, ["sid", "client_secret", "token"])
-
-        try:
-            yield self.store.validate_threepid_session(
-                body["sid"],
-                body["client_secret"],
-                body["token"],
-                self.clock.time_msec(),
-            )
-            return 200, {"success": True}
-        except ThreepidValidationError:
-            # Validation failure
-            return 400, {"success": False}
-
 
 class PasswordRestServlet(RestServlet):
     PATTERNS = client_patterns("/account/password$")
