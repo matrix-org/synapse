af92110c465ea7cf4d04e1193b58f16ae26a75d6
Brendan Abolivier <babolivier@matrix.org>
2019-09-27 16:12:15 +0100

    Update synapse/handlers/deactivate_account.py
    Co-Authored-By: Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>

--- synapse/handlers/deactivate_account.py
+++ synapse/handlers/deactivate_account.py
@@ -156,7 +156,9 @@ class DeactivateAccountHandler(BaseHandler):
                     require_consent=False,
                 )
                 logger.info(
-                    "Rejected invite for user %r in room %r", user_id, room.room_id
+                    "Rejected invite for deactivated user %r in room %r",
+                    user_id,
+                    room.room_id,
                 )
             except Exception:
                 logger.exception(
