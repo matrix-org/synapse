bf4e01a2bb6d0dc00a13d2c227ae1e92d46727aa
Andrew Morgan <andrew@amorgan.xyz>
2019-10-02 16:51:48 +0100

    lint

--- synapse/handlers/room_member.py
+++ synapse/handlers/room_member.py
@@ -459,7 +459,9 @@ class RoomMemberHandler(object):
                 return remote_join_response
 
             # Copy over user state if this is a join on an local upgraded room
-            yield self.copy_user_state_on_room_upgrade(room_id, requester.user.to_string())
+            yield self.copy_user_state_on_room_upgrade(
+                room_id, requester.user.to_string()
+            )
 
         elif effective_membership_state == Membership.LEAVE:
             if not is_host_in_room:
