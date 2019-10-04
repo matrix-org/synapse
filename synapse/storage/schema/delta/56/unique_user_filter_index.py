6f7ce598661cc0a97ea36dacda25617d18427d61
Richard van der Hoff <richard@matrix.org>
2019-10-04 09:48:59 +0100

    lint

--- synapse/storage/schema/delta/56/unique_user_filter_index.py
+++ synapse/storage/schema/delta/56/unique_user_filter_index.py
@@ -1,23 +1,24 @@
-from synapse.storage.engines import PostgresEngine
-
 import logging
+
+from synapse.storage.engines import PostgresEngine
 
 logger = logging.getLogger(__name__)
 
 
 def run_upgrade(cur, database_engine, *args, **kwargs):
     if isinstance(database_engine, PostgresEngine):
-        select_clause = '''
+        select_clause = """
         CREATE TABLE user_filters_migration AS
             SELECT DISTINCT ON (user_id, filter_id) user_id, filter_id, filter_json
             FROM user_filters;
-        '''
+        """
     else:
-        select_clause = '''
+        select_clause = """
         CREATE TABLE user_filters_migration AS
             SELECT * FROM user_filters GROUP BY user_id, filter_id;
-        '''
-    sql = '''
+        """
+    sql = (
+        """
         BEGIN;
             %s
             DROP INDEX user_filters_by_user_id_filter_id;
@@ -28,7 +29,9 @@ def run_upgrade(cur, database_engine, *args, **kwargs):
             CREATE UNIQUE INDEX user_filters_by_user_id_filter_id_unique
                 ON user_filters(user_id, filter_id);
         END;
-    ''' % select_clause
+    """
+        % select_clause
+    )
     if isinstance(database_engine, PostgresEngine):
         cur.execute(sql)
     else:
