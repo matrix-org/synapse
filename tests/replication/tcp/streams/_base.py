65754ed13ca5e3b4d1be50de9b9712334d10265b
Amber Brown <hawkowl@atleastfornow.net>
2019-10-07 16:52:24 +0100

    fix tests

--- tests/replication/tcp/streams/_base.py
+++ tests/replication/tcp/streams/_base.py
@@ -12,14 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from mock import Mock
+
 from synapse.replication.tcp.commands import ReplicateCommand
 from synapse.replication.tcp.protocol import ClientReplicationStreamProtocol
 from synapse.replication.tcp.resource import ReplicationStreamProtocolFactory
 
 from tests import unittest
 from tests.server import FakeTransport
-
-from mock import Mock
 
 
 class BaseStreamTestCase(unittest.HomeserverTestCase):
