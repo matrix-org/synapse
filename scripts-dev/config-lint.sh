25a7977efd8fa9c891935f385a4bf3e0479a99b4
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2019-10-21 14:13:02 +0200

    Update scripts-dev/config-lint.sh
    Co-Authored-By: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- scripts-dev/config-lint.sh
+++ scripts-dev/config-lint.sh
@@ -3,7 +3,7 @@
 # Exits with 0 if there are no problems, or another code otherwise.
 
 # Fix non-lowercase true/false values
-sed -i -E "s/(#.*): +True(.*)/\1: true\2/g; s/(#.*): +False(.*)/\1: false\2/g;" synapse/config/*.py
+sed -i -E "s/(#.*): +True/\1: true/g; s/(#.*): +False/\1: false/g;" synapse/config/*.py
 
 # Check if anything changed
 git diff --quiet
