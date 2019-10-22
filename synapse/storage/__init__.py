1bbc5444a88e7069817433a518df50ac1a4c1811
Erik Johnston <erik@matrix.org>
2019-10-22 17:59:31 +0100

    Move README into synapse/storage/__init__.py

--- synapse/storage/__init__.py
+++ synapse/storage/__init__.py
@@ -14,6 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+The storage layer is split up into multiple parts to allow Synapse to run
+against different configurations of databases (e.g. single or multiple
+databases). The `data_stores` are classes that talk directly to a single
+database and have associated schemas, background updates, etc. On top of those
+there are (or will be) classes that provide high level interfaces that combine
+calls to multiple `data_stores`.
+
+There are also schemas that get applied to every database, regardless of the
+data stores associated with them (e.g. the schema version tables), which are
+stored in `synapse.storage.schema`.
+"""
 
 from synapse.storage.data_stores.main import DataStore  # noqa: F401
 
