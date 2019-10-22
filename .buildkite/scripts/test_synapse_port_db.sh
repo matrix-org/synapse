e9a67875e120a2b8d84ff7bfabd7f02d4e237764
Brendan Abolivier <babolivier@matrix.org>
2019-10-22 17:15:53 +0100

    Move tox environment instructions to a bash script

---
+++ .buildkite/scripts/test_synapse_port_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+
+find . -name '*.pyc' -delete
+
+pip install psycopg2 coverage coverage-enable-subprocess
+
+# Install Synapse itself. This won't update any libraries.
+pip install -e .
+
+# Make sure the SQLite3 database is using the latest schema and has no pending background update.
+scripts-dev/update_database --database-config .buildkite/sqlite-config.yaml
+
+# Create the PostgreSQL database.
+#PGPASSWORD=postgres createdb -h postgres -u postgres synapse
+PGPASSWORD=db_port createdb -h localhost -U synapse_db_port synapse_db_port
+
+# Run the script
+coverage run scripts/synapse_port_db --sqlite-database .buildkite/test_db.db --postgres-config .buildkite/postgres-config.yaml
+scripts/synapse_port_db --sqlite-database .buildkite/test_db.db --postgres-config .buildkite/postgres-config.yaml
