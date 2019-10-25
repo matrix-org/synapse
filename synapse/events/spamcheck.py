f00993987b6920fbb77f218805cccaf9d6c09adb
Travis Ralston <travpc@gmail.com>
2019-10-25 14:51:21 -0600

    Offer the homeserver instance to the spam checker

--- synapse/events/spamcheck.py
+++ synapse/events/spamcheck.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import inspect
 
 class SpamChecker(object):
     def __init__(self, hs):
@@ -26,7 +27,13 @@ class SpamChecker(object):
             pass
 
         if module is not None:
-            self.spam_checker = module(config=config)
+            # Older spam checkers don't accept the `hs` argument, so we
+            # try and detect support.
+            spam_args = inspect.getargspec(module)
+            if 'hs' in spam_args.args:
+                self.spam_checker = module(config=config, hs=hs)
+            else:
+                self.spam_checker = module(config=config)
 
     def check_event_for_spam(self, event):
         """Checks if a given event is considered "spammy" by this server.
