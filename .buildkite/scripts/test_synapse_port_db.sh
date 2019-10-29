5c9f2e230d38e6dc5cf5a8da41ffcb936c7ff4ef
Brendan Abolivier <babolivier@matrix.org>
2019-10-29 15:12:44 +0000

    Improve logging

--- .buildkite/scripts/test_synapse_port_db.sh
+++ .buildkite/scripts/test_synapse_port_db.sh
@@ -9,19 +9,28 @@
 set -xe
 cd `dirname $0`/../..
 
+echo "--- Install dependencies"
+
 # Install dependencies for this test.
 pip install psycopg2 coverage coverage-enable-subprocess
 
 # Install Synapse itself. This won't update any libraries.
 pip install -e .
 
+echo "--- Generate the signing key"
+
+# Generate the server's signing key.
 python -m synapse.app.homeserver --generate-keys -c .buildkite/sqlite-config.yaml
+
+echo "--- Prepare the databases"
 
 # Make sure the SQLite3 database is using the latest schema and has no pending background update.
 scripts-dev/update_database --database-config .buildkite/sqlite-config.yaml
 
 # Create the PostgreSQL database.
 ./.buildkite/scripts/create_postgres_db.py
+
+echo "+++ Run synapse_port_db"
 
 # Run the script
 coverage run scripts/synapse_port_db --sqlite-database .buildkite/test_db.db --postgres-config .buildkite/postgres-config.yaml
