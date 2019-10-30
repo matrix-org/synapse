a2276d4d3ca72896582ef24d01fdff6c01e38689
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2019-10-30 11:28:48 +0000

    Fix log line that was printing undefined value (#6278)

--- synapse/handlers/federation.py
+++ synapse/handlers/federation.py
@@ -1250,7 +1250,7 @@ class FederationHandler(BaseHandler):
                 builder=builder
             )
         except AuthError as e:
-            logger.warn("Failed to create join %r because %s", event, e)
+            logger.warn("Failed to create join to %s because %s", room_id, e)
             raise e
 
         event_allowed = yield self.third_party_event_rules.check_event_allowed(
