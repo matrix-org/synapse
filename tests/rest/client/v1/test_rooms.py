8822b331114a2f6fdcd5916f0c91991c0acae07e
Brendan Abolivier <babolivier@matrix.org>
2019-11-05 10:56:39 +0000

    Update copyrights

--- tests/rest/client/v1/test_rooms.py
+++ tests/rest/client/v1/test_rooms.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014-2016 OpenMarket Ltd
+# Copyright 2017 Vector Creations Ltd
+# Copyright 2018-2019 New Vector Ltd
 # Copyright 2019 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
