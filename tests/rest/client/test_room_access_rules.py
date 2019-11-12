d6b7606e07a83c9bcea4e1bff49fae4b1a25de35
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2019-11-12 11:38:35 +0000

    Create configurable ratelimiter for 3pid invites (#11)

--- tests/rest/client/test_room_access_rules.py
+++ tests/rest/client/test_room_access_rules.py
@@ -326,6 +326,12 @@ class RoomAccessTestCase(unittest.HomeserverTestCase):
             expect_code=200,
         )
 
+        # Disable the 3pid invite ratelimiter
+        burst = self.hs.config.rc_third_party_invite.burst_count
+        per_second = self.hs.config.rc_third_party_invite.per_second
+        self.hs.config.rc_third_party_invite.burst_count = 10
+        self.hs.config.rc_third_party_invite.per_second = 0.1
+
         # We can't send a 3PID invite to a room that already has two members.
         self.send_threepid_invite(
             address="test@allowed_domain",
@@ -353,6 +359,9 @@ class RoomAccessTestCase(unittest.HomeserverTestCase):
             room_id=self.direct_rooms[2],
             expected_code=403,
         )
+
+        self.hs.config.rc_third_party_invite.burst_count = burst
+        self.hs.config.rc_third_party_invite.per_second = per_second
 
     def test_unrestricted(self):
         """Tests that, in unrestricted mode, we can invite whoever we want, but we can
