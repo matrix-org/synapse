c039e3264b2dfefe78bfe2e1ee324cd0d9d66c5a
Neil Johnson <neil@matrix.org>
2019-12-09 12:20:14 +0000

    Back out perf regression from get_cross_signing_keys_from_cache.

--- synapse/handlers/e2e_keys.py
+++ synapse/handlers/e2e_keys.py
@@ -284,30 +284,7 @@ class E2eKeysHandler(object):
         self_signing_keys = {}
         user_signing_keys = {}
 
-        for user_id in query:
-            # XXX: consider changing the store functions to allow querying
-            # multiple users simultaneously.
-            key = yield self.store.get_e2e_cross_signing_key(
-                user_id, "master", from_user_id
-            )
-            if key:
-                master_keys[user_id] = key
-
-            key = yield self.store.get_e2e_cross_signing_key(
-                user_id, "self_signing", from_user_id
-            )
-            if key:
-                self_signing_keys[user_id] = key
-
-            # users can see other users' master and self-signing keys, but can
-            # only see their own user-signing keys
-            if from_user_id == user_id:
-                key = yield self.store.get_e2e_cross_signing_key(
-                    user_id, "user_signing", from_user_id
-                )
-                if key:
-                    user_signing_keys[user_id] = key
-
+        # Currently a stub, implementation coming in https://github.com/matrix-org/synapse/pull/6486
         return {
             "master_keys": master_keys,
             "self_signing_keys": self_signing_keys,
