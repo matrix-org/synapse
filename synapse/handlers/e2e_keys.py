ebaec848e34802116aebba80b46ad7c9cd72c304
Erik Johnston <erik@matrix.org>
2019-12-11 15:47:47 +0000

    Add missing inlineCallbacks

--- synapse/handlers/e2e_keys.py
+++ synapse/handlers/e2e_keys.py
@@ -264,6 +264,7 @@ class E2eKeysHandler(object):
 
         return ret
 
+    @defer.inlineCallbacks
     def get_cross_signing_keys_from_cache(self, query, from_user_id):
         """Get cross-signing keys for users from the database
 
