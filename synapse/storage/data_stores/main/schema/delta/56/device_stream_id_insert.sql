f353d4494db8bb9081af2706c5f90b338741b0ab
Andrew Morgan <andrew@amorgan.xyz>
2019-12-16 17:03:06 +0000

    Add contextual comment

--- synapse/storage/data_stores/main/schema/delta/56/device_stream_id_insert.sql
+++ synapse/storage/data_stores/main/schema/delta/56/device_stream_id_insert.sql
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+-- This line already existed in deltas/35/device_stream_id but was not included in the
+-- 54 full schema SQL. Add some SQL here to insert the missing row if it does not exist
 INSERT INTO device_max_stream_id (stream_id) SELECT 0 WHERE NOT EXISTS (
     SELECT * from device_max_stream_id
 );
