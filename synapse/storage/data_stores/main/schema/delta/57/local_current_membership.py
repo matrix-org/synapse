1e14ca1139220e4da14e5a5cf1de901657da80db
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2020-01-17 21:23:12 +0900

    Fix syntax error in run_upgrade for schema 57
    Fix #6727
    Related #6655

--- synapse/storage/data_stores/main/schema/delta/57/local_current_membership.py
+++ synapse/storage/data_stores/main/schema/delta/57/local_current_membership.py
@@ -56,7 +56,7 @@ def run_upgrade(cur, database_engine, config, *args, **kwargs):
             INSERT INTO local_current_membership (room_id, user_id, event_id, membership)
                 SELECT c.room_id, state_key AS user_id, event_id, c.membership
                 FROM current_state_events AS c
-                WHERE type = 'm.room.member' AND c.membership IS NOT NULL AND state_key like '%' || ?
+                WHERE type = 'm.room.member' AND c.membership IS NOT NULL AND state_key like '%%:%s'
         """
     else:
         # We can't rely on the membership column, so we need to join against
@@ -66,7 +66,7 @@ def run_upgrade(cur, database_engine, config, *args, **kwargs):
                 SELECT c.room_id, state_key AS user_id, event_id, r.membership
                 FROM current_state_events AS c
                 INNER JOIN room_memberships AS r USING (event_id)
-                WHERE type = 'm.room.member' and state_key like '%' || ?
+                WHERE type = 'm.room.member' and state_key like '%%:%s'
         """
     cur.execute(sql, (config.server_name,))
 
