fd16af3b93dba2896050733d79af53337c3dce20
Andrew Morgan <andrew@amorgan.xyz>
2020-01-23 13:40:30 +0000

    Add note in code about future deprecation

--- synapse/util/stringutils.py
+++ synapse/util/stringutils.py
@@ -27,6 +27,8 @@ from synapse.api.errors import Codes, SynapseError
 _string_with_symbols = string.digits + string.ascii_letters + ".,;:^&*-_+=#~@"
 
 # https://matrix.org/docs/spec/client_server/r0.6.0#post-matrix-client-r0-register-email-requesttoken
+# Note: The : character is allowed here for older clients, but will be removed in a
+# future release. Context: https://github.com/matrix-org/synapse/issues/6766
 client_secret_regex = re.compile(r"^[0-9a-zA-Z\.\=\_\-\:]+")
 
 # random_string and random_string_with_symbols are used for a range of things,
