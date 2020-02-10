e303f9850b796b99629cfaeeaa04ad6e88bad752
Richard van der Hoff <richard@matrix.org>
2020-02-10 00:16:35 +0000

    Add a script for buildkite to run for the py35 env
    Of course, it's installing tox that is failing, so upgrading setuptools from
    within `tox` doesn't help. We need to go earlier.
    
    The pipeline command is getting to complicated, so let's move the script in
    here.

---
+++ .buildkite/scripts/test_old_deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this script is run by buildkite in a plain `xenial` container; it installs the
+# minimal requirements for tox and hands over to the py35-old tox environment.
+
+set -ex
+
+apt-get update
+apt-get install -y python3.5 python3.5-dev python3-pip libxml2-dev libxslt-dev zlib1g-dev
+
+# workaround for https://github.com/jaraco/zipp/issues/40
+python3.5 -m pip install setuptools
+
+python3.5 -m pip install tox
+
+exec tox -e py35-old,combine
