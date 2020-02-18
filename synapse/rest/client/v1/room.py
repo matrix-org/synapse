9ccf899229b76d14f0f9c8dee88f837caf778aaf
Richard van der Hoff <richard@matrix.org>
2020-02-18 12:31:47 +0000

    initial impl of GET /_matrix/client/r0/rooms/{roomId}/aliases

--- synapse/rest/client/v1/room.py
+++ synapse/rest/client/v1/room.py
@@ -45,6 +45,10 @@ from synapse.storage.state import StateFilter
 from synapse.streams.config import PaginationConfig
 from synapse.types import RoomAlias, RoomID, StreamToken, ThirdPartyInstanceID, UserID
 
+MYPY = False
+if MYPY:
+    import synapse.server
+
 logger = logging.getLogger(__name__)
 
 
@@ -841,6 +845,24 @@ class RoomTypingRestServlet(RestServlet):
             )
 
         return 200, {}
+
+
+class RoomAliasListServlet(RestServlet):
+    PATTERNS = client_patterns("/rooms/(?P<room_id>[^/]*)/aliases", unstable=False)
+
+    def __init__(self, hs: "synapse.server.HomeServer"):
+        super().__init__()
+        self.auth = hs.get_auth()
+        self.directory_handler = hs.get_handlers().directory_handler
+
+    async def on_GET(self, request, room_id):
+        requester = await self.auth.get_user_by_req(request)
+
+        alias_list = await self.directory_handler.get_aliases_for_room(
+            requester, room_id
+        )
+
+        return 200, {"aliases": alias_list}
 
 
 class SearchRestServlet(RestServlet):
