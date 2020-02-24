6c4d06ceda8e5c38e710b59dcd84ceaf695f8cab
Erik Johnston <erik@matrix.org>
2020-02-24 15:08:24 +0000

    Fix copyright

--- synapse/app/user_dir.py
+++ synapse/app/user_dir.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2016 OpenMarket Ltd
+# Copyright 2017 Vector Creations Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
