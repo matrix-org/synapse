05cd4a142da05493f99d3769ad8df7ebeaf0129e
Patrick Cloke <patrickc@matrix.org>
2020-03-13 08:05:48 -0400

    Update the APIs to optionally logout devices.

--- synapse/rest/client/v2_alpha/account.py
+++ synapse/rest/client/v2_alpha/account.py
@@ -265,8 +265,11 @@ class PasswordRestServlet(RestServlet):
 
         assert_params_in_dict(params, ["new_password"])
         new_password = params["new_password"]
+        logout_devices = params.get("logout_devices", True)
 
-        await self._set_password_handler.set_password(user_id, new_password, requester)
+        await self._set_password_handler.set_password(
+            user_id, new_password, logout_devices, requester
+        )
 
         return 200, {}
 
