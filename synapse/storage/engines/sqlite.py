8895c38202f5f9cd136501cd330185c5b66654f8
Andrew Morgan <andrew@amorgan.xyz>
2020-03-25 15:31:44 +0000

    Use MYPY variable instead

--- synapse/storage/engines/sqlite.py
+++ synapse/storage/engines/sqlite.py
@@ -12,16 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import
-
 import struct
 import threading
 
-from pip._internal.utils.typing import MYPY_CHECK_RUNNING
-
 from synapse.storage.engines import BaseDatabaseEngine
 
-if MYPY_CHECK_RUNNING:
+MYPY = False
+if MYPY:
     import sqlite3
 
 
