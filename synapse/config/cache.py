be0737adc3369e65468f8ae5595896aeecf9f3ba
Andrew Morgan <andrew@amorgan.xyz>
2020-04-03 18:00:53 +0100

    Move _DEFAULT_CONFIG to class method

--- synapse/config/cache.py
+++ synapse/config/cache.py
@@ -31,22 +31,6 @@ CACHE_PROPERTIES = {
     "ensure_correct_cache_sizing": None,
 }
 
-_DEFAULT_CONFIG = """\
-# Cache configuration
-#
-# 'global_factor' controls the global cache factor. This overrides the
-# "SYNAPSE_CACHE_FACTOR" environment variable.
-#
-# 'per_cache_factors' is a dictionary of cache name to cache factor for that
-# individual cache.
-#
-#caches:
-#  global_factor: 0.5
-#  per_cache_factors:
-#    get_users_who_share_room_with_user: 2
-#
-"""
-
 
 def add_resizable_cache(cache_name, cache_resize_callback):
     _CACHES[cache_name.lower()] = cache_resize_callback
@@ -63,6 +47,23 @@ class CacheConfig(Config):
         CACHE_PROPERTIES["default_size_factor"] = float(os.environ.get(_CACHE_PREFIX, 0.5))
         CACHE_PROPERTIES["ensure_correct_cache_sizing"] = None
         _CACHES.clear()
+
+    def generate_config_section(self, **kwargs):
+        return """\
+        # Cache configuration
+        #
+        # 'global_factor' controls the global cache factor. This overrides the
+        # "SYNAPSE_CACHE_FACTOR" environment variable.
+        #
+        # 'per_cache_factors' is a dictionary of cache name to cache factor for that
+        # individual cache.
+        #
+        #caches:
+        #  global_factor: 0.5
+        #  per_cache_factors:
+        #    get_users_who_share_room_with_user: 2
+        #
+        """
 
     def read_config(self, config, **kwargs):
         self.event_cache_size = self.parse_size(config.get("event_cache_size", "10K"))
