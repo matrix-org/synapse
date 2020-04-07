daa82e56c79f9cafce18ae819cfc7a2026aaa5c8
Andrew Morgan <andrew@amorgan.xyz>
2020-04-07 16:56:34 +0100

    Add documentation to password_providers config option

--- synapse/config/password_auth_providers.py
+++ synapse/config/password_auth_providers.py
@@ -52,6 +52,16 @@ class PasswordAuthProviderConfig(Config):
 
     def generate_config_section(self, **kwargs):
         return """\
+        # Password providers allow homeserver administrators to integrate
+        # their Synapse installation with existing authentication methods
+        # ex. LDAP, external tokens, etc.
+        #
+        # For more information and known implementations, please see
+        # https://github.com/matrix-org/synapse/blob/master/docs/password_auth_providers.md
+        #
+        # Note: instances wishing to use SAML or CAS authentication should
+        # use the `saml2_config` option instead.
+        #
         #password_providers:
         #    - module: "ldap_auth_provider.LdapAuthProvider"
         #      config:
