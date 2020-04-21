9ac9c54592bcefeca4c0724adcd281f39427321f
Patrick Cloke <clokep@users.noreply.github.com>
2020-04-21 08:41:18 -0400

    Remove unnecessary lambda
    Co-Authored-By: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- synapse/handlers/auth.py
+++ synapse/handlers/auth.py
@@ -112,10 +112,10 @@ class AuthHandler(BaseHandler):
 
         # Expire old UI auth sessions after a period of time.
         self._clock.looping_call(
-            lambda: run_as_background_process(
-                "expire_old_sessions", self.expire_old_sessions
-            ),
+            run_as_background_process,
             5 * 60 * 1000,
+            "expire_old_sessions",
+            self.expire_old_sessions,
         )
 
         # Load the SSO HTML templates.
