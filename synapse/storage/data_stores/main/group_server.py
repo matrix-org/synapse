155cb5ce8516f0ff3beb1134d546d127fca2e5ec
Andrew Morgan <andrew@amorgan.xyz>
2020-05-14 17:34:30 +0100

    Also filter on rooms with 0 local_users_in_room

--- synapse/storage/data_stores/main/group_server.py
+++ synapse/storage/data_stores/main/group_server.py
@@ -96,6 +96,7 @@ class GroupServerWorkerStore(SQLBaseStore):
                     LEFT JOIN room_stats_current ON
                         group_rooms.room_id = room_stats_current.room_id
                         AND joined_members > 0
+                        AND local_users_in_room > 0
                     LEFT JOIN rooms ON
                         group_rooms.room_id = rooms.room_id
                         AND (room_version <> '') = ?
@@ -155,6 +156,7 @@ class GroupServerWorkerStore(SQLBaseStore):
                     LEFT JOIN room_stats_current ON
                         group_rooms.room_id = room_stats_current.room_id
                         AND joined_members > 0
+                        AND local_users_in_room > 0
                     LEFT JOIN rooms ON
                         group_rooms.room_id = rooms.room_id
                         AND (room_version <> '') = ?
