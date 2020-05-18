56d41a2f8b488d18580bba6c475bc0ecd0f56652
Erik Johnston <erik@matrix.org>
2020-05-18 11:58:36 +0100

    fix comment

--- synapse/replication/http/__init__.py
+++ synapse/replication/http/__init__.py
@@ -36,7 +36,7 @@ class ReplicationRestResource(JsonResource):
         send_event.register_servlets(hs, self)
         federation.register_servlets(hs, self)
 
-        # The following can't currently be instansiated on master.
+        # The following can't currently be instantiated on workers.
         if hs.config.worker.worker_app is None:
             membership.register_servlets(hs, self)
             login.register_servlets(hs, self)
