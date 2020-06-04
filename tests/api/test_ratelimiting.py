38995899ff7ec68963a8f07d1673333519f16241
Andrew Morgan <andrew@amorgan.xyz>
2020-06-04 19:52:25 +0100

    Clean up and split out tests

--- tests/api/test_ratelimiting.py
+++ tests/api/test_ratelimiting.py
@@ -4,7 +4,7 @@ from tests import unittest
 
 
 class TestRatelimiter(unittest.TestCase):
-    def test_allowed(self):
+    def test_allowed_via_can_do_action(self):
         limiter = Ratelimiter(clock=None, rate_hz=0.1, burst_count=1)
         allowed, time_allowed = limiter.can_do_action(key="test_id", _time_now_s=0)
         self.assertTrue(allowed)
@@ -25,79 +25,66 @@ class TestRatelimiter(unittest.TestCase):
         limiter.ratelimit(key="test_id", _time_now_s=0)
 
         # Should raise
-        self.assertRaises(
-            LimitExceededError, limiter.ratelimit, key="test_id", _time_now_s=5,
-        )
+        with self.assertRaises(LimitExceededError) as context:
+            limiter.ratelimit(key="test_id", _time_now_s=5)
+        self.assertEqual(context.exception.retry_after_ms, 5000)
 
         # Shouldn't raise
         limiter.ratelimit(key="test_id", _time_now_s=10)
 
-    def test_allowed_by_overriding_parameters(self):
-        """Test that we can override options of a Ratelimiter that would otherwise fail
+    def test_allowed_via_can_do_action_and_overriding_parameters(self):
+        """Test that we can override options of can_do_action that would otherwise fail
         an action
         """
         # Create a Ratelimiter with a very low allowed rate_hz and burst_count
         limiter = Ratelimiter(clock=None, rate_hz=0.1, burst_count=1)
 
         # First attempt should be allowed
-        time_now = 0
-        expected_allowed = 10.0
-
-        # Shouldn't raise
-        limiter.ratelimit(("test_id",), _time_now_s=time_now, update=False)
-
-        allowed, time_allowed = limiter.can_do_action(
-            key=("test_id",), _time_now_s=time_now,
-        )
+        allowed, time_allowed = limiter.can_do_action(("test_id",), _time_now_s=0,)
         self.assertTrue(allowed)
-        self.assertEquals(expected_allowed, time_allowed)
+        self.assertEqual(10.0, time_allowed)
 
         # Second attempt, 1s later, will fail
-        time_now = 1
-        expected_allowed = 10.0
-
-        # We expect a LimitExceededError to be raised
-        with self.assertRaises(LimitExceededError) as limit_exception:
-            limiter.ratelimit(("test_id",), _time_now_s=time_now, update=False)
-
-        self.assertEquals(
-            limit_exception.retry_after_ms / 1000, expected_allowed - time_now,
-        )
-
-        allowed, time_allowed = limiter.can_do_action(
-            key=("test_id",), _time_now_s=time_now,
-        )
+        allowed, time_allowed = limiter.can_do_action(("test_id",), _time_now_s=1,)
         self.assertFalse(allowed)
-        self.assertEquals(expected_allowed, time_allowed)
+        self.assertEqual(10.0, time_allowed)
 
-        # But, if we allow 10 actions/sec in this specific instance, we should be allowed
-        # to continue. burst_count is still 1.0
-        time_now = 1
-        expected_allowed = 1.1  # Changing rate_hz scales our time_allowed
-
-        # Shouldn't raise
-        limiter.ratelimit(
-            key=("test_id",), _time_now_s=time_now, rate_hz=10, update=False
-        )
-
+        # But, if we allow 10 actions/sec for this request, we should be allowed
+        # to continue.
         allowed, time_allowed = limiter.can_do_action(
-            key=("test_id",), _time_now_s=time_now, rate_hz=10,
+            ("test_id",), _time_now_s=1, rate_hz=10.0
         )
         self.assertTrue(allowed)
-        self.assertEquals(expected_allowed, time_allowed)
+        self.assertEqual(1.1, time_allowed)
 
-        # Similarly if we allow a burst of 10 actions, but a rate_hz of 0.1
-        time_now = 1
-        expected_allowed = 1.0
-        limiter.ratelimit(
-            key=("test_id",), _time_now_s=time_now, burst_count=10, update=False,
-        )
-
+        # Similarly if we allow a burst of 10 actions
         allowed, time_allowed = limiter.can_do_action(
-            key=("test_id",), _time_now_s=time_now, burst_count=10,
+            ("test_id",), _time_now_s=1, burst_count=10
         )
         self.assertTrue(allowed)
-        self.assertEquals(expected_allowed, time_allowed)
+        self.assertEqual(1.0, time_allowed)
+
+    def test_allowed_via_ratelimit_and_overriding_parameters(self):
+        """Test that we can override options of the ratelimit method that would otherwise
+        fail an action
+        """
+        # Create a Ratelimiter with a very low allowed rate_hz and burst_count
+        limiter = Ratelimiter(clock=None, rate_hz=0.1, burst_count=1)
+
+        # First attempt should be allowed
+        limiter.ratelimit(key=("test_id",), _time_now_s=0)
+
+        # Second attempt, 1s later, will fail
+        with self.assertRaises(LimitExceededError) as context:
+            limiter.ratelimit(key=("test_id",), _time_now_s=1)
+        self.assertEqual(context.exception.retry_after_ms, 9000)
+
+        # But, if we allow 10 actions/sec for this request, we should be allowed
+        # to continue.
+        limiter.ratelimit(key=("test_id",), _time_now_s=1, rate_hz=10.0)
+
+        # Similarly if we allow a burst of 10 actions
+        limiter.ratelimit(key=("test_id",), _time_now_s=1, burst_count=10)
 
     def test_pruning(self):
         limiter = Ratelimiter(clock=None, rate_hz=0.1, burst_count=1)
