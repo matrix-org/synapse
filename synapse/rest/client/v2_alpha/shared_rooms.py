676edeaeac9f51b18fab0b8f39e98e2aad7b81ad
Will Hunt <will@half-shot.uk>
2020-07-05 14:35:53 +0100

    fix linting again for isort

--- synapse/rest/client/v2_alpha/shared_rooms.py
+++ synapse/rest/client/v2_alpha/shared_rooms.py
@@ -12,11 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import logging
 
 from synapse.api.errors import AuthError
 from synapse.http.servlet import RestServlet
+
 from ._base import client_patterns
 
 logger = logging.getLogger(__name__)
