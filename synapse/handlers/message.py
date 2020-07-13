05f7bf6696d2ed00fd9ec00c2f2ae153c4d1d793
Patrick Cloke <patrickc@matrix.org>
2020-07-13 14:33:59 -0400

    More handlers / filtering / frozendict.

--- synapse/handlers/message.py
+++ synapse/handlers/message.py
@@ -14,10 +14,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
 import logging
 from typing import TYPE_CHECKING, Optional, Tuple
 
-from canonicaljson import encode_canonical_json, json
+from canonicaljson import encode_canonical_json
 
 from twisted.internet import defer
 from twisted.internet.defer import succeed
