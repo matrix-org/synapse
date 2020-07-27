3728c19944524b7861f3cf7cafe2e34a21bbd189
Patrick Cloke <patrickc@matrix.org>
2020-07-27 12:11:07 -0400

    Use canonicaljson >= 1.2.0

--- synapse/python_dependencies.py
+++ synapse/python_dependencies.py
@@ -43,8 +43,7 @@ REQUIREMENTS = [
     "jsonschema>=2.5.1",
     "frozendict>=1",
     "unpaddedbase64>=1.1.0",
-    # TODO This will require canonicaljson >= 1.2.0.
-    "canonicaljson@https://github.com/matrix-org/python-canonicaljson/archive/clokep/chooseable-json.zip",
+    "canonicaljson>=1.2.0",
     # we use the type definitions added in signedjson 1.1.
     "signedjson>=1.1.0",
     "pynacl>=1.2.1",
