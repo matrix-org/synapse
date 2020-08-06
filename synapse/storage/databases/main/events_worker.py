91e7442640e52a0ac577936065660bbfc4908757
Brendan Abolivier <babolivier@matrix.org>
2020-08-06 16:37:31 +0100

    Remove unused import

--- synapse/storage/databases/main/events_worker.py
+++ synapse/storage/databases/main/events_worker.py
@@ -41,7 +41,6 @@ from synapse.replication.tcp.streams import BackfillStream
 from synapse.replication.tcp.streams.events import EventsStream
 from synapse.storage._base import SQLBaseStore, db_to_json, make_in_list_sql_clause
 from synapse.storage.database import DatabasePool
-from synapse.storage.types import Cursor
 from synapse.storage.util.id_generators import StreamIdGenerator
 from synapse.types import get_domain_from_id
 from synapse.util.caches.descriptors import Cache, cached, cachedInlineCallbacks
