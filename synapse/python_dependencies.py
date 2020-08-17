e04e465b4d2c66acb8885c31736c7b7bb4e7be52
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2020-08-17 17:05:00 +0100

    Use the default templates when a custom template file cannot be found (#8037)
    Fixes https://github.com/matrix-org/synapse/issues/6583

--- synapse/python_dependencies.py
+++ synapse/python_dependencies.py
@@ -78,8 +78,6 @@ CONDITIONAL_REQUIREMENTS = {
     "matrix-synapse-ldap3": ["matrix-synapse-ldap3>=0.1"],
     # we use execute_batch, which arrived in psycopg 2.7.
     "postgres": ["psycopg2>=2.7"],
-    # ConsentResource uses select_autoescape, which arrived in jinja 2.9
-    "resources.consent": ["Jinja2>=2.9"],
     # ACME support is required to provision TLS certificates from authorities
     # that use the protocol, such as Let's Encrypt.
     "acme": [
