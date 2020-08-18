5fbc0362b51c9ea42f3a9feb1c8504bbd49942e7
Brendan Abolivier <babolivier@matrix.org>
2020-08-18 11:22:30 +0100

    Revert "Remove "user_id" from GET /presence. (#7606)"
    This reverts commit a3fbc23c39c0366392fd51faf0b1696f1f1d21c7.

--- synapse/rest/client/v1/presence.py
+++ synapse/rest/client/v1/presence.py
@@ -49,9 +49,7 @@ class PresenceStatusRestServlet(RestServlet):
                 raise AuthError(403, "You are not allowed to see their presence.")
 
         state = await self.presence_handler.get_state(target_user=user)
-        state = format_user_presence_state(
-            state, self.clock.time_msec(), include_user_id=False
-        )
+        state = format_user_presence_state(state, self.clock.time_msec())
 
         return 200, state
 
