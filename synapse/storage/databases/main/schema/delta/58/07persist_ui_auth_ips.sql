4895f8bc64f675d1d000c0a969f621fa45403661
Patrick Cloke <patrickc@matrix.org>
2020-08-20 15:09:40 -0400

    Add a comment to the delta.

--- synapse/storage/databases/main/schema/delta/58/07persist_ui_auth_ips.sql
+++ synapse/storage/databases/main/schema/delta/58/07persist_ui_auth_ips.sql
@@ -13,6 +13,8 @@
  * limitations under the License.
  */
 
+-- A table of the IP address and user-agent used to complete each step of a
+-- user-interactive authentication session.
 CREATE TABLE IF NOT EXISTS ui_auth_sessions_ips(
     session_id TEXT NOT NULL,
     ip TEXT NOT NULL,
