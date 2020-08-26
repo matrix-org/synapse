33874d4e547a21cec7413badade607578cefb519
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2020-08-26 20:23:41 +0100

    SQL column doc

--- synapse/storage/databases/main/schema/delta/58/11recovery_after_outage.sql
+++ synapse/storage/databases/main/schema/delta/58/11recovery_after_outage.sql
@@ -38,5 +38,7 @@ CREATE TABLE IF NOT EXISTS destination_rooms (
 
 -- this column tracks the stream_ordering of the event that was most recently
 -- successfully transmitted to the destination.
+-- A value of NULL means that we have not sent an event successfully yet
+-- (at least, not since the introduction of this column).
 ALTER TABLE destinations
   ADD COLUMN last_successful_stream_ordering INTEGER;
