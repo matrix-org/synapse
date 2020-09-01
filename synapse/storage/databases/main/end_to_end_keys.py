7ca82d3bf9d68d9c23e5b95007e439a02463d829
Richard van der Hoff <richard@matrix.org>
2020-09-01 22:47:12 +0100

    lint

--- synapse/storage/databases/main/end_to_end_keys.py
+++ synapse/storage/databases/main/end_to_end_keys.py
@@ -23,7 +23,7 @@ from twisted.enterprise.adbapi import Connection
 
 from synapse.logging.opentracing import log_kv, set_tag, trace
 from synapse.storage._base import SQLBaseStore, db_to_json
-from synapse.storage.database import LoggingTransaction, make_in_list_sql_clause
+from synapse.storage.database import make_in_list_sql_clause
 from synapse.types import JsonDict
 from synapse.util import json_encoder
 from synapse.util.caches.descriptors import cached, cachedList
