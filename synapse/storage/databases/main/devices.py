5615eb5cb48d63df15c391fe395c8740dc4af017
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2020-09-01 16:02:17 +0100

    Rename `_get_e2e_device_keys_txn` (#8222)
    ... to `_get_e2e_device_keys_and_signatures_txn`, to better reflect what it
    does.

--- synapse/storage/databases/main/devices.py
+++ synapse/storage/databases/main/devices.py
@@ -256,8 +256,8 @@ class DeviceWorkerStore(SQLBaseStore):
         """
         devices = (
             await self.db_pool.runInteraction(
-                "_get_e2e_device_keys_txn",
-                self._get_e2e_device_keys_txn,
+                "get_e2e_device_keys_and_signatures_txn",
+                self._get_e2e_device_keys_and_signatures_txn,
                 query_map.keys(),
                 include_all_devices=True,
                 include_deleted_devices=True,
