e7866a53694500435d07f71996d47a97d1531014
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2020-09-02 13:27:04 +0100

    Add query to insert destination_rooms entries.

--- synapse/storage/databases/main/transactions.py
+++ synapse/storage/databases/main/transactions.py
@@ -15,13 +15,13 @@
 
 import logging
 from collections import namedtuple
-from typing import Optional, Tuple
+from typing import Iterable, Optional, Tuple
 
 from canonicaljson import encode_canonical_json
 
 from synapse.metrics.background_process_metrics import run_as_background_process
 from synapse.storage._base import SQLBaseStore, db_to_json
-from synapse.storage.database import DatabasePool
+from synapse.storage.database import DatabasePool, LoggingTransaction
 from synapse.types import JsonDict
 from synapse.util.caches.expiringcache import ExpiringCache
 
@@ -272,4 +272,57 @@ class TransactionStore(SQLBaseStore):
 
         await self.db_pool.runInteraction(
             "_cleanup_transactions", _cleanup_transactions_txn
+        )
+
+    async def store_destination_rooms_entries(
+        self,
+        destinations: Iterable[str],
+        room_id: str,
+        event_id: str,
+        stream_ordering: int,
+    ) -> None:
+        """
+        Updates or creates `destination_rooms` entries in batch for a single event.
+
+        Args:
+            destinations: list of destinations
+            room_id: the room_id of the event
+            event_id: the ID of the event
+            stream_ordering: the stream_ordering of the event
+        """
+
+        return await self.db_pool.runInteraction(
+            "store_destination_rooms_entries",
+            self._store_destination_rooms_entries_txn,
+            destinations,
+            room_id,
+            event_id,
+            stream_ordering,
+        )
+
+    def _store_destination_rooms_entries_txn(
+        self,
+        txn: LoggingTransaction,
+        destinations: Iterable[str],
+        room_id: str,
+        event_id: str,
+        stream_ordering: int,
+    ) -> None:
+
+        q = """
+            INSERT OR IGNORE INTO destinations (destination)
+                VALUES (?);
+        """
+
+        txn.execute_batch(q, ((destination,) for destination in destinations))
+
+        rows = [(destination, room_id) for destination in destinations]
+
+        self.db_pool.simple_upsert_many_txn(
+            txn,
+            "destination_rooms",
+            ["destination", "room_id"],
+            rows,
+            ["stream_ordering"],
+            [(stream_ordering,)] * len(rows),
         )
