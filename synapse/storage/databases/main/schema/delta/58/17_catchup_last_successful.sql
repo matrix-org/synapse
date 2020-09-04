e5c50cf2d481ac92af9beced16904c73621179f0
Olivier Wilkinson (reivilibre) <olivier@librepush.net>
2020-09-04 14:36:29 +0100

    In light of #8255, use BIGINTs for stream_orderings

--- synapse/storage/databases/main/schema/delta/58/17_catchup_last_successful.sql
+++ synapse/storage/databases/main/schema/delta/58/17_catchup_last_successful.sql
@@ -18,4 +18,4 @@
 -- A value of NULL means that we have not sent an event successfully yet
 -- (at least, not since the introduction of this column).
 ALTER TABLE destinations
-    ADD COLUMN last_successful_stream_ordering INTEGER;
+    ADD COLUMN last_successful_stream_ordering BIGINT;
