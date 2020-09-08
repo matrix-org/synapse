72bec36d50465557744720b3955277da9fa330c4
Patrick Cloke <clokep@users.noreply.github.com>
2020-09-08 07:33:48 -0400

    Directly import json from the standard library. (#8259)
    By importing from canonicaljson the simplejson module was still being used
    in some situations. After this change the std lib json is consistenty used
    throughout Synapse.

--- synapse/api/filtering.py
+++ synapse/api/filtering.py
@@ -15,10 +15,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
 from typing import List
 
 import jsonschema
-from canonicaljson import json
 from jsonschema import FormatChecker
 
 from synapse.api.constants import EventContentFields
