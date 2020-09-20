matrix-org/synapse

commit a7bdf98d01d2225a479753a85ba81adf02b16a32

Rename database classes to make some sense (#8033)

synapse/storage/databases/main/user_erasure_store.py
@@ -1,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 New Vector Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import operator
+
+from synapse.storage._base import SQLBaseStore
+from synapse.util.caches.descriptors import cached, cachedList
+
+
+class UserErasureWorkerStore(SQLBaseStore):
+    @cached()
+    def is_user_erased(self, user_id):
+        """
+        Check if the given user id has requested erasure
+
+        Args:
+            user_id (str): full user id to check
+
+        Returns:
+            Deferred[bool]: True if the user has requested erasure
+        """
+        return self.db_pool.simple_select_onecol(
+            table="erased_users",
+            keyvalues={"user_id": user_id},
+            retcol="1",
+            desc="is_user_erased",
+        ).addCallback(operator.truth)
+
+    @cachedList(
+        cached_method_name="is_user_erased", list_name="user_ids", inlineCallbacks=True
+    )
+    def are_users_erased(self, user_ids):
+        """
+        Checks which users in a list have requested erasure
+
+        Args:
+            user_ids (iterable[str]): full user id to check
+
+        Returns:
+            Deferred[dict[str, bool]]:
+                for each user, whether the user has requested erasure.
+        """
+        # this serves the dual purpose of (a) making sure we can do len and
+        # iterate it multiple times, and (b) avoiding duplicates.
+        user_ids = tuple(set(user_ids))
+
+        rows = yield self.db_pool.simple_select_many_batch(
+            table="erased_users",
+            column="user_id",
+            iterable=user_ids,
+            retcols=("user_id",),
+            desc="are_users_erased",
+        )
+        erased_users = {row["user_id"] for row in rows}
+
+        res = {u: u in erased_users for u in user_ids}
+        return res
+
+
+class UserErasureStore(UserErasureWorkerStore):
+    def mark_user_erased(self, user_id: str) -> None:
+        """Indicate that user_id wishes their message history to be erased.
+
+        Args:
+            user_id: full user_id to be erased
+        """
+
+        def f(txn):
+            # first check if they are already in the list
+            txn.execute("SELECT 1 FROM erased_users WHERE user_id = ?", (user_id,))
+            if txn.fetchone():
+                return
+
+            # they are not already there: do the insert.
+            txn.execute("INSERT INTO erased_users (user_id) VALUES (?)", (user_id,))
+
+            self._invalidate_cache_and_stream(txn, self.is_user_erased, (user_id,))
+
+        return self.db_pool.runInteraction("mark_user_erased", f)
+
+    def mark_user_not_erased(self, user_id: str) -> None:
+        """Indicate that user_id is no longer erased.
+
+        Args:
+            user_id: full user_id to be un-erased
+        """
+
+        def f(txn):
+            # first check if they are already in the list
+            txn.execute("SELECT 1 FROM erased_users WHERE user_id = ?", (user_id,))
+            if not txn.fetchone():
+                return
+
+            # They are there, delete them.
+            self.simple_delete_one_txn(
+                txn, "erased_users", keyvalues={"user_id": user_id}
+            )
+
+            self._invalidate_cache_and_stream(txn, self.is_user_erased, (user_id,))
+
+        return self.db_pool.runInteraction("mark_user_not_erased", f)

synapse/storage/data_stores/main/user_erasure_store.py
@@ -1,113 +1,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 New Vector Ltd
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import operator
-
-from synapse.storage._base import SQLBaseStore
-from synapse.util.caches.descriptors import cached, cachedList
-
-
-class UserErasureWorkerStore(SQLBaseStore):
-    @cached()
-    def is_user_erased(self, user_id):
-        """
-        Check if the given user id has requested erasure
-
-        Args:
-            user_id (str): full user id to check
-
-        Returns:
-            Deferred[bool]: True if the user has requested erasure
-        """
-        return self.db.simple_select_onecol(
-            table="erased_users",
-            keyvalues={"user_id": user_id},
-            retcol="1",
-            desc="is_user_erased",
-        ).addCallback(operator.truth)
-
-    @cachedList(
-        cached_method_name="is_user_erased", list_name="user_ids", inlineCallbacks=True
-    )
-    def are_users_erased(self, user_ids):
-        """
-        Checks which users in a list have requested erasure
-
-        Args:
-            user_ids (iterable[str]): full user id to check
-
-        Returns:
-            Deferred[dict[str, bool]]:
-                for each user, whether the user has requested erasure.
-        """
-        # this serves the dual purpose of (a) making sure we can do len and
-        # iterate it multiple times, and (b) avoiding duplicates.
-        user_ids = tuple(set(user_ids))
-
-        rows = yield self.db.simple_select_many_batch(
-            table="erased_users",
-            column="user_id",
-            iterable=user_ids,
-            retcols=("user_id",),
-            desc="are_users_erased",
-        )
-        erased_users = {row["user_id"] for row in rows}
-
-        res = {u: u in erased_users for u in user_ids}
-        return res
-
-
-class UserErasureStore(UserErasureWorkerStore):
-    def mark_user_erased(self, user_id: str) -> None:
-        """Indicate that user_id wishes their message history to be erased.
-
-        Args:
-            user_id: full user_id to be erased
-        """
-
-        def f(txn):
-            # first check if they are already in the list
-            txn.execute("SELECT 1 FROM erased_users WHERE user_id = ?", (user_id,))
-            if txn.fetchone():
-                return
-
-            # they are not already there: do the insert.
-            txn.execute("INSERT INTO erased_users (user_id) VALUES (?)", (user_id,))
-
-            self._invalidate_cache_and_stream(txn, self.is_user_erased, (user_id,))
-
-        return self.db.runInteraction("mark_user_erased", f)
-
-    def mark_user_not_erased(self, user_id: str) -> None:
-        """Indicate that user_id is no longer erased.
-
-        Args:
-            user_id: full user_id to be un-erased
-        """
-
-        def f(txn):
-            # first check if they are already in the list
-            txn.execute("SELECT 1 FROM erased_users WHERE user_id = ?", (user_id,))
-            if not txn.fetchone():
-                return
-
-            # They are there, delete them.
-            self.simple_delete_one_txn(
-                txn, "erased_users", keyvalues={"user_id": user_id}
-            )
-
-            self._invalidate_cache_and_stream(txn, self.is_user_erased, (user_id,))
-
-        return self.db.runInteraction("mark_user_not_erased", f)

commit 17749e97544d0a12db1fabb067638974de96dde3

Fixed a bug with reactivating users with the admin API
Fixes: #8359
Trying to reactivate a user with the admin API (`PUT
/_synapse/admin/v2/users/<user_name>`) causes an internal server error

Seems to be a regression in #8033

synapse/storage/databases/main/user_erasure_store.py
@@ -100,7 +100,7 @@ class UserErasureStore(UserErasureWorkerStore):
                 return
 
             # They are there, delete them.
-            self.simple_delete_one_txn(
+            self.db_pool.simple_delete_one_txn(
                 txn, "erased_users", keyvalues={"user_id": user_id}
             )
 