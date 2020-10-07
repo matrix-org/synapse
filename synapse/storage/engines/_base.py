ae5b2a72c09d67311c9830f5a6fae1decce03e1f
Erik Johnston <erik@matrix.org>
2020-10-07 15:15:57 +0100

    Reduce serialization errors in MultiWriterIdGen (#8456)
    We call `_update_stream_positions_table_txn` a lot, which is an UPSERT
    that can conflict in `REPEATABLE READ` isolation level. Instead of doing
    a transaction consisting of a single query we may as well run it outside
    of a transaction.

--- synapse/storage/engines/_base.py
+++ synapse/storage/engines/_base.py
@@ -97,3 +97,20 @@ class BaseDatabaseEngine(Generic[ConnectionType], metaclass=abc.ABCMeta):
         """Gets a string giving the server version. For example: '3.22.0'
         """
         ...
+
+    @abc.abstractmethod
+    def in_transaction(self, conn: Connection) -> bool:
+        """Whether the connection is currently in a transaction.
+        """
+        ...
+
+    @abc.abstractmethod
+    def attempt_to_set_autocommit(self, conn: Connection, autocommit: bool):
+        """Attempt to set the connections autocommit mode.
+
+        When True queries are run outside of transactions.
+
+        Note: This has no effect on SQLite3, so callers still need to
+        commit/rollback the connections.
+        """
+        ...
