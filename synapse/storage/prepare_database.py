81dd52a80c6c9a3d992bc7c8d791df466303aecb
Neil Johnson <neil@matrix.org>
2020-10-09 13:53:47 +0100

    backout schema change

--- synapse/storage/prepare_database.py
+++ synapse/storage/prepare_database.py
@@ -34,7 +34,10 @@ logger = logging.getLogger(__name__)
 
 # Remember to update this number every time a change is made to database
 # schema files, so the users will be informed on server restarts.
-SCHEMA_VERSION = 59
+# XXX: If you're about to bump this to 59 (or higher) please create an update
+# that drops the unused `cache_invalidation_stream` table, as per #7436!
+# XXX: Also add an update to drop `account_data_max_stream_id` as per #7656!
+SCHEMA_VERSION = 58
 
 dir_path = os.path.abspath(os.path.dirname(__file__))
 
