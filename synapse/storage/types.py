fe19504bc719d7c7d88072e94efb6372e60f7503
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2020-10-16 23:21:14 +0100

    Update synapse/storage/types.py

--- synapse/storage/types.py
+++ synapse/storage/types.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Iterable, Iterator, List, Tuple, Optional
+from typing import Any, Iterable, Iterator, List, Optional, Tuple
 
 from typing_extensions import Protocol
 
