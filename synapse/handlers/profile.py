ed12369b12a8fbb5c86fcf726bcc2b71137a165b
Pavel Turinsky <pavel.turinsky@matfyz.cz>
2020-10-18 13:38:26 +0200

    Remove useless conversions to str

--- synapse/handlers/profile.py
+++ synapse/handlers/profile.py
@@ -92,11 +92,11 @@ class ProfileHandler(BaseHandler):
                 if e.code < 500 and e.code != 404:
                     # Other codes are not allowed in c2s API
                     logger.error(
-                        "Server replied with wrong response: %s %s", str(e.code), e.msg
+                        "Server replied with wrong response: %s %s", e.code, e.msg
                     )
                     # Change to 500 not to confuse clients
                     e.code = 500
-                    e.msg = "Remote server replied: {} {}".format(str(e.code), e.msg)
+                    e.msg = "Remote server replied: {} {}".format(e.code, e.msg)
                 raise e.to_synapse_error()
 
     async def get_profile_from_cache(self, user_id):
