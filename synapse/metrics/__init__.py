5570f1deae79c4d700d8e084eba4826f0fa8c839
Erik Johnston <erik@matrix.org>
2020-11-13 10:51:12 +0000

    Apply suggestions from code review
    Co-authored-by: Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>

--- synapse/metrics/__init__.py
+++ synapse/metrics/__init__.py
@@ -505,8 +505,8 @@ last_ticked = time.time()
 # 3PID send info
 threepid_send_requests = Histogram(
     "synapse_threepid_send_requests_with_tries",
-    documentation="Number of requests for a 3pid token by retry count. Note if"
-    " there is a request with rerty count of 4, then there would have been one"
+    documentation="Number of requests for a 3pid token by try count. Note if"
+    " there is a request with retry count of 4, then there would have been one"
     " each for 1, 2 and 3",
     buckets=(1, 2, 3, 4, 5, 10),
     labelnames=("type", "reason"),
