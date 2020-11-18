b925c61381e09e8aa055dc3ed0f19f1223c2d5a0
Patrick Cloke <clokep@users.noreply.github.com>
2020-11-18 07:12:10 -0500

    Clean-up formatting of sample config.

--- synapse/config/saml2_config.py
+++ synapse/config/saml2_config.py
@@ -256,8 +256,10 @@ class SAML2Config(Config):
             #  remote:
             #    - url: https://our_idp/metadata.xml
 
-            # Allowed clock difference in seconds from IdP.
+            # Allowed clock difference in seconds between the homeserver and IdP.
+            #
             # Uncomment the below to increase the accepted time difference from 0 to 3 seconds.
+            #
             #accepted_time_diff: 3
 
             # By default, the user has to go to our login page first. If you'd like
