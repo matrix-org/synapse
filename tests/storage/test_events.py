3f80eb55aa7633f535bfd2d37b4dd25d93fd7a8d
Erik Johnston <erik@matrix.org>
2020-12-11 16:44:04 +0000

    Refactor tests

--- tests/storage/test_events.py
+++ tests/storage/test_events.py
@@ -30,31 +30,29 @@ class ExtremPruneTestCase(HomeserverTestCase):
         login.register_servlets,
     ]
 
-    def test_prune_gap(self):
-        """Test that we drop extremities after a gap when we see an event from
-        the same domain.
-        """
-
-        state = self.hs.get_state_handler()
-        persistence = self.hs.get_storage().persistence
-        store = self.hs.get_datastore()
+    def prepare(self, reactor, clock, homeserver):
+        self.state = self.hs.get_state_handler()
+        self.persistence = self.hs.get_storage().persistence
+        self.store = self.hs.get_datastore()
 
         self.register_user("user", "pass")
         token = self.login("user", "pass")
-        room_id = self.helper.create_room_as(
+
+        self.room_id = self.helper.create_room_as(
             "user", room_version=RoomVersions.V6.identifier, tok=token
         )
 
-        body = self.helper.send(room_id, body="Test", tok=token)
+        body = self.helper.send(self.room_id, body="Test", tok=token)
         local_message_event_id = body["event_id"]
 
         # Fudge a remote event an persist it. This will be the extremity before
         # the gap.
-        remote_event_1 = event_from_pdu_json(
+        self.remote_event_1 = event_from_pdu_json(
             {
                 "type": EventTypes.Message,
+                "state_key": "@user:other",
                 "content": {},
-                "room_id": room_id,
+                "room_id": self.room_id,
                 "sender": "@user:other",
                 "depth": 5,
                 "prev_events": [local_message_event_id],
@@ -64,12 +62,31 @@ class ExtremPruneTestCase(HomeserverTestCase):
             RoomVersions.V6,
         )
 
-        context = self.get_success(state.compute_event_context(remote_event_1))
-        self.get_success(persistence.persist_event(remote_event_1, context))
+        self.persist_event(self.remote_event_1)
 
         # Check that the current extremities is the remote event.
-        extremities = self.get_success(store.get_prev_events_for_room(room_id))
-        self.assertCountEqual(extremities, [remote_event_1.event_id])
+        self.assert_extremities([self.remote_event_1.event_id])
+
+    def persist_event(self, event, state=None):
+        """Persist the event, with optional state
+        """
+        context = self.get_success(
+            self.state.compute_event_context(event, old_state=state)
+        )
+        self.get_success(self.persistence.persist_event(event, context))
+
+    def assert_extremities(self, expected_extremities):
+        """Assert the current extremities for the room
+        """
+        extremities = self.get_success(
+            self.store.get_prev_events_for_room(self.room_id)
+        )
+        self.assertCountEqual(extremities, expected_extremities)
+
+    def test_prune_gap(self):
+        """Test that we drop extremities after a gap when we see an event from
+        the same domain.
+        """
 
         # Fudge a second event which points to an event we don't have. This is a
         # state event so that the state changes (otherwise we won't prune the
@@ -79,7 +96,7 @@ class ExtremPruneTestCase(HomeserverTestCase):
                 "type": EventTypes.Member,
                 "state_key": "@user:other",
                 "content": {"membership": Membership.JOIN},
-                "room_id": room_id,
+                "room_id": self.room_id,
                 "sender": "@user:other",
                 "depth": 10,
                 "prev_events": ["$some_unknown_message"],
@@ -89,61 +106,17 @@ class ExtremPruneTestCase(HomeserverTestCase):
             RoomVersions.V6,
         )
 
-        state_before_gap = self.get_success(state.get_current_state(room_id))
+        state_before_gap = self.get_success(self.state.get_current_state(self.room_id))
 
-        context = self.get_success(
-            state.compute_event_context(
-                remote_event_2, old_state=state_before_gap.values()
-            )
-        )
-
-        self.get_success(persistence.persist_event(remote_event_2, context))
+        self.persist_event(remote_event_2, state=state_before_gap.values())
 
         # Check the new extremity is just the new remote event.
-        extremities = self.get_success(store.get_prev_events_for_room(room_id))
-        self.assertCountEqual(extremities, [remote_event_2.event_id])
+        self.assert_extremities([remote_event_2.event_id])
 
-    def test_dont_prune_gap_if_state_different(self):
+    def test_do_not_prune_gap_if_state_different(self):
         """Test that we don't prune extremities after a gap if the resolved
         state is different.
         """
-
-        state = self.hs.get_state_handler()
-        persistence = self.hs.get_storage().persistence
-        store = self.hs.get_datastore()
-
-        self.register_user("user", "pass")
-        token = self.login("user", "pass")
-        room_id = self.helper.create_room_as(
-            "user", room_version=RoomVersions.V6.identifier, tok=token
-        )
-
-        body = self.helper.send(room_id, body="Test", tok=token)
-        local_message_event_id = body["event_id"]
-
-        # Fudge a remote event an persist it. This will be the extremity before
-        # the gap.
-        remote_event_1 = event_from_pdu_json(
-            {
-                "type": EventTypes.Message,
-                "state_key": "@user:other",
-                "content": {},
-                "room_id": room_id,
-                "sender": "@user:other",
-                "depth": 5,
-                "prev_events": [local_message_event_id],
-                "auth_events": [],
-                "origin_server_ts": self.clock.time_msec(),
-            },
-            RoomVersions.V6,
-        )
-
-        context = self.get_success(state.compute_event_context(remote_event_1))
-        self.get_success(persistence.persist_event(remote_event_1, context))
-
-        # Check that the current extremities is the remote event.
-        extremities = self.get_success(store.get_prev_events_for_room(room_id))
-        self.assertCountEqual(extremities, [remote_event_1.event_id])
 
         # Fudge a second event which points to an event we don't have.
         remote_event_2 = event_from_pdu_json(
@@ -151,7 +124,7 @@ class ExtremPruneTestCase(HomeserverTestCase):
                 "type": EventTypes.Message,
                 "state_key": "@user:other",
                 "content": {},
-                "room_id": room_id,
+                "room_id": self.room_id,
                 "sender": "@user:other",
                 "depth": 10,
                 "prev_events": ["$some_unknown_message"],
@@ -164,19 +137,18 @@ class ExtremPruneTestCase(HomeserverTestCase):
         # Now we persist it with state with a dropped history visibility
         # setting. The state resolution across the old and new event will then
         # include it, and so the resolved state won't match the new state.
-        state_before_gap = dict(self.get_success(state.get_current_state(room_id)))
+        state_before_gap = dict(
+            self.get_success(self.state.get_current_state(self.room_id))
+        )
         state_before_gap.pop(("m.room.history_visibility", ""))
 
         context = self.get_success(
-            state.compute_event_context(
+            self.state.compute_event_context(
                 remote_event_2, old_state=state_before_gap.values()
             )
         )
 
-        self.get_success(persistence.persist_event(remote_event_2, context))
+        self.get_success(self.persistence.persist_event(remote_event_2, context))
 
         # Check that we haven't dropped the old extremity.
-        extremities = self.get_success(store.get_prev_events_for_room(room_id))
-        self.assertCountEqual(
-            extremities, [remote_event_1.event_id, remote_event_2.event_id]
-        )
+        self.assert_extremities([self.remote_event_1.event_id, remote_event_2.event_id])
