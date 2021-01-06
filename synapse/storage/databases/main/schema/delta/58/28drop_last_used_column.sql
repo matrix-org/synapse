81f3b6634e5a787369a3d95e37679b7f9c946dd2
Jerin J Titus <72017981+jerinjtitus@users.noreply.github.com>
2021-01-06 20:50:19 +0530

    Added leftout columns
    Signed-off-by: Jerin J Titus <72017981+jerinjtitus@users.noreply.github.com>

--- synapse/storage/databases/main/schema/delta/58/28drop_last_used_column.sql
+++ synapse/storage/databases/main/schema/delta/58/28drop_last_used_column.sql
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
  -- Dropping last_used column from access_tokens table.
 
 CREATE TABLE access_tokens2 (
@@ -20,13 +21,42 @@ CREATE TABLE access_tokens2 (
     user_id TEXT NOT NULL, 
     device_id TEXT, 
     token TEXT NOT NULL,
+    valid_until_ms BIGINT,
+    puppets_user_id TEXT,
+    last_validated BIGINT,
     UNIQUE(token) 
 );
 
 INSERT INTO access_tokens2(id, user_id, device_id, token)
-    SELECT id, user_id, device_id, token from access_tokens;
+    SELECT id, user_id, device_id, token FROM access_tokens;
 
 DROP TABLE access_tokens;
 ALTER TABLE access_tokens2 RENAME TO access_tokens;
 
 CREATE INDEX access_tokens_device_id ON access_tokens (user_id, device_id);
+
+
+-- Re-adding foreign key reference in event_txn_id table
+
+CREATE TABLE event_txn_id2 (
+    event_id TEXT NOT NULL,
+    room_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    token_id BIGINT NOT NULL,
+    txn_id TEXT NOT NULL,
+    inserted_ts BIGINT NOT NULL,
+    FOREIGN KEY (event_id)
+        REFERENCES events (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (token_id)
+        REFERENCES access_tokens (id) ON DELETE CASCADE
+);
+
+INSERT INTO event_txn_id2(event_id, room_id, user_id, token_id, txn_id, inserted_ts)
+    SELECT event_id, room_id, user_id, token_id, txn_id, inserted_ts FROM event_txn_id;
+
+DROP TABLE event_txn_id;
+ALTER TABLE event_txn_id2 RENAME TO event_txn_id;
+
+CREATE UNIQUE INDEX IF NOT EXISTS event_txn_id_event_id ON event_txn_id(event_id);
+CREATE UNIQUE INDEX IF NOT EXISTS event_txn_id_txn_id ON event_txn_id(room_id, user_id, token_id, txn_id);
+CREATE INDEX IF NOT EXISTS event_txn_id_ts ON event_txn_id(inserted_ts);
