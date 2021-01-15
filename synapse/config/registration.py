0a6f8d40a6bc559619c18544fab66a38d86e542c
Richard van der Hoff <richard@matrix.org>
2021-01-15 11:46:45 +0000

    Remote dependency on distutils
    `distutils` is pretty much deprecated these days, and replaced with
    `setuptools`. It's also annoying because it's you can't `pip install` it, and
    it's hard to figure out which debian package we should depend on to make sure
    it's there.
    
    Since we only use it for a tiny function anyway, let's just vendor said
    function into our codebase.

--- synapse/config/registration.py
+++ synapse/config/registration.py
@@ -14,14 +14,13 @@
 # limitations under the License.
 
 import os
-from distutils.util import strtobool
 
 import pkg_resources
 
 from synapse.api.constants import RoomCreationPreset
 from synapse.config._base import Config, ConfigError
 from synapse.types import RoomAlias, UserID
-from synapse.util.stringutils import random_string_with_symbols
+from synapse.util.stringutils import random_string_with_symbols, strtobool
 
 
 class AccountValidityConfig(Config):
@@ -86,12 +85,12 @@ class RegistrationConfig(Config):
     section = "registration"
 
     def read_config(self, config, **kwargs):
-        self.enable_registration = bool(
-            strtobool(str(config.get("enable_registration", False)))
+        self.enable_registration = strtobool(
+            str(config.get("enable_registration", False))
         )
         if "disable_registration" in config:
-            self.enable_registration = not bool(
-                strtobool(str(config["disable_registration"]))
+            self.enable_registration = not strtobool(
+                str(config["disable_registration"])
             )
 
         self.account_validity = AccountValidityConfig(
