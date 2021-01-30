aa2a3a4696d8c6e0ae5e0225f73804b75889740e
Jonathan de Jong <jonathan@automatia.nl>
2021-01-30 23:15:49 +0100

    pet the linter

--- synapse/storage/engines/__init__.py
+++ synapse/storage/engines/__init__.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import platform
 
 from ._base import BaseDatabaseEngine, IncorrectDatabaseSetup
 from .postgres import PostgresEngine
