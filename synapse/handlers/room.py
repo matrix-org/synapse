24fc99f00bb339d92ef2fe69bd8cb82c61dc3231
Patrick Cloke <patrickc@matrix.org>
2021-02-11 15:26:56 -0500

    Handle missing data in power levels events during room upgrade.

--- synapse/handlers/room.py
+++ synapse/handlers/room.py
@@ -425,17 +425,19 @@ class RoomCreationHandler(BaseHandler):
 
         # Copy over user power levels now as this will not be possible with >100PL users once
         # the room has been created
-
         # Calculate the minimum power level needed to clone the room
         event_power_levels = power_levels.get("events", {})
-        state_default = power_levels.get("state_default", 0)
-        ban = power_levels.get("ban")
+        state_default = power_levels.get("state_default", 50)
+        ban = power_levels.get("ban", 50)
         needed_power_level = max(state_default, ban, max(event_power_levels.values()))
 
+        # Get the user's current power level, this matches the logic in get_user_power_level,
+        # but without the entire state map.
+        current_power_levels = power_levels.setdefault("users", {})
+        current_power_level = current_power_levels.get(user_id, 0)
         # Raise the requester's power level in the new room if necessary
-        current_power_level = power_levels["users"][user_id]
         if current_power_level < needed_power_level:
-            power_levels["users"][user_id] = needed_power_level
+            current_power_levels[user_id] = needed_power_level
 
         await self._send_events_for_new_room(
             requester,
