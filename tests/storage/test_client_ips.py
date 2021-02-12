05dce7cdb098736f24361af1d1ee54b264e21242
dklimpel <5740567+dklimpel@users.noreply.github.com>
2021-02-12 23:46:13 +0100

    fix ClientIpAuthTestCase

--- tests/storage/test_client_ips.py
+++ tests/storage/test_client_ips.py
@@ -372,7 +372,7 @@ class ClientIpStoreTestCase(unittest.HomeserverTestCase):
 class ClientIpAuthTestCase(unittest.HomeserverTestCase):
 
     servlets = [
-        synapse.rest.admin.register_servlets_for_client_rest_resource,
+        synapse.rest.admin.register_servlets,
         login.register_servlets,
     ]
 
@@ -416,7 +416,7 @@ class ClientIpAuthTestCase(unittest.HomeserverTestCase):
             self.reactor,
             self.site,
             "GET",
-            "/_synapse/admin/v1/users/" + self.user_id,
+            "/_synapse/admin/v2/users/" + self.user_id,
             access_token=access_token,
             custom_headers=headers1.items(),
             **make_request_args,
