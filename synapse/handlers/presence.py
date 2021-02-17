8f79d6ef5bb7d3423eb5a0f6f8e2924db7bdb835
Andrew Morgan <andrew@amorgan.xyz>
2021-02-17 16:50:01 +0000

    Add type hints and args docstring to _update_states

--- synapse/handlers/presence.py
+++ synapse/handlers/presence.py
@@ -349,10 +349,13 @@ class PresenceHandler(BasePresenceHandler):
                 [self.user_to_current_state[user_id] for user_id in unpersisted]
             )
 
-    async def _update_states(self, new_states):
+    async def _update_states(self, new_states: Iterable[UserPresenceState]):
         """Updates presence of users. Sets the appropriate timeouts. Pokes
         the notifier and federation if and only if the changed presence state
         should be sent to clients/servers.
+
+        Args:
+            new_states: The new user presence state updates to process.
         """
         now = self.clock.time_msec()
 
