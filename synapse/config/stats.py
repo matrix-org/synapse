c6c7d03c6fb5ba0f0824b8d1d3e67767c0de5e55
Richard van der Hoff <richard@matrix.org>
2021-03-12 12:11:13 +0000

    Remove unused `stats.retention` configuration setting

--- synapse/config/stats.py
+++ synapse/config/stats.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+import logging
 
 from ._base import Config
 
@@ -28,15 +28,11 @@ class StatsConfig(Config):
     def read_config(self, config, **kwargs):
         self.stats_enabled = True
         self.stats_bucket_size = 86400 * 1000
-        self.stats_retention = sys.maxsize
         stats_config = config.get("stats", None)
         if stats_config:
             self.stats_enabled = stats_config.get("enabled", self.stats_enabled)
             self.stats_bucket_size = self.parse_duration(
                 stats_config.get("bucket_size", "1d")
-            )
-            self.stats_retention = self.parse_duration(
-                stats_config.get("retention", "%ds" % (sys.maxsize,))
             )
 
     def generate_config_section(self, config_dir_path, server_name, **kwargs):
@@ -46,12 +42,7 @@ class StatsConfig(Config):
         # 'bucket_size' controls how large each statistics timeslice is. It can
         # be defined in a human readable short form -- e.g. "1d", "1y".
         #
-        # 'retention' controls how long historical statistics will be kept for.
-        # It can be defined in a human readable short form -- e.g. "1d", "1y".
-        #
-        #
         #stats:
         #   enabled: true
         #   bucket_size: 1d
-        #   retention: 1y
         """
