a089ad3b66abb9246ee284320322555d70903cc0
Andrew Morgan <andrew@amorgan.xyz>
2021-03-25 12:13:10 +0000

    Rewrite complement.sh
    * We now use a local checkout of Complement, instead of downloading the
    master branch
    * You can now specify a regex of test names to run, or just run all
    tests
    * We now use the Synapse test blacklist tag

--- scripts-dev/complement.sh
+++ scripts-dev/complement.sh
@@ -1,22 +1,35 @@
-#! /bin/bash -eu
+#! /bin/bash -e
 # This script is designed for developers who want to test their code
 # against Complement.
 #
 # It makes a Synapse image which represents the current checkout,
-# then downloads Complement and runs it with that image.
+# builds a synapse-complement image on top, then runs tests with it.
+#
+# By default the script assumes that a Complement checkout exists next to
+# the current Synapse checkout directory. This can be overridden by setting
+# the COMPLEMENT_DIR env var to the path to your Complement checkout.
+#
+# A regular expression of test method names can be supplied as the first
+# argument to the script. Complement will then only run those tests. If
+# no regex is supplied, all tests are run. Ex.
+#
+# ./complement.sh "TestOutboundFederation(Profile|Send)"
+#
+COMPLEMENT_DIR="${COMPLEMENT_DIR:-$(dirname $0)/../../complement}"
 
 cd "$(dirname $0)/.."
 
 # Build the base Synapse image from the local checkout
-docker build -t matrixdotorg/synapse:latest -f docker/Dockerfile .
+docker build -t matrixdotorg/synapse -f docker/Dockerfile .
+# Build the Synapse monolith image from Complement, based on the above image we just built
+docker build -t complement-synapse -f "$COMPLEMENT_DIR/dockerfiles/Synapse.Dockerfile" "$COMPLEMENT_DIR/dockerfiles"
 
-# Download Complement
-wget -N https://github.com/matrix-org/complement/archive/master.tar.gz
-tar -xzf master.tar.gz
-cd complement-master
-
-# Build the Synapse image from Complement, based on the above image we just built
-docker build -t complement-synapse -f dockerfiles/Synapse.Dockerfile ./dockerfiles
+cd "$COMPLEMENT_DIR"
 
 # Run the tests on the resulting image!
-COMPLEMENT_BASE_IMAGE=complement-synapse go test -v -count=1 ./tests
+if [[ -z $1 ]]; then
+  # No test name regex is set, run all tests
+  COMPLEMENT_BASE_IMAGE=complement-synapse go test -v -tags synapse_blacklist ./tests
+else
+  COMPLEMENT_BASE_IMAGE=complement-synapse go test -v -tags synapse_blacklist -run "$1" ./tests
+fi
