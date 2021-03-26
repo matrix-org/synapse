b5efcb577e2c9b8b38cb86f87cf65fa93eb2566b
Erik Johnston <erik@matrix.org>
2021-03-26 16:49:46 +0000

    Make it possible to use dmypy (#9692)
    Running `dmypy run` will do a `mypy` check while spinning up a daemon
    that makes rerunning `dmypy run` a lot faster.
    
    `dmypy` doesn't support `follow_imports = silent` and has
    `local_partial_types` enabled, so this PR enables those options and
    fixes the issues that were newly raised. Note that `local_partial_types`
    will be enabled by default in upcoming mypy releases.

--- synapse/replication/tcp/protocol.py
+++ synapse/replication/tcp/protocol.py
@@ -104,7 +104,7 @@ tcp_outbound_commands_counter = Counter(
 
 # A list of all connected protocols. This allows us to send metrics about the
 # connections.
-connected_connections = []
+connected_connections = []  # type: List[BaseReplicationStreamProtocol]
 
 
 logger = logging.getLogger(__name__)
