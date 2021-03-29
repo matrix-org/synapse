5c6aee74123d661e69af2ed24b371ffb47658332
Andrew Morgan <andrew@amorgan.xyz>
2021-03-29 17:12:17 +0100

    Copyrig**n**t -> Copyright

--- synapse/federation/federation_client.py
+++ synapse/federation/federation_client.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015, 2016 OpenMarket Ltd
-# Copyrignt 2020 Sorunome
-# Copyrignt 2020 The Matrix.org Foundation C.I.C.
+# Copyright 2020 Sorunome
+# Copyright 2020 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
