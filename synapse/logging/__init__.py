50e45ab75a2be347d7e29379d059a99bc508a822
Richard van der Hoff <richard@matrix.org>
2021-05-10 17:20:38 +0100

    Add debug logging for issue #9553
    Hopefully this will help us track down where to-device messages are getting
    lost/delayed.

--- synapse/logging/__init__.py
+++ synapse/logging/__init__.py
@@ -13,7 +13,12 @@
 # limitations under the License.
 
 # These are imported to allow for nicer logging configuration files.
+import logging
+
 from synapse.logging._remote import RemoteHandler
 from synapse.logging._terse_json import JsonFormatter, TerseJsonFormatter
 
 __all__ = ["RemoteHandler", "JsonFormatter", "TerseJsonFormatter"]
+
+# Debug logger for https://github.com/matrix-org/synapse/issues/9533 etc
+issue9533_logger = logging.getLogger("synapse.9533_debug")
