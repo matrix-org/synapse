3dc8841f657bd193cf74f92cd10fe6ab9188667a
Erik Johnston <erik@matrix.org>
2021-05-28 14:45:24 +0100

    Add docstring

--- synapse/crypto/keyring.py
+++ synapse/crypto/keyring.py
@@ -135,7 +135,18 @@ class KeyLookupError(ValueError):
 
 @attr.s(slots=True)
 class _FetchKeyRequest:
-    """A request for keys for a given server."""
+    """A request for keys for a given server.
+
+    We will continue to try and fetch until we have all the keys listed under
+    `key_ids` (with an appropriate `valid_until_ts` property) or we run out of
+    places to fetch keys from.
+
+    Attributes:
+        server_name: The name of the server that owns the keys.
+        minimum_valid_until_ts: The earliest timestamp at which we need the
+            keys to be valid at.
+        key_ids: The IDs of the keys to attempt to fetch
+    """
 
     server_name = attr.ib(type=str)
     minimum_valid_until_ts = attr.ib(type=int)
