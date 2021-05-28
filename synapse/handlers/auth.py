9e7ce1fe4f8fcccbe8894ff104691a89986ae204
Quentin Gliech <quentingliech@gmail.com>
2021-05-28 11:10:59 +0200

    MSC2918: lint: remove unused import

--- synapse/handlers/auth.py
+++ synapse/handlers/auth.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import base64
-import binascii
 import logging
 import secrets
 import time
