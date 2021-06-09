ae85719c5873e41886bbe7e8e8d8e05b23b68ca8
Eric Eastwood <erice@element.io>
2021-06-09 01:58:46 -0500

    Fix tests using False as a depth value (should be None)
    See https://github.com/matrix-org/synapse/pull/9247#discussion_r648019790

--- tests/storage/test_redaction.py
+++ tests/storage/test_redaction.py
@@ -239,7 +239,7 @@ class RedactionTestCase(unittest.HomeserverTestCase):
                 depth: Optional[int] = None,
             ):
                 built_event = await self._base_builder.build(
-                    prev_event_ids, auth_event_ids
+                    prev_event_ids=prev_event_ids, auth_event_ids=auth_event_ids
                 )
 
                 built_event._event_id = self._event_id
