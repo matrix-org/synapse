1dbd3110403ea80c9fbae4b245c18c530243ccba
Erik Johnston <erik@matrix.org>
2021-06-22 11:12:23 +0100

    Fix schema delta to not take as long on large servers
    Introduced in #6739

--- synapse/storage/schema/main/delta/59/11add_knock_members_to_stats.sql
+++ synapse/storage/schema/main/delta/59/11add_knock_members_to_stats.sql
@@ -13,5 +13,8 @@
  * limitations under the License.
  */
 
-ALTER TABLE room_stats_current ADD COLUMN knocked_members INT NOT NULL DEFAULT '0';
-ALTER TABLE room_stats_historical ADD COLUMN knocked_members BIGINT NOT NULL DEFAULT '0';
+-- Existing rows will default to NULL, so anything reading from these tables
+-- needs to interpret NULL as 0. This is fine here as no existing rooms can have
+-- any knocked members.
+ALTER TABLE room_stats_current ADD COLUMN knocked_members INT;
+ALTER TABLE room_stats_historical ADD COLUMN knocked_members BIGINT;
