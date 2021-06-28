5575b5cb43125d334f510027f6a3482d0b15bd4c
Erik Johnston <erik@matrix.org>
2021-06-28 14:12:48 +0100

    Don't send catchup transactions for matrix hq
    This is because there are problems with HQ atm.

--- synapse/storage/databases/main/transactions.py
+++ synapse/storage/databases/main/transactions.py
@@ -411,6 +411,7 @@ class TransactionWorkerStore(CacheInvalidationWorkerStore):
                  JOIN events USING (stream_ordering)
                 WHERE destination = ?
                   AND stream_ordering > ?
+                  AND room_id != '!OGEhHVWSdvArJzumhm:matrix.org'
                 ORDER BY stream_ordering
                 LIMIT 50
             """
