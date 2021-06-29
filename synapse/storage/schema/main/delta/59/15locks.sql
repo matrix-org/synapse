fba61152c4d84325f8da1cadbce6d63261fb64ae
Erik Johnston <erik@matrix.org>
2021-06-29 17:38:08 +0100

    Add some comments on DB fields

--- synapse/storage/schema/main/delta/59/15locks.sql
+++ synapse/storage/schema/main/delta/59/15locks.sql
@@ -21,7 +21,15 @@
 CREATE TABLE worker_locks (
     lock_name TEXT NOT NULL,
     lock_key TEXT NOT NULL,
+    -- We write the instance name to ease manual debugging, we don't ever read
+    -- from it.
+    -- Note: instance names aren't guarenteed to be unique.
     instance_name TEXT NOT NULL,
+    -- A random string generated each time an instance takes out a lock. Used by
+    -- the instance to tell whether the lock is still held by it (e.g. in the
+    -- case where the process stalls for a long time the lock may time out and
+    -- be taken out by another instance, at which point the original instance
+    -- can tell it no longer holds the lock as the tokens no longer match).
     token TEXT NOT NULL,
     last_renewed_ts BIGINT NOT NULL
 );
