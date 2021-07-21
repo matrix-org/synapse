4b1300b73ae31cdc6300163e2182c239e6ebf8a9
Toni Spets <toni.spets@iki.fi>
2021-07-21 09:07:45 +0300

    Allow setting transaction limit for db connections
    Setting the value will help PostgreSQL free up memory by recycling
    the connections in the connection pool.
    
    Signed-off-by: Toni Spets <toni.spets@iki.fi>

--- synapse/storage/database.py
+++ synapse/storage/database.py
@@ -405,6 +405,8 @@ class DatabasePool:
         self._previous_txn_total_time = 0.0
         self._current_txn_total_time = 0.0
         self._previous_loop_ts = 0.0
+        self._txn_counters: Dict[int, int] = {}
+        self._txn_limit = database_config.config.get("txn_limit", 0)
 
         # TODO(paul): These can eventually be removed once the metrics code
         #   is running in mainline, and we have some nice monitoring frontends
@@ -750,10 +752,26 @@ class DatabasePool:
                     sql_scheduling_timer.observe(sched_duration_sec)
                     context.add_database_scheduled(sched_duration_sec)
 
+                    if self._txn_limit > 0:
+                        tid = self._db_pool.threadID()
+                        if tid not in self._txn_counters:
+                            self._txn_counters[tid] = 0
+                        self._txn_counters[tid] += 1
+
+                        if self._txn_counters[tid] > self._txn_limit:
+                            logger.debug(
+                                "Reconnecting database connection over transaction limit"
+                            )
+                            conn.reconnect()
+                            opentracing.log_kv({"message": "reconnected"})
+                            self._txn_counters[tid] = 0
+
                     if self.engine.is_connection_closed(conn):
                         logger.debug("Reconnecting closed database connection")
                         conn.reconnect()
                         opentracing.log_kv({"message": "reconnected"})
+                        if self._txn_limit > 0:
+                            self._txn_counters[tid] = 0
 
                     try:
                         if db_autocommit:
