dcb02c392dcfc075382af5e604ee1919a485ebf4
Eric Eastwood <erice@element.io>
2021-08-10 22:57:14 -0500

    Move /batch_send to /v2_alpha directory
    As pointed out by @erikjohnston,
    https://github.com/matrix-org/synapse/pull/10552#discussion_r685836624

--- synapse/rest/__init__.py
+++ synapse/rest/__init__.py
@@ -49,6 +49,7 @@ from synapse.rest.client.v2_alpha import (
     report_event,
     room_keys,
     room_upgrade_rest_servlet,
+    room as roomv2,
     sendtodevice,
     shared_rooms,
     sync,
@@ -117,6 +118,7 @@ class ClientRestResource(JsonResource):
         user_directory.register_servlets(hs, client_resource)
         groups.register_servlets(hs, client_resource)
         room_upgrade_rest_servlet.register_servlets(hs, client_resource)
+        roomv2.register_servlets(hs, client_resource)
         capabilities.register_servlets(hs, client_resource)
         account_validity.register_servlets(hs, client_resource)
         relations.register_servlets(hs, client_resource)
