8fb5363436d515ebaf3d62d3fdabea0d935a475c
Azrenbeth <7782548+Azrenbeth@users.noreply.github.com>
2021-08-11 11:08:21 +0100

    Added error handling when calling callbacks
    Also renamed the callback types to the form
    of XYZ_CALLBACK

--- synapse/events/presence_router.py
+++ synapse/events/presence_router.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import logging
 from typing import (
     TYPE_CHECKING,
     Awaitable,
@@ -27,12 +28,14 @@ from synapse.api.presence import UserPresenceState
 from synapse.util.async_helpers import maybe_awaitable
 
 if TYPE_CHECKING:
-    from synapse.server import HomeServer, logger
+    from synapse.server import HomeServer
 
 GET_USERS_FOR_STATES_CALLBACK = Callable[
     [Iterable[UserPresenceState]], Awaitable[Dict[str, Set[UserPresenceState]]]
 ]
 GET_INTERESTED_USERS_CALLBACK = Callable[[str], Awaitable[Union[Set[str], str]]]
+
+logger = logging.getLogger(__name__)
 
 
 def load_legacy_presence_router(hs: "HomeServer"):
