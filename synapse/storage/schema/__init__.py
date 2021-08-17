f6dcc61581b2593b6aa05a89b86a662aac13a24c
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2021-08-17 11:23:15 +0100

    lint

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -19,7 +19,7 @@ This should be incremented whenever the codebase changes its requirements on the
 shape of the database schema (even if those requirements are backwards-compatible with
 older versions of Synapse).
 
-See https://matrix-org.github.io/synapse/develop/development/database_schema.html 
+See https://matrix-org.github.io/synapse/develop/development/database_schema.html
 for more information on how this works.
 
 Changes in SCHEMA_VERSION = 61:
