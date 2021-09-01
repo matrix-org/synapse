462ab259c8db0d9da7c7cd6ef4a8ac0067c7d450
Eric Eastwood <erice@element.io>
2021-08-31 20:04:04 -0500

    Add comment to better document constant
    See https://github.com/matrix-org/synapse/pull/10697#discussion_r699674458

--- synapse/api/constants.py
+++ synapse/api/constants.py
@@ -198,6 +198,7 @@ class EventContentFields:
     # cf https://github.com/matrix-org/matrix-doc/pull/1772
     ROOM_TYPE = "type"
 
+    # The creator of the room, as used in `m.room.create` events.
     ROOM_CREATOR = "creator"
 
     # Used on normal messages to indicate they were historically imported after the fact
