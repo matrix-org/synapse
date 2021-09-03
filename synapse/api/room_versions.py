68017b5beecb420ca26a5b800f6a0d11381625ba
Patrick Cloke <clokep@users.noreply.github.com>
2021-09-03 09:14:31 -0400

    Do not update the preferred version.

--- synapse/api/room_versions.py
+++ synapse/api/room_versions.py
@@ -324,7 +324,7 @@ MSC3244_CAPABILITIES = {
         ),
         RoomVersionCapability(
             "restricted",
-            RoomVersions.V9,
+            RoomVersions.V8,
             lambda room_version: room_version.msc3083_join_rules,
         ),
     )
