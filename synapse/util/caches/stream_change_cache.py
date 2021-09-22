53dae76188f0b5e406dffd3d843ae3a296fcdf7f
David Robertson <davidr@element.io>
2021-09-22 16:49:03 +0100

    Make `stream_change_cache` pass `no-untyped-defs`

--- synapse/util/caches/stream_change_cache.py
+++ synapse/util/caches/stream_change_cache.py
@@ -40,10 +40,10 @@ class StreamChangeCache:
         self,
         name: str,
         current_stream_pos: int,
-        max_size=10000,
+        max_size: int = 10000,
         prefilled_cache: Optional[Mapping[EntityType, int]] = None,
-    ):
-        self._original_max_size = max_size
+    ) -> None:
+        self._original_max_size: int = max_size
         self._max_size = math.floor(max_size)
         self._entity_to_key: Dict[EntityType, int] = {}
 
