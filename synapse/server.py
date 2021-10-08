a7d22c36dbbbdd396aeb8938b57b5fd7edb689f3
Eric Eastwood <erice@element.io>
2021-10-08 18:35:00 -0500

    Refactor MSC2716 `/batch_send` endpoint into separate handler functions (#10974)

--- synapse/server.py
+++ synapse/server.py
@@ -97,6 +97,7 @@ from synapse.handlers.room import (
     RoomCreationHandler,
     RoomShutdownHandler,
 )
+from synapse.handlers.room_batch import RoomBatchHandler
 from synapse.handlers.room_list import RoomListHandler
 from synapse.handlers.room_member import RoomMemberHandler, RoomMemberMasterHandler
 from synapse.handlers.room_member_worker import RoomMemberWorkerHandler
@@ -436,6 +437,10 @@ class HomeServer(metaclass=abc.ABCMeta):
     @cache_in_self
     def get_room_creation_handler(self) -> RoomCreationHandler:
         return RoomCreationHandler(self)
+
+    @cache_in_self
+    def get_room_batch_handler(self) -> RoomBatchHandler:
+        return RoomBatchHandler(self)
 
     @cache_in_self
     def get_room_shutdown_handler(self) -> RoomShutdownHandler:
