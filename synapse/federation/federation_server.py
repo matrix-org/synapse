dce957c0d494016909944c28a70c03adbb327c45
reivilibre <oliverw@matrix.org>
2021-10-22 12:13:42 +0100

    Update synapse/federation/federation_server.py
    Co-authored-by: Patrick Cloke <clokep@users.noreply.github.com>

--- synapse/federation/federation_server.py
+++ synapse/federation/federation_server.py
@@ -634,7 +634,7 @@ class FederationServer(FederationBase):
 
     async def on_make_knock_request(
         self, origin: str, room_id: str, user_id: str, supported_versions: List[str]
-    ) -> Dict[str, Union[JsonDict, str]]:
+    ) -> JsonDict:
         """We've received a /make_knock/ request, so we create a partial knock
         event for the room and hand that back, along with the room version, to the knocking
         homeserver. We do *not* persist or process this event until the other server has
