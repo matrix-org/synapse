9d0f9d51d5da7ebcd3178fefc1e68c3c5494d673
Dan Callahan <danc@element.io>
2021-10-22 21:44:25 +0100

    Fix Shellcheck SC2016: Single quotes don't expand
    Expressions don't expand in single quotes, use double quotes for that.
    
    https://github.com/koalaman/shellcheck/wiki/SC2016
    
    This specifically warned about the '$aregis...' part of the sed script.
    Which is a relatively obscure use of sed.
    
    Splitting this into two commands makes its intent more obvious and
    avoids contravening Shellcheck's lints.
    
    Signed-off-by: Dan Callahan <danc@element.io>

--- debian/test/provision.sh
+++ debian/test/provision.sh
@@ -19,5 +19,6 @@ EOF
 
 dpkg -i "$deb"
 
-sed -i -e '/port: 8...$/{s/8448/18448/; s/8008/18008/}' -e '$aregistration_shared_secret: secret' /etc/matrix-synapse/homeserver.yaml
+sed -i -e 's/port: 8448$/port: 18448/; s/port: 8008$/port: 18008' /etc/matrix-synapse/homeserver.yaml
+echo 'registration_shared_secret: secret' >> /etc/matrix-synapse/homeserver.yaml
 systemctl restart matrix-synapse
