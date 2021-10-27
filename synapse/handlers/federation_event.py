75ca0a6168f92dab3255839cf85fb0df3a0076c3
reivilibre <oliverw@matrix.org>
2021-10-27 17:27:23 +0100

    Annotate `log_function` decorator (#10943)
    Co-authored-by: Patrick Cloke <clokep@users.noreply.github.com>

--- synapse/handlers/federation_event.py
+++ synapse/handlers/federation_event.py
@@ -477,7 +477,7 @@ class FederationEventHandler:
 
     @log_function
     async def backfill(
-        self, dest: str, room_id: str, limit: int, extremities: Iterable[str]
+        self, dest: str, room_id: str, limit: int, extremities: Collection[str]
     ) -> None:
         """Trigger a backfill request to `dest` for the given `room_id`
 
