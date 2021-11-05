8f9bf40cb5dd015a61bb4542d0effb72dfb109ff
David Robertson <davidr@element.io>
2021-11-05 14:45:14 +0000

    Update synapse/handlers/room.py
    Co-authored-by: Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>

--- synapse/handlers/room.py
+++ synapse/handlers/room.py
@@ -1383,7 +1383,7 @@ class RoomShutdownHandler:
             else:
                 # But if you don't want to preventatively block another room,
                 # this function can't do anything useful.
-                raise NotFoundError("Unknown room id %s" % (room_id,))
+                raise NotFoundError("Cannot shut down room: unknown room id %s" % (room_id,))
 
         if new_room_user_id is not None:
             if not self.hs.is_mine_id(new_room_user_id):
