d404a4cea8747fdfedb99f17f508d0890326036c
Marcus Hoffmann <bubu@bubu1.eu>
2021-11-22 23:18:31 +0100

    use TypedDict from typing_extensions

--- synapse/storage/databases/main/event_push_actions.py
+++ synapse/storage/databases/main/event_push_actions.py
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-from typing import TYPE_CHECKING, Dict, List, Optional, Tuple, TypedDict, Union
+from typing import TYPE_CHECKING, Dict, List, Optional, Tuple, Union
+from typing_extensions import TypedDict
 
 import attr
 
