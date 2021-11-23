6f8c5d5b775e150c08df36eb5c67c4a6715a8153
Marcus <bubu@bubu1.eu>
2021-11-23 12:42:14 +0100

    Update synapse/push/httppusher.py
    Co-authored-by: Sean Quah <8349537+squahtx@users.noreply.github.com>

--- synapse/push/httppusher.py
+++ synapse/push/httppusher.py
@@ -27,7 +27,7 @@ from synapse.logging import opentracing
 from synapse.metrics.background_process_metrics import run_as_background_process
 from synapse.push import Pusher, PusherConfig, PusherConfigException
 
-from ..storage.databases.main.event_push_actions import HttpPushAction
+from synapse.storage.databases.main.event_push_actions import HttpPushAction
 from . import push_rule_evaluator, push_tools
 
 if TYPE_CHECKING:
