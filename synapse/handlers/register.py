774909429797a5766c8455b4385f5d32a11371dc
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2021-11-29 13:03:40 +0000

    Assert rather than checking something that we know to be true in a sane world

--- synapse/handlers/register.py
+++ synapse/handlers/register.py
@@ -812,10 +812,12 @@ class RegistrationHandler:
             assert access_token_expiry is None
             access_token = self.macaroon_gen.generate_guest_access_token(user_id)
         else:
-            if (
-                should_issue_refresh_token
-                and self.refreshable_access_token_lifetime is not None
-            ):
+            if should_issue_refresh_token:
+                # A refreshable access token lifetime must be configured
+                # since we're told to issue a refresh token (the caller checks
+                # that this value is set before setting this flag).
+                assert self.refreshable_access_token_lifetime is not None
+
                 now_ms = self.clock.time_msec()
 
                 # Set the expiry time of the refreshable access token
