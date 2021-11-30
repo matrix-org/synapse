967755d574d38e59415c5858c32b3fb69bdf262b
Andrew Morgan <andrew@amorgan.xyz>
2021-11-30 14:15:38 +0000

    Fix calling is_interested_in_event with store.

--- synapse/handlers/appservice.py
+++ synapse/handlers/appservice.py
@@ -762,7 +762,7 @@ class ApplicationServicesHandler:
         # inside of a list comprehension anymore.
         interested_list = []
         for s in services:
-            if await s.is_interested_in_event(event, self.store):
+            if await s.is_interested_in_event(event, store=self.store):
                 interested_list.append(s)
 
         return interested_list
