8e129c85514b96ba767a116387fcbc4201a95f86
Patrick Cloke <patrickc@matrix.org>
2021-11-30 14:00:37 -0500

    Lint

--- synapse/config/registration.py
+++ synapse/config/registration.py
@@ -12,9 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Optional
-
 import argparse
+from typing import Optional
 
 from synapse.api.constants import RoomCreationPreset
 from synapse.config._base import Config, ConfigError
