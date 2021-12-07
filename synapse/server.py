db6625e3346b29d22a5f24fec6fca42c6dbabbcc
Jonathan de Jong <jonathan@automatia.nl>
2021-12-07 18:26:37 +0100

    add special case for user directory workers

--- synapse/server.py
+++ synapse/server.py
@@ -836,8 +836,13 @@ class HomeServer(metaclass=abc.ABCMeta):
 
     def should_update_user_directory(self) -> bool:
         "Should this process be updating the user directory?"
+        from synapse.config.server import ANY_USER_DIRECTORY_WORKER
+
         if self.config.worker.worker_app is None:  # we're the main process
             return self.config.server.worker_to_update_user_directory is None
+        elif self.config.server.worker_to_update_user_directory is ANY_USER_DIRECTORY_WORKER:
+            # A special backwards-compatible case for if update_user_directory is False
+            return self.config.worker.worker_app == "synapse.app.user_dir"
         else:
             return (
                 self.config.server.worker_to_update_user_directory
