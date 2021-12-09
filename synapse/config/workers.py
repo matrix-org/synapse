ec9f0357800365a08bf195e0c1562ed867d9dc7b
Jonathan de Jong <jonathan@automatia.nl>
2021-12-09 16:36:33 +0100

    i sort, you sort, we sort

--- synapse/config/workers.py
+++ synapse/config/workers.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 import argparse
+import logging
 from typing import Any, List, Union
 
 import attr
