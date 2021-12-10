d368d3706180a53013768a142ba4ba2b7c711d46
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2021-12-10 14:46:27 +0000

    Fix up type after rebase onto anoa's branch

--- synapse/appservice/api.py
+++ synapse/appservice/api.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 import logging
 import urllib.parse
-from typing import TYPE_CHECKING, Dict, Iterable, List, Optional, Tuple
+from typing import TYPE_CHECKING, Dict, Iterable, List, Optional, Tuple, Union
 
 from prometheus_client import Counter
 
@@ -256,7 +256,7 @@ class ApplicationServiceApi(SimpleHttpClient):
         uri = service.url + ("/transactions/%s" % urllib.parse.quote(str(txn_id)))
 
         # Never send ephemeral events to appservices that do not support it
-        body: Dict[str, List[JsonDict]] = {"events": serialized_events}
+        body: Dict[str, Union[JsonDict, List[JsonDict]]] = {"events": serialized_events}
         if service.supports_ephemeral:
             body.update(
                 {
