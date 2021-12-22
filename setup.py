56e2bde19ab13c848c248e526273c2dae399905e
H. Shay <hillerys@element.io>
2021-12-22 10:03:27 -0800

    update Trove classifiers to remove py36

--- setup.py
+++ setup.py
@@ -162,7 +162,6 @@ setup(
         "Topic :: Communications :: Chat",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
