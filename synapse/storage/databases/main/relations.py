4064b66ca5233457a7de7657b9057525a52f8efb
Patrick Cloke <patrickc@matrix.org>
2021-12-29 13:05:19 -0500

    Remove unneeded room_id from get_thread_summary.

--- synapse/storage/databases/main/relations.py
+++ synapse/storage/databases/main/relations.py
@@ -441,13 +441,12 @@ class RelationsWorkerStore(SQLBaseStore):
 
     @cached()
     async def get_thread_summary(
-        self, event_id: str, room_id: str
+        self, event_id: str
     ) -> Tuple[int, Optional[EventBase]]:
         """Get the number of threaded replies and the latest reply (if any) for the given event.
 
         Args:
             event_id: Summarize the thread related to this event ID.
-            room_id: The room the event belongs to.
 
         Returns:
             The number of items in the thread and the most recent response, if any.
@@ -459,18 +458,19 @@ class RelationsWorkerStore(SQLBaseStore):
             # Fetch the latest event ID in the thread.
             # TODO Should this only allow m.room.message events.
             sql = """
-                SELECT event_id
-                FROM event_relations
-                INNER JOIN events USING (event_id)
+                SELECT child.event_id FROM events AS child
+                INNER JOIN event_relations USING (event_id)
+                INNER JOIN events AS parent ON
+                    parent.event_id = relates_to_id
+                    AND parent.room_id = child.room_id
                 WHERE
                     relates_to_id = ?
-                    AND room_id = ?
                     AND relation_type = ?
-                ORDER BY topological_ordering DESC, stream_ordering DESC
+                ORDER BY child.topological_ordering DESC, child.stream_ordering DESC
                 LIMIT 1
             """
 
-            txn.execute(sql, (event_id, room_id, RelationTypes.THREAD))
+            txn.execute(sql, (event_id, RelationTypes.THREAD))
             row = txn.fetchone()
             if row is None:
                 return 0, None
@@ -479,15 +479,16 @@ class RelationsWorkerStore(SQLBaseStore):
 
             # Fetch the number of threaded replies.
             sql = """
-                SELECT COUNT(event_id)
-                FROM event_relations
-                INNER JOIN events USING (event_id)
+                SELECT COUNT(child.event_id) FROM events AS child
+                INNER JOIN event_relations USING (event_id)
+                INNER JOIN events AS parent ON
+                    parent.event_id = relates_to_id
+                    AND parent.room_id = child.room_id
                 WHERE
                     relates_to_id = ?
-                    AND room_id = ?
                     AND relation_type = ?
             """
-            txn.execute(sql, (event_id, room_id, RelationTypes.THREAD))
+            txn.execute(sql, (event_id, RelationTypes.THREAD))
             count = cast(Tuple[int], txn.fetchone())[0]
 
             return count, latest_event_id
@@ -687,7 +688,7 @@ class RelationsWorkerStore(SQLBaseStore):
         # If this event is the start of a thread, include a summary of the replies.
         if self._msc3440_enabled:
             thread_count, latest_thread_event = await self.get_thread_summary(
-                event_id, room_id
+                event_id
             )
             participated = await self.get_thread_participated(
                 event_id, room_id, user_id
