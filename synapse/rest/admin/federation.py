25d16a1c214289857bf8d0af020ba86daf67e6a1
dklimpel <5740567+dklimpel@users.noreply.github.com>
2022-01-05 19:19:56 +0100

    uses `Authenticator._reset_retry_timings` and fix import loop

--- synapse/rest/admin/federation.py
+++ synapse/rest/admin/federation.py
@@ -16,6 +16,7 @@ from http import HTTPStatus
 from typing import TYPE_CHECKING, Tuple
 
 from synapse.api.errors import Codes, NotFoundError, SynapseError
+from synapse.federation.transport.server._base import Authenticator
 from synapse.http.servlet import RestServlet, parse_integer, parse_string
 from synapse.http.site import SynapseRequest
 from synapse.rest.admin._base import admin_patterns, assert_requester_is_admin
@@ -165,7 +166,7 @@ class DestinationResetConnectionRestServlet(RestServlet):
     def __init__(self, hs: "HomeServer"):
         self._auth = hs.get_auth()
         self._store = hs.get_datastore()
-        self._notifier = hs.get_notifier()
+        self._authenticator = Authenticator(hs)
 
     async def on_POST(
         self, request: SynapseRequest, destination: str
@@ -182,8 +183,7 @@ class DestinationResetConnectionRestServlet(RestServlet):
                 "The retry timing does not need to be reset for this destination.",
             )
 
-        # reset timings and wake up, this is based on `Authenticator._reset_retry_timings`
-        await self._store.set_destination_retry_timings(destination, None, 0, 0)
-        self._notifier.notify_remote_server_up(destination)
+        # reset timings and wake up
+        await self._authenticator.reset_retry_timings(destination)
 
         return HTTPStatus.OK, {}
