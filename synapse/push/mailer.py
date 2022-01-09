8ccb8d4a6f361c7a319820eab7b7876ae5927c95
br4nnigan <10244835+br4nnigan@users.noreply.github.com>
2022-01-09 22:36:59 +0100

    fix missing app variable in mail subject
    documentation claims that you can use the %(app)s variable in password_reset and email_validation subjects, but if you do you end up with an error 500

--- synapse/push/mailer.py
+++ synapse/push/mailer.py
@@ -178,7 +178,10 @@ class Mailer:
         await self.send_email(
             email_address,
             self.email_subjects.email_validation
-            % {"server_name": self.hs.config.server.server_name},
+            % {
+                "server_name": self.hs.config.server.server_name,
+                "app": self.app_name
+            },
             template_vars,
         )
 
@@ -209,7 +212,10 @@ class Mailer:
         await self.send_email(
             email_address,
             self.email_subjects.email_validation
-            % {"server_name": self.hs.config.server.server_name},
+            % {
+                "server_name": self.hs.config.server.server_name,
+                "app": self.app_name
+            },
             template_vars,
         )
 
