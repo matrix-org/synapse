0a566a93ebe62ffa579834fcb01ca4f9e783db05
H. Shay <hillerys@element.io>
2022-01-10 10:37:29 -0800

    bump lxml depenency

--- synapse/python_dependencies.py
+++ synapse/python_dependencies.py
@@ -107,7 +107,7 @@ CONDITIONAL_REQUIREMENTS = {
     # `systemd.journal.JournalHandler`, as is documented in
     # `contrib/systemd/log_config.yaml`.
     "systemd": ["systemd-python>=231"],
-    "url_preview": ["lxml>=3.5.0"],
+    "url_preview": ["lxml>=4.2.0"],
     "sentry": ["sentry-sdk>=0.7.2"],
     "opentracing": ["jaeger-client>=4.0.0", "opentracing>=2.2.0"],
     "jwt": ["pyjwt>=1.6.4"],
