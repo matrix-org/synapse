451b4ff5385382a14b6ebf2c4fb2e7b813e1a09e
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2022-01-11 14:58:51 +0000

    Check for 2 free slots before adding a cross-signing key update

--- synapse/storage/databases/main/devices.py
+++ synapse/storage/databases/main/devices.py
@@ -300,6 +300,19 @@ class DeviceWorkerStore(SQLBaseStore):
                 and device_id == self_signing_key_by_user[user_id]["device_id"]
             )
 
+            is_cross_signing_key_update = (
+                is_master_key_update or is_self_signing_key_update
+            )
+
+            if (
+                is_cross_signing_key_update
+                and user_id not in cross_signing_keys_by_user
+            ):
+                # This will give rise to 2 device updates.
+                # If we don't have the budget, stop here!
+                if remaining_length_budget < 2:
+                    break
+
             if is_master_key_update:
                 result = cross_signing_keys_by_user.setdefault(user_id, {})
                 result["master_key"] = master_key_by_user[user_id]["key_info"]
@@ -310,6 +323,10 @@ class DeviceWorkerStore(SQLBaseStore):
                 ]
             else:
                 key = (user_id, device_id)
+
+                if key not in query_map and remaining_length_budget < 1:
+                    # We don't have space for a new entry
+                    break
 
                 previous_update_stream_id, _ = query_map.get(key, (0, None))
 
