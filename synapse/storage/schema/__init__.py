89ec7a655eebdfd401eba03bb4adec5c4235836f
Richard van der Hoff <richard@matrix.org>
2022-01-20 22:26:24 +0000

    Stop reading from `event_refrence_hashes`
    Preparation for dropping this table altogether. Part of #6574.

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SCHEMA_VERSION = 67  # remember to update the list below when updating
+SCHEMA_VERSION = 68  # remember to update the list below when updating
 """Represents the expectations made by the codebase about the database schema
 
 This should be incremented whenever the codebase changes its requirements on the
@@ -53,6 +53,9 @@ Changes in SCHEMA_VERSION = 66:
 
 Changes in SCHEMA_VERSION = 67:
     - state_events.prev_state is no longer written to.
+
+Changes in SCHEMA_VERSION = 68:
+    - event_reference_hashes is no longer read.
 """
 
 
