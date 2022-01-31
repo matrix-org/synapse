34d2577f9fbf71f1008fdd9a11762edf781f7530
Richard van der Hoff <richard@matrix.org>
2022-01-31 23:46:04 +0000

    Improve opentracing for federation requests
    The idea here is to set the parent span for incoming federation requests to the
    *outgoing* span on the other end. That means that you can see (most of) the
    full end-to-end flow when you have a process that includes federation requests.
    
    However, in order not to lose information, we still want a link to the
    `incoming-federation-request` span from the servlet, so we have to create
    another span to do exactly that.

--- synapse/logging/opentracing.py
+++ synapse/logging/opentracing.py
@@ -468,13 +468,25 @@ def start_active_span(
 
 
 def start_active_span_follows_from(
-    operation_name: str, contexts: Collection, inherit_force_tracing=False
+    operation_name: str,
+    contexts: Collection,
+    child_of=None,
+    start_time: Optional[float] = None,
+    inherit_force_tracing=False,
 ):
     """Starts an active opentracing span, with additional references to previous spans
 
     Args:
         operation_name: name of the operation represented by the new span
         contexts: the previous spans to inherit from
+
+        child_of: optionally override the parent span. If unset, the currently active
+           span will be the parent. (If there is no currently active span, the first
+           span in `contexts` will be the parent.)
+
+        start_time: optional override for the start time of the created span. Seconds
+            since the epoch.
+
         inherit_force_tracing: if set, and any of the previous contexts have had tracing
            forced, the new span will also have tracing forced.
     """
@@ -482,7 +494,12 @@ def start_active_span_follows_from(
         return noop_context_manager()  # type: ignore[unreachable]
 
     references = [opentracing.follows_from(context) for context in contexts]
-    scope = start_active_span(operation_name, references=references)
+    scope = start_active_span(
+        operation_name,
+        child_of=child_of,
+        references=references,
+        start_time=start_time,
+    )
 
     if inherit_force_tracing and any(
         is_context_forced_tracing(ctx) for ctx in contexts
