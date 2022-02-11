d66cbdfc28b90590c1a1be276931a109a915cca7
H. Shay <hillerys@element.io>
2022-02-11 15:24:07 -0800

    fix license date

--- synapse/config/background_updates.py
+++ synapse/config/background_updates.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2021 Matrix.org Foundation C.I.C.
+# Copyright 2022 Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
