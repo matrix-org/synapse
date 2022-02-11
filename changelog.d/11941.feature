4d7e74b2e503373da66ec929afd1aa7010676878
Patrick Cloke <clokep@users.noreply.github.com>
2022-02-11 11:20:27 -0500

    Support the MSC3715 for `/relations`. (#11941)
    This adds an unstable org.matrix.msc3715.dir parameter
    which acts like dir on /mesages.

---
+++ changelog.d/11941.feature
@@ -0,0 +1,1 @@
+Support the `dir` parameter on the `/relations` endpoint, per [MSC3715](https://github.com/matrix-org/matrix-doc/pull/3715).
--- synapse/rest/client/relations.py
+++ synapse/rest/client/relations.py
@@ -111,6 +111,9 @@ class RelationPaginationServlet(RestServlet):
             raise SynapseError(404, "Unknown parent event.")
 
         limit = parse_integer(request, "limit", default=5)
+        direction = parse_string(
+            request, "org.matrix.msc3715.dir", default="b", allowed_values=["f", "b"]
+        )
         from_token_str = parse_string(request, "from")
         to_token_str = parse_string(request, "to")
 
@@ -128,6 +131,7 @@ class RelationPaginationServlet(RestServlet):
                 relation_type=relation_type,
                 event_type=event_type,
                 limit=limit,
+                direction=direction,
                 from_token=from_token,
                 to_token=to_token,
             )
