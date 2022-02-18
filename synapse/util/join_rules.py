475c7587b55fec18435486d447fb975fafaeb523
Travis Ralston <travisr@matrix.org>
2022-02-17 22:28:31 -0700

    This linter makes me unhappy

--- synapse/util/join_rules.py
+++ synapse/util/join_rules.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Optional
+
 from synapse.api.constants import JoinRules
 from synapse.api.room_versions import RoomVersion
 from synapse.events import EventBase
-from typing import Optional
 
 
 def is_join_rule(
