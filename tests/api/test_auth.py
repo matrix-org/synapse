32648b2972217557e0e35dc6687e9e12d5579307
Richard van der Hoff <richard@matrix.org>
2022-02-22 13:51:09 +0000

    fix olddeps build

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -16,7 +16,7 @@ from unittest.mock import Mock
 
 import pymacaroons
 
-from twisted.internet.testing import MemoryReactor
+from twisted.test.proto_helpers import MemoryReactor
 
 from synapse.api.auth import Auth
 from synapse.api.constants import UserTypes
