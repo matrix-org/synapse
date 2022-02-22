81364db49b7778021edcd5912555dd3e1583c1b8
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2022-02-22 13:33:22 +0000

    Run `_handle_queued_pdus` as a background process (#12041)
    ... to ensure it gets a proper log context, mostly.

--- synapse/handlers/federation.py
+++ synapse/handlers/federation.py
@@ -49,8 +49,8 @@ from synapse.logging.context import (
     make_deferred_yieldable,
     nested_logging_context,
     preserve_fn,
-    run_in_background,
 )
+from synapse.metrics.background_process_metrics import run_as_background_process
 from synapse.replication.http.federation import (
     ReplicationCleanRoomRestServlet,
     ReplicationStoreRoomOnOutlierMembershipRestServlet,
@@ -559,7 +559,9 @@ class FederationHandler:
             # lots of requests for missing prev_events which we do actually
             # have. Hence we fire off the background task, but don't wait for it.
 
-            run_in_background(self._handle_queued_pdus, room_queue)
+            run_as_background_process(
+                "handle_queued_pdus", self._handle_queued_pdus, room_queue
+            )
 
     async def do_knock(
         self,
