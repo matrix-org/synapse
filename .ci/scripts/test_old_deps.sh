050a6cdd5d48e3bbec29c5bb96a0e1b2ee094eb3
David Robertson <davidr@element.io>
2022-03-11 15:40:31 +0000

    rollback?

--- .ci/scripts/test_old_deps.sh
+++ .ci/scripts/test_old_deps.sh
@@ -23,20 +23,15 @@ export VIRTUALENV_NO_DOWNLOAD=1
 # rather than this sed script. But that's an Opinion.
 
 # patch the project definitions in-place
-# - replace all lower bounds with exact bounds
-# - delete all lines referring to psycopg2 --- so no postgres support
-# - but make the pyopenssl 17.0, which can work against an
-# - OpenSSL 1.1 compiled cryptography (as older ones don't compile on Travis).
-# - remove pygithub from dev dependencies, because this wants a higher version of
-#   pynacl than our minimum and we're not using it here
-# - remove systemd-python. This used to be omitted when running `python_dependencies.py`
-#   as a script; we preserve this behaviour here.
+# replace all lower bounds with exact bounds
+# delete all lines referring to psycopg2 --- so no postgres support
+# but make the pyopenssl 17.0, which can work against an
+# OpenSSL 1.1 compiled cryptography (as older ones don't compile on Travis).
+
 sed -i-backup \
    -e "s/[~>]=/==/g" \
    -e "/psycopg2/d" \
    -e 's/pyOpenSSL = "==16.0.0"/pyOpenSSL = "==17.0.0"/' \
-   -e '/pygithub/d' \
-   -e '/systemd/d' \
    pyproject.toml
 
 # There are almost certainly going to be dependency conflicts there, so I'm going to
@@ -48,11 +43,15 @@ sed -i-backup \
 # https://github.com/python-poetry/poetry/issues/34#issuecomment-1055142428
 # So instead, make a virtualenv and install in there.
 
-pipx install poetry==1.1.12
-~/.local/bin/poetry lock
-~/.local/bin/poetry install --extras all
+python3 -m venv env
+source env/bin/activate
+# At the time of writing, this notes:
+#  ERROR: ldap3 2.9.1 has requirement pyasn1>=0.4.6, but you'll have pyasn1 0.1.9 which is incompatible.
+# Helpfully, pip doesn't indicate the error; it returns 0.
+# TODO: bump pyasn to >=0.4.6?
+pip install .[all,test]
 
 # I've no idea why, but trial complains
 #     twisted.python.reflect.ModuleNotFound: No module named 'tests'
 # Unless I invoke trial in this way.
-~/.local/bin/poetry run trial -j 2 tests
+python3 -m twisted.trial -j 2 tests
