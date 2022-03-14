4ab106b6166749d8ecd9e7a6bfbe652cb78f87e5
David Robertson <davidr@element.io>
2022-03-14 13:10:13 +0000

    WIP: Try to fix twisted trunk job
    This is a dummy message to change the commit to force another CI run

--- .ci/patch_for_twisted_trunk.sh
+++ .ci/patch_for_twisted_trunk.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# replaces the dependency on Twisted in `python_dependencies` with trunk.
+# replaces the dependency on Twisted with Twisted's trunk.
+# We make full use of the poetry machinery (rather than just `pip install ...`)
+# so that we'll catch dependency resolver problems that could arise from twisted
+# bumping its dependencies.
 
-set -e
+set -xe
 cd "$(dirname "$0")"/..
 
-sed -i -e 's#"Twisted.*"#"Twisted @ git+https://github.com/twisted/twisted"#' synapse/python_dependencies.py
+sed -ibackup -e 's!^Twisted = .*!Twisted = { git = "https://github.com/twisted/twisted.git", rev = "trunk" }!' pyproject.toml
+
+poetry lock --no-update
+poetry install --no-interaction --extras "all test"
+# Confirm the version of twisted in use
+poetry run pip show twisted
