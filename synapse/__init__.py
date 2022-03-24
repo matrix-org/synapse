088f3ae1823f335b6fa6d470a49d55a4c3cfdf0a
Andrew Morgan <andrewm@element.io>
2022-03-24 17:47:03 +0000

    1.55.1

--- synapse/__init__.py
+++ synapse/__init__.py
@@ -68,7 +68,7 @@ try:
 except ImportError:
     pass
 
-__version__ = "1.55.0"
+__version__ = "1.55.1"
 
 if bool(os.environ.get("SYNAPSE_TEST_PATCH_LOG_CONTEXTS", False)):
     # We import here so that we don't have to install a bunch of deps when
