339fe3f4c677b801a7f90990aaa6886717bdc20b
Sean Quah <seanq@element.io>
2022-04-05 16:10:25 +0100

    fixup merge

--- tests/util/test_linearizer.py
+++ tests/util/test_linearizer.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, Hashable, Tuple
+from typing import Hashable, Tuple
 
 from typing_extensions import Protocol
 
