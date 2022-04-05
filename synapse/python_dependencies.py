1e5690e5a07ab0028eed67a7d9e4af6c0356a35c
David Robertson <davidr@element.io>
2022-04-05 18:35:47 +0100

    setuptools doesn't like my environment marker
    This is only temporary, so screw it: let's make it unconditional.

--- synapse/python_dependencies.py
+++ synapse/python_dependencies.py
@@ -91,7 +91,7 @@ REQUIREMENTS = [
     "packaging>=16.1",
     # At the time of writing, we only use functions from the version `importlib.metadata`
     # which shipped in Python 3.8. This corresponds to version 1.4 of the backport.
-    "importlib_metadata>=1.4; python_version < 3.8",
+    "importlib_metadata>=1.4",
 ]
 
 CONDITIONAL_REQUIREMENTS = {
