3f2e23684612ebf21830fa3e484790989ba3be8b
David Robertson <davidr@element.io>
2022-04-07 17:31:17 +0100

    Note that `doseq=True` is required

--- synapse/http/types.py
+++ synapse/http/types.py
@@ -1,6 +1,6 @@
 from typing import Iterable, Mapping, Union
 
-# the type of the query params, to be passed into `urlencode`
+# the type of the query params, to be passed into `urlencode` with `doseq=True`.
 QueryParamValue = Union[str, bytes, Iterable[Union[str, bytes]]]
 QueryParams = Union[Mapping[str, QueryParamValue], Mapping[bytes, QueryParamValue]]
 
