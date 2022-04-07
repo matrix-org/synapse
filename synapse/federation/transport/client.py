matrix-org/synapse

commit 42d8710f38d465904dfe2823d132c299b94f7a3a

Fix a spec compliance issue where requests to the `/publicRooms` federation API would specify `limit` as a string. (#12364)

synapse/federation/transport/client.py
@@ -481,7 +481,7 @@ class TransportLayerClient:
             if third_party_instance_id:
                 data["third_party_instance_id"] = third_party_instance_id
             if limit:
-                data["limit"] = str(limit)
+                data["limit"] = limit
             if since_token:
                 data["since"] = since_token
 
@@ -509,7 +509,7 @@ class TransportLayerClient:
             if third_party_instance_id:
                 args["third_party_instance_id"] = (third_party_instance_id,)
             if limit:
-                args["limit"] = [str(limit)]
+                args["limit"] = [limit]
             if since_token:
                 args["since"] = [since_token]
 

commit 8b4b0655e63981ee9c1f48b8fca738efc568758f

Fix fetching public rooms over federation
Broke by #12364

synapse/federation/transport/client.py
@@ -509,7 +509,7 @@ class TransportLayerClient:
             if third_party_instance_id:
                 args["third_party_instance_id"] = (third_party_instance_id,)
             if limit:
-                args["limit"] = [limit]
+                args["limit"] = [str(limit)]
             if since_token:
                 args["since"] = [since_token]
 