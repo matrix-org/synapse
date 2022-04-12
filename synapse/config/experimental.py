86cf6a3a17376dd83a9222f77521d1619b528d66
Patrick Cloke <clokep@users.noreply.github.com>
2022-04-12 08:42:03 -0400

    Remove references to unstable identifiers from MSC3440. (#12382)
    Removes references to unstable thread relation, unstable
    identifiers for filtering parameters, and the experimental
    config flag.

--- synapse/config/experimental.py
+++ synapse/config/experimental.py
@@ -26,9 +26,6 @@ class ExperimentalConfig(Config):
     def read_config(self, config: JsonDict, **kwargs: Any) -> None:
         experimental = config.get("experimental_features") or {}
 
-        # MSC3440 (thread relation)
-        self.msc3440_enabled: bool = experimental.get("msc3440_enabled", False)
-
         # MSC3026 (busy presence state)
         self.msc3026_enabled: bool = experimental.get("msc3026_enabled", False)
 
