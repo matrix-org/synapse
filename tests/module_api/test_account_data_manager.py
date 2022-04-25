38ece6087531b7e596d1cd475f83250b87506ca7
David Robertson <davidr@element.io>
2022-04-25 12:11:27 +0100

    Fix test twisted import for olddeps

--- tests/module_api/test_account_data_manager.py
+++ tests/module_api/test_account_data_manager.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from twisted.internet.testing import MemoryReactor
+from twisted.test.proto_helpers import MemoryReactor
 
 from synapse.api.errors import SynapseError
 from synapse.rest import admin
