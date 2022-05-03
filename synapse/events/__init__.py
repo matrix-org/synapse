77dee1b451aa5c5c3644e5e4e5afefda2a44aa15
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2022-05-03 13:59:28 +0100

    fix imports
    broken in 5938928 :-S

--- synapse/events/__init__.py
+++ synapse/events/__init__.py
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import abcimport os
+import abc
+import os
 from typing import (
     TYPE_CHECKING,
     Any,
