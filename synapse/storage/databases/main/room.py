77f65b012942e1a4cd58907d7f64e3d59ded0c87
Brendan Abolivier <babolivier@matrix.org>
2022-05-03 10:33:56 +0100

    Lint

--- synapse/storage/databases/main/room.py
+++ synapse/storage/databases/main/room.py
@@ -699,7 +699,9 @@ class RoomWorkerStore(CacheInvalidationWorkerStore):
         await self.db_pool.runInteraction("delete_ratelimit", delete_ratelimit_txn)
 
     @cached()
-    async def get_retention_policy_for_room(self, room_id: str) -> Dict[str, Optional[int]]:
+    async def get_retention_policy_for_room(
+        self, room_id: str
+    ) -> Dict[str, Optional[int]]:
         """Get the retention policy for a given room.
 
         If no retention policy has been found for this room, returns a policy defined
