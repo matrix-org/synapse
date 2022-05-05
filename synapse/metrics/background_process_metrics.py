8459efa6e8d4b2b5623265fb87fe2c23ae9cc3a4
David Robertson <davidr@element.io>
2022-05-06 00:49:33 +0100

    Try to fix complaints about unawaited coroutines

--- synapse/metrics/background_process_metrics.py
+++ synapse/metrics/background_process_metrics.py
@@ -28,11 +28,11 @@ from typing import (
     Type,
     TypeVar,
     Union,
-    cast,
 )
 
 from prometheus_client import Metric
 from prometheus_client.core import REGISTRY, Counter, Gauge
+from typing_extensions import ParamSpec
 
 from twisted.internet import defer
 
@@ -256,24 +256,51 @@ def run_as_background_process(
         return defer.ensureDeferred(run())
 
 
-F = TypeVar("F", bound=Callable[..., Awaitable[Optional[Any]]])
+P = ParamSpec("P")
 
 
-def wrap_as_background_process(desc: str) -> Callable[[F], F]:
-    """Decorator that wraps a function that gets called as a background
-    process.
+def wrap_as_background_process(
+    desc: str,
+) -> Callable[
+    [Callable[P, Awaitable[Optional[R]]]],
+    Callable[P, "defer.Deferred[Optional[R]]"],
+]:
+    """Decorator that wraps an asynchronous function `func`, returning a synchronous
+    decorated function. Calling the decorated version runs `func` as a background
+    process, forwarding all arguments verbatim.
 
-    Equivalent to calling the function with `run_as_background_process`
+    That is,
+
+        @wrap_as_background_process
+        def func(*args): ...
+        func(1, 2, third=3)
+
+    is equivalent to:
+
+        def func(*args): ...
+        run_as_background_process(func, 1, 2, third=3)
+
+    The former can be convenient if `func` needs to be run as a background process in
+    multiple places.
     """
-
-    def wrap_as_background_process_inner(func: F) -> F:
+    # Note: the decorated version of `func` (`wrap_as_background_process_inner`) could be
+    # considered an asynchronous function. However, recent mypy versions warn us that
+    # we have forgotten to `await` a coroutine if we mark the decorated function as
+    # returning an Awaitable.
+    #
+    #     error: Value of type "Coroutine[Any, Any, None]" must be used  [unused-coroutine]
+    #     note: Are you missing an await?
+    # It seems happier when the decorated version of `func` returns a Deferred.
+    def wrap_as_background_process_inner(
+        func: Callable[P, Awaitable[Optional[R]]]
+    ) -> Callable[P, "defer.Deferred[Optional[R]]"]:
         @wraps(func)
         def wrap_as_background_process_inner_2(
-            *args: Any, **kwargs: Any
+            *args: P.args, **kwargs: P.kwargs
         ) -> "defer.Deferred[Optional[R]]":
             return run_as_background_process(desc, func, *args, **kwargs)
 
-        return cast(F, wrap_as_background_process_inner_2)
+        return wrap_as_background_process_inner_2
 
     return wrap_as_background_process_inner
 
