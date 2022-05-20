99ef23d2d22ead001e0ab420fe4b34439d39ff11
Travis Ralston <travisr@matrix.org>
2022-05-19 19:02:18 -0600

    Remove defunct opt2 room version

--- synapse/api/room_versions.py
+++ synapse/api/room_versions.py
@@ -322,24 +322,6 @@ class RoomVersions:
         msc2716_redactions=False,
         msc3667_int_only_power_levels=True,
     )
-    MSC3604opt2 = RoomVersion(
-        # v9 + MSC2176 + MSC3667
-        "org.matrix.msc3604.opt2",
-        RoomDisposition.UNSTABLE,
-        EventFormatVersions.V3,
-        StateResolutionVersions.V2,
-        enforce_key_validity=True,
-        special_case_aliases_auth=False,
-        strict_canonicaljson=True,
-        limit_notifications_power_levels=True,
-        msc2176_redaction_rules=True,
-        msc3083_join_rules=True,
-        msc3375_redaction_rules=True,
-        msc2403_knocking=True,
-        msc2716_historical=False,
-        msc2716_redactions=False,
-        msc3667_int_only_power_levels=True,
-    )
 
 
 KNOWN_ROOM_VERSIONS: Dict[str, RoomVersion] = {
@@ -358,7 +340,6 @@ KNOWN_ROOM_VERSIONS: Dict[str, RoomVersion] = {
         RoomVersions.MSC2716v3,
         RoomVersions.MSC3787,
         RoomVersions.MSC3667,
-        RoomVersions.MSC3604opt2,
     )
 }
 
