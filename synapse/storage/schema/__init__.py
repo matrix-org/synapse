4bc172f652abb3e4a55263b9c2a21d6783c64159
Richard van der Hoff <richard@matrix.org>
2022-05-26 23:45:36 +0100

    Clean up the event_edges table
    We make a number of changes to `event_edges`:
    
     * We give the `room_id` and `is_state` columns defaults (null and false
       respectively) so that we can stop populating them.
     * We drop any rows that have `is_state` set true - they should no longer
       exist.
     * We drop any rows that do not exist in `events` - these should not exist
       either.
     * We drop the old unique constraint on all the colums, which wasn't much use.
     * We create a new unique index on `(event_id, prev_event_id)`.
     * We add a foreign key constraint to `events`.
    
    These happen rather differently depending on whether we are on Postgres or
    SQLite. For SQLite, we just rebuild the whole table, copying only the rows we
    want to keep. For Postgres, we try to do things in the background as much as
    possible.

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SCHEMA_VERSION = 70  # remember to update the list below when updating
+SCHEMA_VERSION = 71  # remember to update the list below when updating
 """Represents the expectations made by the codebase about the database schema
 
 This should be incremented whenever the codebase changes its requirements on the
@@ -67,6 +67,9 @@ Changes in SCHEMA_VERSION = 69:
 
 Changes in SCHEMA_VERSION = 70:
     - event_reference_hashes is no longer written to.
+
+Changes in SCHEMA_VERSION = 71:
+    - event_edges.(room_id, is_state) are no longer written to.
 """
 
 
