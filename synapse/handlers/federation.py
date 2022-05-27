55460dacf80ecdc0d0995b743e8a238681045532
Sean Quah <seanq@matrix.org>
2022-05-27 12:42:17 +0100

    Complain if 0 destinations have been provided

--- synapse/handlers/federation.py
+++ synapse/handlers/federation.py
@@ -1482,6 +1482,9 @@ class FederationHandler:
         #   really leave, that might mean we have difficulty getting the room state over
         #   federation.
 
+        if initial_destination is None and len(other_destinations) == 0:
+            raise ValueError(f"Cannot resync state of {room_id}: no destinations provided")
+
         # Make an infinite iterator of destinations to try. Once we find a working
         # destination, we'll stick with it until it flakes.
         if initial_destination is not None:
