81cf5d073ca1c198f3b9e3ba9608454b056af545
David Robertson <davidr@element.io>
2022-06-14 15:07:52 +0100

    Batch of suggestions from Patrick
    Co-authored-by: Patrick Cloke <clokep@users.noreply.github.com>

--- synapse/storage/databases/main/devices.py
+++ synapse/storage/databases/main/devices.py
@@ -78,8 +78,8 @@ class DeviceWorkerStore(EndToEndKeyWorkerStore):
     ):
         super().__init__(database, db_conn, hs)
 
-        # Type-ignore: _device_list_id_gen is mixed in from either DataStore, where it
-        # is a StreamIdGenerator or SlavedDataStore where it is a SlavedIdTracker.
+        # Type-ignore: _device_list_id_gen is mixed in from either DataStore (as a
+        # StreamIdGenerator) or SlavedDataStore (as a SlavedIdTracker).
         device_list_max = self._device_list_id_gen.get_current_token()  # type: ignore[attr-defined]
         device_list_prefill, min_device_list_id = self.db_pool.get_cache_dict(
             db_conn,
@@ -345,6 +345,7 @@ class DeviceWorkerStore(EndToEndKeyWorkerStore):
         # following this stream later.
         last_processed_stream_id = from_stream_id
 
+        # A map of (user ID, device ID) to (stream ID, context).
         query_map: Dict[Tuple[str, str], Tuple[int, Optional[str]]] = {}
         cross_signing_keys_by_user: Dict[str, Dict[str, object]] = {}
         for user_id, device_id, update_stream_id, update_context in updates:
@@ -627,8 +628,6 @@ class DeviceWorkerStore(EndToEndKeyWorkerStore):
             The new stream ID.
         """
 
-        # Type-ignore: _device_list_id_gen is mixed in from either DataStore, where it
-        # is a StreamIdGenerator or SlavedDataStore where it is a SlavedIdTracker.
         # TODO: this looks like it's _writing_. Should this be on DeviceStore rather
         #  than DeviceWorkerStore?
         async with self._device_list_id_gen.get_next() as stream_id:  # type: ignore[attr-defined]
@@ -1676,8 +1675,6 @@ class DeviceStore(DeviceWorkerStore, DeviceBackgroundUpdateStore):
                 context,
             )
 
-        # Type-ignore: _device_list_id_gen is mixed in from either DataStore, where it
-        # is a StreamIdGenerator or SlavedDataStore where it is a SlavedIdTracker.
         async with self._device_list_id_gen.get_next_mult(  # type: ignore[attr-defined]
             len(device_ids)
         ) as stream_ids:
@@ -1902,7 +1899,6 @@ class DeviceStore(DeviceWorkerStore, DeviceBackgroundUpdateStore):
                 [],
             )
 
-        # Type-ignore: _device_list_id_gen is mixed in from either DataStore where it
         # is a StreamIdGenerator, or SlavedDataStore where it is a SlavedIdTracker.
         async with self._device_list_id_gen.get_next_mult(len(hosts)) as stream_ids:  # type: ignore[attr-defined]
             return await self.db_pool.runInteraction(
