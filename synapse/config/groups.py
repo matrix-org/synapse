493c2fc44abcf3457953cc2f6f23509ff7855253
Shay <hillerys@element.io>
2022-06-14 07:53:42 -0700

    Remove code generating comments in configuration file (#12941)

--- synapse/config/groups.py
+++ synapse/config/groups.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Matrix.org Foundation C.I.C.
+# Copyright 2017 New Vector Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import Any
 
 from synapse.types import JsonDict
@@ -18,20 +19,9 @@ from synapse.types import JsonDict
 from ._base import Config
 
 
-class BackgroundUpdateConfig(Config):
-    section = "background_updates"
+class GroupsConfig(Config):
+    section = "groups"
 
     def read_config(self, config: JsonDict, **kwargs: Any) -> None:
-        bg_update_config = config.get("background_updates") or {}
-
-        self.update_duration_ms = bg_update_config.get(
-            "background_update_duration_ms", 100
-        )
-
-        self.sleep_enabled = bg_update_config.get("sleep_enabled", True)
-
-        self.sleep_duration_ms = bg_update_config.get("sleep_duration_ms", 1000)
-
-        self.min_batch_size = bg_update_config.get("min_batch_size", 1)
-
-        self.default_batch_size = bg_update_config.get("default_batch_size", 100)
+        self.enable_group_creation = config.get("enable_group_creation", False)
+        self.group_creation_prefix = config.get("group_creation_prefix", "")
