6b47e82ca2e4b3709bb55e1ba98079aaf4e96589
David Robertson <davidr@element.io>
2022-06-29 12:28:53 +0100

    Track per-room join rates actioned by this worker
    and consult it when actioning joins.
    Only bump rate limit if we will persist the event; otherwise we'll see
    it over replication

--- synapse/handlers/room_member.py
+++ synapse/handlers/room_member.py
@@ -103,6 +103,19 @@ class RoomMemberHandler(metaclass=abc.ABCMeta):
             rate_hz=hs.config.ratelimiting.rc_joins_remote.per_second,
             burst_count=hs.config.ratelimiting.rc_joins_remote.burst_count,
         )
+        # TODO: find a better place to keep this Ratelimiter.
+        #   It needs to be
+        #    - written to by something which can snoop on replication streams
+        #    - read by the RoomMemberHandler to rate limit joins from local users
+        #    - read by the FederationServer to rate limit make_joins and send_joins from
+        #      other homeservers
+        #   I wonder if a homeserver-wide collection of rate limiters might be cleaner?
+        self._join_rate_per_room_limiter = Ratelimiter(
+            store=self.store,
+            clock=self.clock,
+            rate_hz=hs.config.ratelimiting.rc_joins_per_room.per_second,
+            burst_count=hs.config.ratelimiting.rc_joins_per_room.burst_count,
+        )
 
         self._invites_per_room_limiter = Ratelimiter(
             store=self.store,
@@ -125,6 +138,18 @@ class RoomMemberHandler(metaclass=abc.ABCMeta):
         )
 
         self.request_ratelimiter = hs.get_request_ratelimiter()
+        hs.get_notifier().add_new_join_in_room_callback(self._on_user_joined_room)
+
+    def _on_user_joined_room(self, event_id: str, room_id: str) -> None:
+        """Notify the rate limiter that a room join has occurred.
+
+        Use this to inform the RoomMemberHandler about joins that have either
+        - taken place on another homeserver, or
+        - on another worker in this homeserver.
+        Joins actioned by this worker should use the usual `ratelimit` method, which
+        checks the limit and increments the counter in one go.
+        """
+        self._join_rate_per_room_limiter.record_action(requester=None, key=room_id)
 
     @abc.abstractmethod
     async def _remote_join(
@@ -378,6 +403,11 @@ class RoomMemberHandler(metaclass=abc.ABCMeta):
             # up blocking profile updates.
             if newly_joined and ratelimit:
                 await self._join_rate_limiter_local.ratelimit(requester)
+                await self._join_rate_per_room_limiter.ratelimit(
+                    requester,
+                    key=room_id,
+                    update=self.hs.persists_events_for_room(room_id),
+                )
 
         result_event = await self.event_creation_handler.handle_new_client_event(
             requester,
@@ -825,6 +855,11 @@ class RoomMemberHandler(metaclass=abc.ABCMeta):
                 if ratelimit:
                     await self._join_rate_limiter_remote.ratelimit(
                         requester,
+                    )
+                    await self._join_rate_per_room_limiter.ratelimit(
+                        requester,
+                        key=room_id,
+                        update=self.hs.persists_events_for_room(room_id),
                     )
 
                 inviter = await self._get_inviter(target.to_string(), room_id)
