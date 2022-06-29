484024438c6fbb59ac5bb0e9f625e7e6df69ea55
Šimon Brandner <simon.bra.ag@gmail.com>
2022-06-29 06:11:24 +0200

    Improve comment
    Co-authored-by: Sean Quah <8349537+squahtx@users.noreply.github.com>

--- synapse/api/constants.py
+++ synapse/api/constants.py
@@ -261,7 +261,10 @@ class ReceiptTypes:
     FULLY_READ: Final = "m.fully_read"
 
 
-# As defined in https://spec.matrix.org/v1.3/client-server-api/#post_matrixclientv3publicrooms
 class PublicRoomsFilterFields:
+    """Fields in the search filter for `/publicRooms` that we understand.
+    
+    As defined in https://spec.matrix.org/v1.3/client-server-api/#post_matrixclientv3publicrooms
+    """
     GENERIC_SEARCH_TERM: Final = "generic_search_term"
     ROOM_TYPES: Final = "org.matrix.msc3827.room_types"
