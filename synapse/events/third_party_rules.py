b3e735bcad58b5244879a11670862cdb0ee9a436
David Teller <d.o.teller+github@gmail.com>
2022-07-05 11:13:38 +0200

    WIP: More general fix

--- synapse/events/third_party_rules.py
+++ synapse/events/third_party_rules.py
@@ -397,7 +397,9 @@ class ThirdPartyEventRules:
             return
 
         event = await self.store.get_event(event_id)
-        state_events = await self._get_state_map_for_room(event.room_id)
+        state_events = await self._storage_controllers.state.get_current_state(
+            event.room_id
+        )
 
         for callback in self._on_new_event_callbacks:
             try:
@@ -464,18 +466,7 @@ class ThirdPartyEventRules:
         Returns:
             A dict mapping (event type, state key) to state event.
         """
-        state_ids = await self._storage_controllers.state.get_current_state_ids(room_id)
-        room_state_events = await self.store.get_events(state_ids.values())
-
-        state_events = {}
-        for key, event_id in state_ids.items():
-            try:
-                state_events[key] = room_state_events[event_id]
-            except KeyError:
-                # Ignore missing event id
-                pass
-
-        return state_events
+        return await self._storage_controllers.state.get_current_state(room_id)
 
     async def on_profile_update(
         self, user_id: str, new_profile: ProfileInfo, by_admin: bool, deactivation: bool
