172ce29b149afb91bbf728b88bffb117172a8f2c
Sean Quah <8349537+squahtx@users.noreply.github.com>
2022-07-19 19:15:54 +0100

    Fix spurious warning when fetching state after a missing prev event (#13258)

--- synapse/handlers/federation_event.py
+++ synapse/handlers/federation_event.py
@@ -1037,6 +1037,9 @@ class FederationEventHandler:
         # XXX: this doesn't sound right? it means that we'll end up with incomplete
         #   state.
         failed_to_fetch = desired_events - event_metadata.keys()
+        # `event_id` could be missing from `event_metadata` because it's not necessarily
+        # a state event. We've already checked that we've fetched it above.
+        failed_to_fetch.discard(event_id)
         if failed_to_fetch:
             logger.warning(
                 "Failed to fetch missing state events for %s %s",
