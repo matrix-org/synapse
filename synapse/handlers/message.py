b2f418d7b1a314d657b1bb0c359744ade969ac8b
Andrew Doh <andrewddo@gmail.com>
2022-07-25 10:02:17 -0700

    update per feedback. made error message returned more descriptive and correct.
    Signed-off-by: Andrew Doh <andrewddo@gmail.com>

--- synapse/handlers/message.py
+++ synapse/handlers/message.py
@@ -322,7 +322,7 @@ class MessageHandler:
                 raise SynapseError(
                     code=403,
                     errcode=Codes.FORBIDDEN,
-                    msg="Getting joined members after leaving is forbidden.",
+                    msg="Getting joined members while not being a current member of the room is forbidden.",
                 )
 
         users_with_profile = await self.store.get_users_in_room_with_profiles(room_id)
