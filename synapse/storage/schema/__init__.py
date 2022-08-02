c26fa2d0d19aa46ea4d03eaf72c2a498a62477a3
Eric Eastwood <erice@element.io>
2022-08-02 15:10:01 -0500

    Move to 72 schema version
    Do as they tell us:
    https://github.com/matrix-org/synapse/runs/7639487550?check_suite_focus=true

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SCHEMA_VERSION = 73  # remember to update the list below when updating
+SCHEMA_VERSION = 72  # remember to update the list below when updating
 """Represents the expectations made by the codebase about the database schema
 
 This should be incremented whenever the codebase changes its requirements on the
@@ -75,8 +75,6 @@ Changes in SCHEMA_VERSION = 71:
 Changes in SCHEMA_VERSION = 72:
     - event_edges.(room_id, is_state) are no longer written to.
     - Tables related to groups are dropped.
-
-Changes in SCHEMA_VERSION = 73:
     - Rename column in `device_lists_outbound_pokes` from `opentracing_context` to
       generalized `tracing_context`.
 """
