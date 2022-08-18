b1e7a767a887dc2878d6d93bf6c5ae483b9dd2db
Eric Eastwood <erice@element.io>
2022-08-18 13:13:59 -0500

    Our cache functions do not play well with the type system
    See https://github.com/matrix-org/synapse/pull/13545#discussion_r949384054

--- synapse/rest/client/room.py
+++ synapse/rest/client/room.py
@@ -630,9 +630,13 @@ class RoomMessageListRestServlet(RestServlet):
     ) -> Tuple[int, JsonDict]:
         processing_start_time = self.clock.time_msec()
         # Fire off and hope that we get a result by the end.
-        room_member_count_deferred = run_in_background(
+        #
+        # We're using the mypy type ignore comment because the `@cached` decorator on
+        # `get_number_joined_users_in_room` doesn't play well with the type system. Maybe in the
+        # future, it can use some ParamSpec wizardry to fix it up.
+        room_member_count_deferred = run_in_background(  # type: ignore[call-arg]
             self.store.get_number_joined_users_in_room,
-            room_id,
+            room_id,  # type: ignore[arg-type]
         )
 
         requester = await self.auth.get_user_by_req(request, allow_guest=True)
