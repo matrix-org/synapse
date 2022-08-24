86d135ac639ad451f8ece43c7530f7c77d4df98f
H. Shay <hillerys@element.io>
2022-08-24 14:08:47 -0700

    fix test to align with new behaviour

--- tests/rest/client/test_rooms.py
+++ tests/rest/client/test_rooms.py
@@ -710,7 +710,7 @@ class RoomsCreateTestCase(RoomBase):
         self.assertEqual(HTTPStatus.OK, channel.code, channel.result)
         self.assertTrue("room_id" in channel.json_body)
         assert channel.resource_usage is not None
-        self.assertEqual(36, channel.resource_usage.db_txn_count)
+        self.assertEqual(35, channel.resource_usage.db_txn_count)
 
     def test_post_room_initial_state(self) -> None:
         # POST with initial_state config key, expect new room id
