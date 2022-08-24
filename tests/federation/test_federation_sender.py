8504037829f109b9df444aae00d89f91c3b7b860
Eric Eastwood <erice@element.io>
2022-08-24 11:07:12 -0500

    Make the test fail with meaningful message when wrong room is used
    See https://github.com/matrix-org/synapse/pull/13605#discussion_r953996355

--- tests/federation/test_federation_sender.py
+++ tests/federation/test_federation_sender.py
@@ -186,8 +186,10 @@ class FederationSenderDevicesTestCases(HomeserverTestCase):
             if room_id == test_room_id:
                 return ["host2"]
 
-            assert "We only expect our stubbed `get_current_hosts_in_room` to be called with "
-            f"test_room_id={test_room_id} but it was called with room_id={room_id}"
+            raise AssertionError(
+                "We only expect our stubbed `get_current_hosts_in_room` to be called with "
+                f"test_room_id={test_room_id} but it was called with room_id={room_id}"
+            )
 
         hs.get_datastores().main.get_current_hosts_in_room = get_current_hosts_in_room
 
