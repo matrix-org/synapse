fa8f079680c7247e8fd96b8086a93abd820bb8c2
Erik Johnston <erik@matrix.org>
2022-09-06 16:51:13 +0100

    Add typing

--- build_rust.py
+++ build_rust.py
@@ -1,11 +1,12 @@
 # A build script for poetry that adds the rust extension.
 
 import os
+from typing import Any, Dict
 
 from setuptools_rust import Binding, RustExtension
 
 
-def build(setup_kwargs):
+def build(setup_kwargs: Dict[str, Any]) -> None:
     original_project_dir = os.path.dirname(os.path.realpath(__file__))
     cargo_toml_path = os.path.join(original_project_dir, "rust", "Cargo.toml")
 
