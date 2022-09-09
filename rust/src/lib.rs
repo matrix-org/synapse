c85c5ace525c3cadac8501c2eba4abbc91d9f09d
Erik Johnston <erik@matrix.org>
2022-09-09 11:29:04 +0100

    Add rust to CI (#13763)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,5 +11,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn synapse_rust(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
     Ok(())
 }
