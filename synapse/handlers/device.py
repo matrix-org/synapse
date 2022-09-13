7e127f850586d61e34c9eb30c77c70c9e19b0987
Nick Barrett <nick@beeper.com>
2022-09-13 11:40:10 +0100

    Don't overwrite variable

--- synapse/handlers/device.py
+++ synapse/handlers/device.py
@@ -273,7 +273,7 @@ class DeviceWorkerHandler:
             # Double check if we still share rooms with the given user.
             users_rooms = await self.store.get_rooms_for_users(possibly_left)
             for changed_user_id, entries in users_rooms.items():
-                if any(room_id in room_ids for room_id in entries):
+                if any(rid in room_ids for rid in entries):
                     possibly_left.discard(changed_user_id)
                 else:
                     possibly_joined.discard(changed_user_id)
