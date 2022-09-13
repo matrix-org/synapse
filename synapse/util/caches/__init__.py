e3d73c0fc78eaaaf39d902645a87e9225e87a002
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2022-09-13 17:03:17 +0100

    Register metric hooks by name to prevent duplicates from building up

--- synapse/util/caches/__init__.py
+++ synapse/util/caches/__init__.py
@@ -205,8 +205,9 @@ def register_cache(
         add_resizable_cache(cache_name, resize_callback)
 
     metric = CacheMetric(cache, cache_type, cache_name, collect_callback)
+    metric_name = "cache_%s_%s" % (cache_type, cache_name)
     caches_by_name[cache_name] = cache
-    CACHE_METRIC_REGISTRY.register_hook(metric.collect)
+    CACHE_METRIC_REGISTRY.register_hook(metric_name, metric.collect)
     return metric
 
 
