f9f8f430328715edaf6005fc5320601e755cc579
Catalan Lover <catalanlover@protonmail.com>
2022-10-09 23:02:58 +0200

    Change Default Room version to 10

--- synapse/config/server.py
+++ synapse/config/server.py
@@ -151,7 +151,7 @@ DEFAULT_IP_RANGE_BLACKLIST = [
     "fec0::/10",
 ]
 
-DEFAULT_ROOM_VERSION = "9"
+DEFAULT_ROOM_VERSION = "10"
 
 ROOM_COMPLEXITY_TOO_GREAT = (
     "Your homeserver is unable to join rooms this large or complex. "
