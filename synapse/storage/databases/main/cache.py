1f8bf7388ba0e992745dab8d8103d95f729bb569
Nick Barrett <nick@beeper.com>
2022-10-18 17:22:47 +0100

    Lint style fix

--- synapse/storage/databases/main/cache.py
+++ synapse/storage/databases/main/cache.py
@@ -253,9 +253,7 @@ class CacheInvalidationWorkerStore(SQLBaseStore):
             self._attempt_to_invalidate_cache(
                 "get_rooms_for_user_with_stream_ordering", (state_key,)
             )
-            self._attempt_to_invalidate_cache(
-                "get_rooms_for_user", (state_key,)
-            )
+            self._attempt_to_invalidate_cache("get_rooms_for_user", (state_key,))
 
         if relates_to:
             self._attempt_to_invalidate_cache("get_relations_for_event", (relates_to,))
