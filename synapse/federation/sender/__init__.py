5acc7d28d06f26097513fa3a1243e3d054ce7904
Erik Johnston <erik@matrix.org>
2022-10-24 12:12:44 +0100

    Fix lint

--- synapse/federation/sender/__init__.py
+++ synapse/federation/sender/__init__.py
@@ -536,7 +536,7 @@ class FederationSender(AbstractFederationSender):
 
                 if event_entries:
                     now = self.clock.time_msec()
-                    ts = max(event_to_received_ts.values())
+                    ts = max(t for t in event_to_received_ts.values() if t)
                     assert ts is not None
 
                     synapse.metrics.event_processing_lag.labels(
