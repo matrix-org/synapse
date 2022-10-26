7b7a928ca521a1ad34ca4c1f39098e9c22b0ed46
Jason Little <realtyem@gmail.com>
2022-10-26 05:21:36 -0500

    Make media_repository shared_extra_conf consistent with other types, like background_worker.

--- docker/configure_workers_and_start.py
+++ docker/configure_workers_and_start.py
@@ -84,7 +84,11 @@ WORKERS_CONFIG: Dict[str, Dict[str, Any]] = {
             "^/_synapse/admin/v1/media/.*$",
             "^/_synapse/admin/v1/quarantine_media/.*$",
         ],
-        "shared_extra_conf": {"enable_media_repo": False},
+        # The first configured media worker will run the media background jobs
+        "shared_extra_conf": {
+            "enable_media_repo": False,
+            "media_instance_running_background_jobs", "media_repository1",
+        },
         "worker_extra_conf": "enable_media_repo: true",
     },
     "appservice": {
@@ -370,10 +374,6 @@ def handle_map_instances_for_shared_config(
             "host": "localhost",
             "port": worker_port,
         }
-
-    elif worker_type == "media_repository":
-        # The first configured media worker will run the media background jobs
-        shared_config.setdefault("media_instance_running_background_jobs", worker_name)
 
 
 def generate_base_homeserver_config() -> None:
