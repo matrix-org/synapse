0ad4b5d78bca1275d8394e3ef13a09e24600ffe1
David Robertson <davidr@element.io>
2022-10-31 12:25:38 +0000

    No, really, fix typechecking

--- tests/metrics/test_metrics.py
+++ tests/metrics/test_metrics.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Protocol
+from typing_extensions import Protocol
 
 try:
     from importlib import metadata
