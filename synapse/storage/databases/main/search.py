f8f01b869e20df9628b90e0343b0c2ea9c660933
Sean Quah <seanq@matrix.org>
2022-11-04 11:38:23 +0000

    Revert "Disable auto search for prefixes in event search"
    This reverts commit 24409c7c158c108fa31433dd20ee33d423f5cb04.

--- synapse/storage/databases/main/search.py
+++ synapse/storage/databases/main/search.py
@@ -771,7 +771,7 @@ def _parse_query(database_engine: BaseDatabaseEngine, search_term: str) -> str:
     results = re.findall(r"([\w\-]+)", search_term, re.UNICODE)
 
     if isinstance(database_engine, PostgresEngine):
-        return " & ".join(result for result in results)
+        return " & ".join(result + ":*" for result in results)
     elif isinstance(database_engine, Sqlite3Engine):
         return " & ".join(result + "*" for result in results)
     else:
