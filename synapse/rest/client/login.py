a4b1f6456276e62b3f4d6b060c289b6413b8a5c2
Tulir Asokan <tulir@maunium.net>
2022-11-04 18:43:51 +0200

    Fix /refresh endpoint version (#14364)

--- synapse/rest/client/login.py
+++ synapse/rest/client/login.py
@@ -536,7 +536,7 @@ def _get_auth_flow_dict_for_idp(idp: SsoIdentityProvider) -> JsonDict:
 
 
 class RefreshTokenServlet(RestServlet):
-    PATTERNS = (re.compile("^/_matrix/client/v1/refresh$"),)
+    PATTERNS = client_patterns("/refresh$")
 
     def __init__(self, hs: "HomeServer"):
         self._auth_handler = hs.get_auth_handler()
