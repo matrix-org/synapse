69814eb2824daf846f869cb9579eb1008e61f8ad
realtyem <realtyem@gmail.com>
2022-11-08 06:34:09 -0600

    Allow override for requesting specific worker types for Complement on command line. (#14324)
    * Expose getting SYNAPSE_WORKER_TYPES from external, allowing override of workers requested.
    
    * Add WORKER_TYPES variable option to complement.sh script that passes requested workers into start_for_complement.sh entrypoint.
    
    * Update docs to reflect this new ability.
    
    * Changelog
    
    * Don't rely on soft wrapping to format long strings
    
    Good idea dklimpel. Thanks for catching that.
    
    Co-authored-by: Dirk Klimpel <5740567+dklimpel@users.noreply.github.com>
    
    * Small nits just noticed in docs.
    
    * Fixup new line in docs.
    
    Co-authored-by: Dirk Klimpel <5740567+dklimpel@users.noreply.github.com>

--- docker/complement/conf/start_for_complement.sh
+++ docker/complement/conf/start_for_complement.sh
@@ -45,7 +45,12 @@ esac
 
 if [[ -n "$SYNAPSE_COMPLEMENT_USE_WORKERS" ]]; then
   # Specify the workers to test with
-  export SYNAPSE_WORKER_TYPES="\
+  # Allow overriding by explicitly setting SYNAPSE_WORKER_TYPES outside, while still
+  # utilizing WORKERS=1 for backwards compatibility.
+  # -n True if the length of string is non-zero.
+  # -z True if the length of string is zero.
+  if [[ -z "$SYNAPSE_WORKER_TYPES" ]]; then
+    export SYNAPSE_WORKER_TYPES="\
       event_persister, \
       event_persister, \
       background_worker, \
@@ -61,6 +66,8 @@ if [[ -n "$SYNAPSE_COMPLEMENT_USE_WORKERS" ]]; then
       appservice, \
       pusher"
 
+  fi
+  log "Workers requested: $SYNAPSE_WORKER_TYPES"
   # Improve startup times by using a launcher based on fork()
   export SYNAPSE_USE_EXPERIMENTAL_FORKING_LAUNCHER=1
 else
