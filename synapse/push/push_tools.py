8804840c2bafb481b3abc0d17101b77a80fa4fbf
Brad Murray <bradtgmurray@gmail.com>
2022-11-16 11:02:20 -0500

    Fix HTTPPusherTests by honouring mac2654 configuration

--- synapse/push/push_tools.py
+++ synapse/push/push_tools.py
@@ -46,7 +46,10 @@ async def get_badge_count(store: DataStore, user_id: str, group_by_room: bool) -
 
         # Beeper change: Only count a room as having unread messages if we
         # have both unread events (MSC2654) *and* notifications (ie, not muted).
-        if notify_count == 0 or notifs.main_timeline.unread_count == 0:
+        if notify_count == 0 or (
+            store.hs.config.experimental.msc2654_enabled
+            and notifs.main_timeline.unread_count == 0
+        ):
             continue
 
         if group_by_room:
