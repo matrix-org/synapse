1183c372fa9da01b2667f1b83dab958dad432c68
Andrew Ferrazzutti <andrewf@element.io>
2022-11-28 11:17:29 -0500

    Use `device_one_time_keys_count` to match MSC3202 (#14565)
    * Use `device_one_time_keys_count` to match MSC3202
    
    Rename the `device_one_time_key_counts` key in responses to
    `device_one_time_keys_count` to match the name specified by MSC3202.
    
    Also change related variable/class names for consistency.
    
    Signed-off-by: Andrew Ferrazzutti <andrewf@element.io>
    
    * Update changelog.d/14565.misc
    
    * Revert name change for `one_time_key_counts` key
    
    as this is a different key altogether from `device_one_time_keys_count`,
    which is used for `/sync` instead of appservice transactions.
    
    Signed-off-by: Andrew Ferrazzutti <andrewf@element.io>

--- synapse/storage/databases/main/appservice.py
+++ synapse/storage/databases/main/appservice.py
@@ -20,7 +20,7 @@ from synapse.appservice import (
     ApplicationService,
     ApplicationServiceState,
     AppServiceTransaction,
-    TransactionOneTimeKeyCounts,
+    TransactionOneTimeKeysCount,
     TransactionUnusedFallbackKeys,
 )
 from synapse.config.appservice import load_appservices
@@ -260,7 +260,7 @@ class ApplicationServiceTransactionWorkerStore(
         events: List[EventBase],
         ephemeral: List[JsonDict],
         to_device_messages: List[JsonDict],
-        one_time_key_counts: TransactionOneTimeKeyCounts,
+        one_time_keys_count: TransactionOneTimeKeysCount,
         unused_fallback_keys: TransactionUnusedFallbackKeys,
         device_list_summary: DeviceListUpdates,
     ) -> AppServiceTransaction:
@@ -273,7 +273,7 @@ class ApplicationServiceTransactionWorkerStore(
             events: A list of persistent events to put in the transaction.
             ephemeral: A list of ephemeral events to put in the transaction.
             to_device_messages: A list of to-device messages to put in the transaction.
-            one_time_key_counts: Counts of remaining one-time keys for relevant
+            one_time_keys_count: Counts of remaining one-time keys for relevant
                 appservice devices in the transaction.
             unused_fallback_keys: Lists of unused fallback keys for relevant
                 appservice devices in the transaction.
@@ -299,7 +299,7 @@ class ApplicationServiceTransactionWorkerStore(
                 events=events,
                 ephemeral=ephemeral,
                 to_device_messages=to_device_messages,
-                one_time_key_counts=one_time_key_counts,
+                one_time_keys_count=one_time_keys_count,
                 unused_fallback_keys=unused_fallback_keys,
                 device_list_summary=device_list_summary,
             )
@@ -379,7 +379,7 @@ class ApplicationServiceTransactionWorkerStore(
             events=events,
             ephemeral=[],
             to_device_messages=[],
-            one_time_key_counts={},
+            one_time_keys_count={},
             unused_fallback_keys={},
             device_list_summary=DeviceListUpdates(),
         )
