36269c72e8973a5066d3cf07c3e021d0ffe879ac
David Robertson <davidr@element.io>
2022-11-29 13:05:07 +0000

    Fix `UndefinedColumn: column "key_json" does not exist` errors when handling users with more than 50 non-E2E devices (#14580)

--- synapse/storage/databases/main/devices.py
+++ synapse/storage/databases/main/devices.py
@@ -1564,6 +1564,7 @@ class DeviceBackgroundUpdateStore(SQLBaseStore):
             # that ensures we keep at most 50 devices.
             sql = """
                 SELECT last_seen FROM devices
+                LEFT JOIN e2e_device_keys_json USING (user_id, device_id)
                 WHERE
                     user_id = ?
                     AND NOT hidden
