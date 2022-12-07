15ff27b5ab26d4d9c4f55297c30edb1d1120e2be
Nick Mills-Barrett <nick@fizzadar.com>
2022-12-07 15:33:51 +0000

    Fix copyright notice
    Co-authored-by: Patrick Cloke <clokep@users.noreply.github.com>

--- tests/rest/client/test_receipts.py
+++ tests/rest/client/test_receipts.py
@@ -1,5 +1,4 @@
-# Copyright 2018-2019 New Vector Ltd
-# Copyright 2019 The Matrix.org Foundation C.I.C.
+# Copyright 2022 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
