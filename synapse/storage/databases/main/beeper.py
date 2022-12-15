dbfe2121b25c5e72da31f04caf555d8c444a5199
Nick Barrett <nick@beeper.com>
2022-12-15 16:10:30 +0000

    Support edited messages and fix quotes in query

--- synapse/storage/databases/main/beeper.py
+++ synapse/storage/databases/main/beeper.py
@@ -19,19 +19,37 @@ class BeeperStore(SQLBaseStore):
 
         def _beeper_preview_for_room_id_and_user_id(txn: LoggingTransaction) -> None:
             sql = """
-            SELECT e.event_id, e.origin_server_ts
-                FROM events AS e \
-                LEFT JOIN redactions as r \
+            SELECT e.event_id, COALESCE(re.origin_server_ts, e.origin_server_ts) as origin_server_ts
+            FROM events AS e
+            LEFT JOIN redactions as r
                 ON e.event_id = r.redacts
-            WHERE e.stream_ordering <= ? AND e.room_id = ? \
-            AND (e.type = "m.room.message" \
-            OR e.type = "m.room.encrypted" \
-            OR e.type = "m.reaction") \
-            AND r.redacts IS NULL \
-            AND CASE WHEN (e.type = "m.reaction") \
-                THEN (SELECT ? = ee.sender AND ee.event_id NOT IN (SELECT redacts FROM redactions WHERE redacts = ee.event_id) FROM events as ee \
-                    WHERE ee.event_id = (SELECT er.relates_to_id FROM event_relations AS er \
-                    WHERE er.event_id = e.event_id)) ELSE (true) END
+            -- Join to relations to find replacements
+            LEFT JOIN event_relations as er
+                ON e.event_id = er.event_id AND er.relation_type = 'm.replace'
+            -- Join the original event that was replaced
+            LEFT JOIN events as re
+                ON re.event_id = er.relates_to_id
+            WHERE
+                e.stream_ordering <= ?
+                AND e.room_id = ?
+                AND r.redacts IS NULL
+                AND (
+                    e.type = 'm.room.message'
+                    OR e.type = 'm.room.encrypted'
+                    OR e.type = 'm.reaction'
+                )
+                AND CASE
+                    -- Only find non-redacted reactions to our own messages
+                    WHEN (e.type = 'm.reaction') THEN (
+                        SELECT ? = ee.sender AND ee.event_id NOT IN (
+                            SELECT redacts FROM redactions WHERE redacts = ee.event_id
+                        ) FROM events as ee
+                        WHERE ee.event_id = (
+                            SELECT eer.relates_to_id FROM event_relations AS eer
+                            WHERE eer.event_id = e.event_id
+                        )
+                    )
+                    ELSE (true) END
             ORDER BY stream_ordering DESC
             LIMIT 1
             """
