ae11d626ff6f5e40a30c2e35b518688dd8d090c3
Nick Barrett <nick@beeper.com>
2022-12-20 11:28:39 +0000

    Only include non-bot member counts if beeper previews enabled

--- synapse/handlers/sync.py
+++ synapse/handlers/sync.py
@@ -803,9 +803,10 @@ class SyncHandler:
         ).count
 
         # Beeper TODO: remove once synapse no longer handles bridges
-        summary[
-            "com.beeper.m.joined_users_without_bots"
-        ] = self.store.get_number_joined_non_bot_users_in_room(room_id)
+        if sync_config.beeper_previews:
+            summary[
+                "com.beeper.m.joined_users_without_bots"
+            ] = self.store.get_number_joined_non_bot_users_in_room(room_id)
 
         # if the room has a name or canonical_alias set, we can skip
         # calculating heroes. Empty strings are falsey, so we check
