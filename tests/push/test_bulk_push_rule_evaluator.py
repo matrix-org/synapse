b86dbb2bdae3509b1ac9ece2b287268a6b750b3d
Patrick Cloke <patrickc@matrix.org>
2023-01-11 10:25:22 -0500

    Add a comment.

--- tests/push/test_bulk_push_rule_evaluator.py
+++ tests/push/test_bulk_push_rule_evaluator.py
@@ -118,6 +118,9 @@ class TestBulkPushRuleEvaluator(HomeserverTestCase):
         )
 
         bulk_evaluator = BulkPushRuleEvaluator(self.hs)
+        # Mock the method which calculates push rules -- we do this instead of
+        # e.g. checking the results in the database because we want to ensure
+        # that code isn't even running.
         bulk_evaluator._action_for_event_by_user = simple_async_mock()  # type: ignore[assignment]
 
         # Ensure no actions are generated!
