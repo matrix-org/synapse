b50c008453001aee8dd7dbd6f36ec32039e6ce76
Erik Johnston <erik@matrix.org>
2023-01-12 10:52:07 +0000

    Re-enable some linting (#14821)
    * Re-enable some linting
    
    * Newsfile
    
    * Remove comment

--- stubs/synapse/synapse_rust/push.pyi
+++ stubs/synapse/synapse_rust/push.pyi
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Collection, Dict, Mapping, Optional, Sequence, Set, Tuple, Union
+from typing import Any, Collection, Dict, Mapping, Optional, Sequence, Tuple, Union
 
 from synapse.types import JsonDict
 
