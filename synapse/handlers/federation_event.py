a03ee6ec7d200dda62c0ef96511c73f410c97360
Erik Johnston <erik@matrix.org>
2023-01-17 09:29:38 +0000

    Change ID generator to return position of last write
    This is already true when asking for stream positions of other
    instances, but for our own instance we have fudged it. Changing this
    should be fine (it was just an optimisation), and I don't think it
    should have much impact in practice at all.
    
    The reason to do this is so that when tell remotes what our current
    position is we only include *our* writes, rather than writes of other
    instances. This reduces delays when the remote instance is waiting for
    stream positions to update. In practice, this is probably only a problem
    for tests, though we may as well do it for all of them.

--- synapse/handlers/federation_event.py
+++ synapse/handlers/federation_event.py
@@ -2259,6 +2259,10 @@ class FederationEventHandler:
                 event_and_contexts, backfilled=backfilled
             )
 
+            # After persistence we always need to notify replication there may
+            # be new data.
+            self._notifier.notify_replication()
+
             if self._ephemeral_messages_enabled:
                 for event in events:
                     # If there's an expiry timestamp on the event, schedule its expiry.
