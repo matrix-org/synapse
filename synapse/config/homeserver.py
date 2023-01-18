f06d4c8bcf204e0e334406d91bf739109283114b
CJS <1005871594@qq.com>
2023-01-18 16:06:33 +0800

    修改路径
    修改路径

--- synapse/config/homeserver.py
+++ synapse/config/homeserver.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from synapse.config.login import LoginConfig
+from .login import LoginConfig
 from ._base import RootConfig
 from .account_validity import AccountValidityConfig
 from .api import ApiConfig
