71647080613319ac91d8f0ca1f19326e96adc39e
Harishankar Kumar <hari01584@gmail.com>
2023-01-29 15:53:12 +0530

    remove unused imports, lint
    Signed-off-by: Harishankar Kumar <hari01584@gmail.com>

--- synapse/storage/databases/main/events.py
+++ synapse/storage/databases/main/events.py
@@ -26,7 +26,6 @@ from typing import (
     Iterable,
     List,
     Optional,
-    Sequence,
     Set,
     Tuple,
 )
--- synapse/storage/databases/main/events_bg_updates.py
+++ synapse/storage/databases/main/events_bg_updates.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import logging
-from typing import TYPE_CHECKING, Dict, List, Optional, Sequence, Set, Tuple, cast
+from typing import TYPE_CHECKING, Dict, List, Optional, Set, Tuple, cast
 
 import attr
 
