7b9faea42af2b2743573905a447ef12f007943bc
David Robertson <davidr@element.io>
2023-01-30 20:52:17 +0000

    linter scripttttttttt

--- tests/test_event_auth.py
+++ tests/test_event_auth.py
@@ -733,6 +733,7 @@ class EventAuthTestCase(unittest.TestCase):
 
         test_room_v10_rejects_string_power_levels above handles the string case.
         """
+
         def create_event(pl_event_content: Dict[str, Any]) -> EventBase:
             return make_event_from_dict(
                 {
