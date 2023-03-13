1b23a6a872ee0868c2fea143a0bb04f1bc5032ab
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2023-03-13 11:09:28 +0000

    Enable autodoc extension in Sphinx

--- dev-docs/conf.py
+++ dev-docs/conf.py
@@ -13,7 +13,9 @@ author = 'The Synapse Maintainers and Community'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
