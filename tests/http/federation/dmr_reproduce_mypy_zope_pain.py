9b985cf8a82076d8ae5c91b111920593fec211ab
David Robertson <davidr@element.io>
2023-03-23 14:58:38 +0000

    wip 6

--- tests/http/federation/dmr_reproduce_mypy_zope_pain.py
+++ tests/http/federation/dmr_reproduce_mypy_zope_pain.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from twisted.internet.endpoints import _WrappingProtocol
-from twisted.internet.interfaces import IProtocol
 from zope.interface import Interface, implementer
 
 
@@ -17,17 +16,18 @@ class DummyAddress:
 dummy_address = DummyAddress()
 
 
+class IProtocol(Interface):
+    pass
+
+
 class IProtocolFactory(Interface):
     def buildProtocol(addr: IAddress) -> Optional[IProtocol]:
         pass
 
+
 def _make_connection(
     client_factory: IProtocolFactory,
-    server_factory: IProtocolFactory,
 ) -> None:
-    server_protocol = server_factory.buildProtocol(dummy_address)
-    assert server_protocol is not None
-
     client_protocol = client_factory.buildProtocol(dummy_address)
     assert isinstance(client_protocol, _WrappingProtocol)
     print("Hello")
