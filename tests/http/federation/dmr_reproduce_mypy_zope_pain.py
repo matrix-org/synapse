af2f251ace5952b4de3837030e2ce721b06501d1
David Robertson <davidr@element.io>
2023-03-23 14:47:10 +0000

    WIP 4

--- tests/http/federation/dmr_reproduce_mypy_zope_pain.py
+++ tests/http/federation/dmr_reproduce_mypy_zope_pain.py
@@ -3,30 +3,20 @@ from twisted.internet.interfaces import (
     IProtocolFactory,
 )
 
-from tests import unittest
 from tests.http import (
     dummy_address,
 )
 
+def _make_connection(
+    client_factory: IProtocolFactory,
+    server_factory: IProtocolFactory,
+) -> None:
+    server_protocol = server_factory.buildProtocol(dummy_address)
+    assert server_protocol is not None
 
-
-class MatrixFederationAgentTests(unittest.TestCase):
-
-    def _make_connection(
-        self,
-        client_factory: IProtocolFactory,
-    ) -> None:
-        server_factory = _get_test_protocol_factory()
-        server_protocol = server_factory.buildProtocol(dummy_address)
-        assert server_protocol is not None
-
-        client_protocol = client_factory.buildProtocol(dummy_address)
-        assert isinstance(client_protocol, _WrappingProtocol)
-        print("Hello")
-
-
-def _get_test_protocol_factory() -> IProtocolFactory:
-    raise NotImplementedError()
+    client_protocol = client_factory.buildProtocol(dummy_address)
+    assert isinstance(client_protocol, _WrappingProtocol)
+    print("Hello")
 
 
 
