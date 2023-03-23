a64e13fe4a58dbda02a2c9c68aaf0bf36d03fdb3
Patrick Cloke <patrickc@matrix.org>
2023-03-23 14:53:50 -0400

    Force ensure there are no non-null thread_id values for event_push_{actions_staging,actions,sumary} and set the column to non-nullable.

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -95,9 +95,9 @@ Changes in SCHEMA_VERSION = 74:
 
 
 SCHEMA_COMPAT_VERSION = (
-    # The threads_id column must exist for event_push_actions, event_push_summary,
-    # receipts_linearized, and receipts_graph.
-    73
+    # The threads_id column must written to with non-null values event_push_actions,
+    # event_push_actions_staging, and event_push_summary.
+    74
 )
 """Limit on how far the synapse codebase can be rolled back without breaking db compat
 
