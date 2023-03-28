86f4c053a13c0196226548d82f0eee1442d2e404
H. Shay <hillerys@element.io>
2023-03-28 14:26:31 -0700

    fix even more drift

--- synapse/storage/schema/main/delta/75/01_delete_e2e_backup_keys_for_deactivated_users.sql
+++ synapse/storage/schema/main/delta/75/01_delete_e2e_backup_keys_for_deactivated_users.sql
@@ -14,4 +14,4 @@
  */
 
 INSERT INTO background_updates (ordering, update_name, progress_json) VALUES
-  (7403, 'delete_e2e_backup_keys_for_deactivated_users', '{}');
+  (7501, 'delete_e2e_backup_keys_for_deactivated_users', '{}');
