e3bd59b9582cff4788ea731793d5f70b909e71ad
Dirk Klimpel <5740567+dklimpel@users.noreply.github.com>
2023-04-14 18:41:01 +0200

    Update synapse/http/server.py
    Co-authored-by: Patrick Cloke <clokep@users.noreply.github.com>

--- synapse/http/server.py
+++ synapse/http/server.py
@@ -46,7 +46,10 @@ from twisted.internet import defer, interfaces
 from twisted.internet.defer import CancelledError
 from twisted.python import failure
 from twisted.web import resource
-from twisted.web.pages import notFound
+try:
+    from twisted.web.pages import notFound
+except ImportError:
+    from twisted.web.resource import NoResource as notFound
 from twisted.web.resource import IResource
 from twisted.web.server import NOT_DONE_YET, Request
 from twisted.web.static import File
