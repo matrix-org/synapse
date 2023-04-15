76d6379727eadf53a61c1c91f158251534a0dfb2
Sean Quah <seanq@matrix.org>
2023-04-15 02:20:36 +0100

    De-localpart `ProfileWorkerStore.create_profile()`
    Signed-off-by: Sean Quah <seanq@matrix.org>

--- synapse/storage/databases/main/profile.py
+++ synapse/storage/databases/main/profile.py
@@ -99,9 +99,12 @@ class ProfileWorkerStore(SQLBaseStore):
             else:
                 raise
 
-    async def create_profile(self, user_localpart: str) -> None:
+    async def create_profile(self, user_id: str) -> None:
+        user_localpart = UserID.from_string(user_id).localpart
         await self.db_pool.simple_insert(
-            table="profiles", values={"user_id": user_localpart}, desc="create_profile"
+            table="profiles",
+            values={"user_id": user_localpart, "full_user_id": user_id},
+            desc="create_profile",
         )
 
     async def set_profile_displayname(
