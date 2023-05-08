2987d9af80b4c1b97ee89c93110f3cd9daadf63b
Travis Ralston <travisr@matrix.org>
2023-05-08 11:21:38 -0600

    Clean up imports

--- tests/rest/media/test_domain_blocking.py
+++ tests/rest/media/test_domain_blocking.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from binascii import unhexlify
 from typing import Dict
 
 from twisted.test.proto_helpers import MemoryReactor
