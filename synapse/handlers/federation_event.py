30925fd0ffa36f2ba9ac546b366ba324cc1321cc
Eric Eastwood <contact@ericeastwood.com>
2023-05-18 16:21:59 -0500

    Trace how many new events from the backfill response we need to process

--- synapse/handlers/federation_event.py
+++ synapse/handlers/federation_event.py
@@ -890,6 +890,11 @@ class FederationEventHandler:
             # Continue on with the events that are new to us.
             new_events.append(event)
 
+        set_tag(
+            SynapseTags.RESULT_PREFIX + "new_events.length",
+            str(len(new_events)),
+        )
+
         # We want to sort these by depth so we process them and
         # tell clients about them in order.
         sorted_events = sorted(new_events, key=lambda x: x.depth)
