39861db55e0b33e7caee5d6292509102fa79570d
H. Shay <hillerys@element.io>
2023-05-22 14:15:16 -0700

    add space back

--- tests/storage/test_profile.py
+++ tests/storage/test_profile.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import importlib.util
+
 from twisted.test.proto_helpers import MemoryReactor
 
 from synapse.server import HomeServer
