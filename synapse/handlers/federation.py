e49d58fb50b68819b00c6e95caed87e1a199c02e
Eric Eastwood <contact@ericeastwood.com>
2023-06-02 19:29:11 -0500

    Backfill in the background if we're doing it "just because"
    Fix https://github.com/matrix-org/synapse/issues/15702

--- synapse/handlers/federation.py
+++ synapse/handlers/federation.py
@@ -310,7 +310,9 @@ class FederationHandler:
             logger.debug(
                 "_maybe_backfill_inner: all backfill points are *after* current depth. Trying again with later backfill points."
             )
-            return await self._maybe_backfill_inner(
+            run_as_background_process(
+                "_maybe_backfill_inner_anyway_with_max_depth",
+                self._maybe_backfill_inner,
                 room_id=room_id,
                 # We use `MAX_DEPTH` so that we find all backfill points next
                 # time (all events are below the `MAX_DEPTH`)
@@ -321,6 +323,10 @@ class FederationHandler:
                 # overall otherwise the smaller one will throw off the results.
                 processing_start_time=None,
             )
+            # We return `False` because we're backfilling but doing it in the background
+            # and is just a best effort attempt for eventual consistency's sake. The
+            # return value of this function isn't used for anything yet anyway.
+            return False
 
         # Even after recursing with `MAX_DEPTH`, we didn't find any
         # backward extremities to backfill from.
