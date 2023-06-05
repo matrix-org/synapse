9dd056c19f9c3f4e93567373ecc90ce08f0f57d8
Eric Eastwood <contact@ericeastwood.com>
2023-06-05 15:07:55 -0500

    Grammar

--- synapse/http/matrixfederationclient.py
+++ synapse/http/matrixfederationclient.py
@@ -506,7 +506,7 @@ class MatrixFederationHttpClient:
                 server) we can be a lot more lenient but its very fuzzy / hand-wavey.
 
                 In the future, we could be more intelligent about doing this sort of
-                thing looking at things with the bigger picture in mind,
+                thing by looking at things with the bigger picture in mind,
                 https://github.com/matrix-org/synapse/issues/8917
 
             ignore_backoff: true to ignore the historical backoff data
