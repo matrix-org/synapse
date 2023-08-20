548b64b7a63f3aff7b2a9bdfc52a01aab1fb3b24
Neil Johnson <neil@matrix.org>
2023-08-20 20:55:02 +0100

    Filtered in the incorrect place. Revert "filter out unwanted user_agents"
    This reverts commit 6fabc26074f0f36151922d2431bd0dcc47c2f87a.

--- synapse/storage/databases/main/metrics.py
+++ synapse/storage/databases/main/metrics.py
@@ -411,10 +411,7 @@ class ServerMetricsStore(EventPushActionsWorkerStore, SQLBaseStore):
                     ON u.user_id = udv.user_id AND u.device_id=udv.device_id
                     INNER JOIN users ON users.name=u.user_id
                     WHERE ? <= last_seen AND last_seen < ?
-                    AND udv.timestamp IS NULL
-                    AND u.user_agent !='sync-v3-proxy-'
-                    AND u.user_agent !='matrix-rust-sdk'
-                    AND users.is_guest=0
+                    AND udv.timestamp IS NULL AND users.is_guest=0
                     AND users.appservice_id IS NULL
                     GROUP BY u.user_id, u.device_id
             """
