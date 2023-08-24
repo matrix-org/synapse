e2e84d263948bd7510dfc4ebf42806c0ce8d3d53
Patrick Cloke <patrickc@matrix.org>
2023-08-24 14:48:01 -0400

    Lint

--- tests/handlers/test_e2e_keys.py
+++ tests/handlers/test_e2e_keys.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Iterable, Dict
+from typing import Dict, Iterable
 from unittest import mock
 
 from parameterized import parameterized
