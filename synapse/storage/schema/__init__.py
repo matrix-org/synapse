0d33bafaf81a2fc488d94f6c8bac6d02589257c7
H. Shay <hillerys@element.io>
2023-08-24 15:14:28 -0700

    drop not null constraint on column `user_id` of tables `profiles` and `user_filters`

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SCHEMA_VERSION = 80  # remember to update the list below when updating
+SCHEMA_VERSION = 81  # remember to update the list below when updating
 """Represents the expectations made by the codebase about the database schema
 
 This should be incremented whenever the codebase changes its requirements on the
@@ -114,6 +114,9 @@ Changes in SCHEMA_VERSION = 79
 Changes in SCHEMA_VERSION = 80
     - The event_txn_id_device_id is always written to for new events.
     - Add tables for the task scheduler.
+
+Changes in SCHEMA_VERSION = 81
+    - We no longer write to column user_id of tables profiles and user_filters
 """
 
 
