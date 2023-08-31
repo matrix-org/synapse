99a7fa859fc86969a0995e7dba457ae31cce2b51
Mathieu Velten <mathieuv@matrix.org>
2023-08-31 14:19:33 +0200

    lint

--- synapse/api/auth/msc3861_delegated.py
+++ synapse/api/auth/msc3861_delegated.py
@@ -28,7 +28,6 @@ from twisted.web.http_headers import Headers
 from synapse.api.auth.base import BaseAuth
 from synapse.api.errors import (
     AuthError,
-    Codes,
     HttpResponseException,
     InvalidClientTokenError,
     OAuthInsufficientScopeError,
