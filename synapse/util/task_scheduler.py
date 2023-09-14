7f801bc884f918d5a3622f520ac0c07c839063ec
Erik Johnston <erik@matrix.org>
2023-09-14 15:34:53 +0100

    LINT

--- synapse/util/task_scheduler.py
+++ synapse/util/task_scheduler.py
@@ -15,7 +15,6 @@
 import logging
 from typing import TYPE_CHECKING, Awaitable, Callable, Dict, List, Optional, Set, Tuple
 
-
 from twisted.python.failure import Failure
 
 from synapse.logging.context import nested_logging_context
