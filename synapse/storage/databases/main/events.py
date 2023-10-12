ade43723aa962e2517141969bacaad76bc31fa6d
Erik Johnston <erik@matrix.org>
2023-10-12 15:04:43 +0100

    Record the stream ordering of where we have (potential) gaps in rooms

--- synapse/storage/databases/main/events.py
+++ synapse/storage/databases/main/events.py
@@ -2280,7 +2280,7 @@ class PersistEventsStore:
         if not potential_backwards_extremities:
             return
 
-        existing_outliers = self.db_pool.simple_select_many_txn(
+        existing_events_outliers = self.db_pool.simple_select_many_txn(
             txn,
             table="events",
             column="event_id",
@@ -2289,7 +2289,9 @@ class PersistEventsStore:
             retcols=("event_id",),
         )
 
-        potential_backwards_extremities.difference_update(e for e, in existing_outliers)
+        potential_backwards_extremities.difference_update(
+            e for e, in existing_events_outliers
+        )
 
         if potential_backwards_extremities:
             self.db_pool.simple_upsert_many_txn(
@@ -2299,6 +2301,23 @@ class PersistEventsStore:
                 key_values=[(room_id, ev) for ev in potential_backwards_extremities],
                 value_names=(),
                 value_values=(),
+            )
+
+            # Record the stream orderings where we have new gaps.
+            gap_events = [
+                (room_id, self._instance_name, ev.internal_metadata.stream_ordering)
+                for ev in events
+                if any(
+                    e_id in potential_backwards_extremities
+                    for e_id in ev.prev_event_ids()
+                )
+            ]
+
+            self.db_pool.simple_insert_many_txn(
+                txn,
+                table="timeline_gaps",
+                keys=("room_id", "instance_name", "stream_ordering"),
+                values=gap_events,
             )
 
         # Delete all these events that we've already fetched and now know that their
