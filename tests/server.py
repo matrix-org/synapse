79c76e6c70ebbc64acd0ee833923d93303cdff8b
Patrick Cloke <patrickc@matrix.org>
2023-10-23 16:22:36 -0400

    Revert "TEMP: Test w/o patch."
    This reverts commit 5dcaea64635b89953e66bb9e3ed9537cf178f1b1.

--- tests/server.py
+++ tests/server.py
@@ -40,7 +40,7 @@ from typing import (
     Union,
     cast,
 )
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import attr
 from typing_extensions import ParamSpec
@@ -473,6 +473,18 @@ class ThreadedMemoryReactorClock(MemoryReactorClock):
                 if name not in lookups:
                     return fail(DNSLookupError("OH NO: unknown %s" % (name,)))
                 return succeed(lookups[name])
+
+        # In order for the TLS protocols to use the proper reactor's clock we need to patch
+        # _get_default_clock.
+        #
+        # This is *super* dirty since we never unpatch and rely on the next test
+        # to patch over us.
+        #
+        # Use create=True for backwards compatibilty with Twisted <= 23.8.0.
+        self._tls_clock_patcher = patch(
+            "twisted.protocols.tls._get_default_clock", return_value=self, create=True
+        )
+        self._tls_clock_patcher.start()
 
         self.nameResolver = SimpleResolverComplexifier(FakeResolver())
         super().__init__()
