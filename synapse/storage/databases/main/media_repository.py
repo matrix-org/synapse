38badff6aa7c73bf77aa3df73d794aa96c23c23c
Patrick Cloke <clokep@users.noreply.github.com>
2023-11-15 08:26:10 -0500

    Appease mypy

--- synapse/storage/databases/main/media_repository.py
+++ synapse/storage/databases/main/media_repository.py
@@ -151,7 +151,9 @@ class MediaRepositoryBackgroundUpdateStore(SQLBaseStore):
         )
 
         if hs.config.media.can_load_media_repo:
-            self.unused_expiration_time = hs.config.media.unused_expiration_time
+            self.unused_expiration_time: Optional[
+                int
+            ] = hs.config.media.unused_expiration_time
         else:
             self.unused_expiration_time = None
 
